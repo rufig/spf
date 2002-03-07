5f3915deb85c998c3424b2737c55c2097735f4b5
spf <>
2002-03-07 23:49:11 +0000

    COM-server samples

---
+++ ac-lib3/win/com/com_server.f
@@ -0,0 +1,51 @@
+REQUIRE CreateObject ~ac/lib/win/com/com.f
+
+0x80020006 CONSTANT DISP_E_UNKNOWNNAME
+0x8002000E CONSTANT DISP_E_BADPARAMCOUNT
+0x80020009 CONSTANT DISP_E_EXCEPTION
+         4 CONSTANT DISPATCH_PROPERTYPUT
+
+: Class. ( oid -- oid )
+  CR DUP WordByAddr TYPE R@ WordByAddr TYPE SPACE
+;
+
+: Class: ( implement_interface "name" "clsid" -- current class_int )
+  CREATE 
+    HERE SWAP
+    BL WORD COUNT CLSID, 
+    DUP ,               \ предок
+    Methods#            \ к-во методов предка
+    DUP ,               \ свои методы (к-во)
+    LATEST ,            \ своё имя
+    HERE CELL+ ,        \ oid класса (указатель на Vtable)
+    1+ CELLS HERE SWAP DUP ALLOT ERASE     \ VTABLE
+    -1 ,
+    GET-CURRENT WORDLIST SET-CURRENT SWAP
+  DOES> 7 CELLS + ( oid )
+;
+
+: Class; DROP SET-CURRENT ;
+
+: Class ( oid -- class_int ) 7 CELLS - ;
+
+: Extends ( class_int -- class_int )
+  DUP
+  ' EXECUTE             \ oid класса, из которого копируем VTABLE
+  DUP 2 CELLS - @ CELLS \ сколько копировать
+  ( class_int class_int oid n )
+  SWAP @                \ откуда копировать
+  ROT 8 CELLS +         \ куда
+  ROT MOVE
+;
+
+: ToVtable ( class_int xt -- class_int )
+  OVER >R
+  LAST @ FIND
+  IF >BODY @ ( номер метода в VTABLE )
+     8 + \ смещение VTABLE в определении класса
+     CELLS R> + !
+  ELSE -321 THROW THEN
+;
+: METHOD ( class_int -- class_int )
+  LAST @ NAME> TASK ToVtable
+;
---
+++ ac-lib3/win/com/samples/com-sample2.f
@@ -0,0 +1,2 @@
+REQUIRE TEST2 ~ac/lib/win/com/samples/com-sample4.f
+: -Embedding TEST ;
