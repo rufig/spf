fde180a021a4105a554eb5fe6e8922fbed06cc04
spf <>
2002-10-15 19:51:44 +0000

    Win9x not supports share_delete

--- ac-lib3/win/file/share-delete.f
+++ ac-lib3/win/file/share-delete.f
@@ -1,13 +1,16 @@
 \ открытие файла в таком режиме позволяет переименовывать
 \ и даже удалять этот открытый файл. Реальное удаление
 \ произойдет после закрытия всех хэндлов.
+\ работает только в WinNT*
+
+REQUIRE WinNT? ~ac/lib/win/winver.f
 
 : OPEN-FILE-SHARED-DELETE ( c-addr u fam -- fileid ior )
   SWAP DROP SWAP >R >R
   0 FILE_ATTRIBUTE_ARCHIVE ( template attrs )
   OPEN_EXISTING
   SA ( secur )
-  7 ( share read/write/delete )  
+  WinNT? IF 7 ( share read/write/delete ) ELSE 3 THEN
   R> ( access=fam )
   R> ( filename )
   CreateFileA DUP -1 = IF GetLastError ELSE 0 THEN
@@ -17,7 +20,7 @@
   0 FILE_ATTRIBUTE_ARCHIVE ( template attrs )
   CREATE_ALWAYS
   SA ( secur )
-  7 ( share )  
+  WinNT? IF 7 ( share read/write/delete ) ELSE 3 THEN
   R> ( access=fam )
   R> ( filename )
   CreateFileA DUP -1 = IF GetLastError ELSE 0 THEN
