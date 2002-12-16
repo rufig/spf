69ee86d02cbc7d90fd2a6bd1f764664f7e097092
anfilat <>
2002-12-16 17:24:43 +0000

    был не прав

--- devel/~af/lib/api-func.f
+++ devel/~af/lib/api-func.f
@@ -21,24 +21,9 @@ REQUIRE ON         lib\ext\onoff.f
     SWAP OVER + SWAP 0 ?DO DUP I - 1- C@ HOLD LOOP DROP
   ;
 [THEN]
-[UNDEFINED] SEARCH-IN-LIST [IF]
-  : SEARCH-IN-LIST ( a u list -- FALSE | addr TRUE )
-    ROT ROT 2>R
-    BEGIN
-      @ DUP
-    WHILE
-      DUP CELL+ ASCIIZ> 2R@ COMPARE
-      0= IF
-        RDROP RDROP
-        CELL+ TRUE EXIT
-      THEN
-    REPEAT DROP RDROP RDROP
-    FALSE
-  ;
-[THEN]
 
-\ в этом списке хранятся имена dll, в которых ищется функция
-VARIABLE DLL-LIST
+\ в этом словаре хранится список dll, в которых ищется функция
+VOCABULARY API-FUNC-VOC
 VARIABLE ANSIAPI
 ANSIAPI ON
 \ либа замедляет цикл интерпретации, поэтому нужна возможность отключать ее
@@ -50,13 +35,15 @@ API-FUNC ON
     [CHAR] " = IF [CHAR] " DUP SKIP PARSE ELSE NextWord THEN
   ELSE DROP NextWord THEN
   2DUP
-  DLL-LIST SEARCH-IN-LIST 0= IF
+  [ ALSO API-FUNC-VOC CONTEXT @  PREVIOUS ] LITERAL
+  SEARCH-WORDLIST 0= IF
     2DUP + 0 SWAP C!
     OVER LoadLibraryA 0= IF -2009 THROW THEN
-    GET-CURRENT >R FORTH-WORDLIST SET-CURRENT
-    HERE DLL-LIST @ , DLL-LIST ! ( связь )
-    1+ HERE SWAP DUP ALLOT MOVE
-    R> SET-CURRENT
+    GET-CURRENT >R ALSO API-FUNC-VOC DEFINITIONS
+    2DUP <# HOLDS S" CREATE " HOLDS 0 0 #> EVALUATE
+    1+ HERE OVER ALLOT
+    SWAP MOVE
+    PREVIOUS R> SET-CURRENT
   ELSE
     DROP 2DROP
   THEN
@@ -78,14 +65,17 @@ USER ListFunc
 
 \ Поиск функции, имя которой лежит в PAD, в подключенных длльках
 : SEARCH-FUNC ( -- NameLibAddr ProcAddr t | f )
-  DLL-LIST
+  [ ALSO API-FUNC-VOC CONTEXT @  PREVIOUS ] LITERAL
+  @
   BEGIN
-    @ ?DUP
+    DUP
   WHILE
-    DUP CELL+ LoadLibraryA DUP 0= IF -2009 THROW THEN
+    DUP NAME> EXECUTE DUP LoadLibraryA DUP 0= IF -2009 THROW THEN
     PAD SWAP GetProcAddress
-    ?DUP IF SWAP CELL+ SWAP TRUE EXIT THEN
+    ?DUP IF ROT DROP TRUE EXIT THEN
+    DROP CDR
   REPEAT
+  DROP
   FALSE
 ;
 
