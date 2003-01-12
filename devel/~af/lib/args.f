0da4f0f470892076ab0c1410835beb94961bccdf
anfilat <>
2003-01-12 12:34:24 +0000

    *** empty log message ***

--- devel/~af/lib/args.f
+++ devel/~af/lib/args.f
@@ -8,20 +8,19 @@
 ;
 : ArgC ( -- n )
   >IN @ >R  0 >IN !
-  -1 BEGIN NextArg NIP WHILE 1+ REPEAT
+  0 BEGIN NextArg NIP WHILE 1+ REPEAT
   R> >IN !
 ;
 : ArgV ( n -- addr )
-  HERE DUP ROT 1+
-  DUP CELLS ALLOT
+  HERE DUP ROT
+  DUP 1+ CELLS ALLOT
   0 DO
     HERE OVER ! CELL+
     NextArg HERE SWAP DUP ALLOT MOVE 0 C,
   LOOP
-  DROP
+  0!
 ;
 : ParseArgs ( -- argc argv )  ArgC DUP  ArgV ;
 : args ( addr u -- argc argv )  ['] ParseArgs EVALUATE-WITH ;
 
-\ WINAPI: GetCommandLineA kernel32.dll
 \ GetCommandLineA ASCIIZ> args
