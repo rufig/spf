cef3fdc71e1870c0f9a4df41a5a4b0c0f9a1e3a7
ruv <>
2003-09-15 17:43:07 +0000

    + Queue-Count +  mapQueue

--- devel/~pinka/lib/queue_pr.f
+++ devel/~pinka/lib/queue_pr.f
@@ -6,6 +6,8 @@
      .queue  LeaveLow        Enterly New-Queue       
      VocPrioritySupport      Del-Queue 
 )
+\ 05.Jul.2002 Fri 23:47 + Queue-Count + ^cnt
+\ 14.Sep.2003 Sun  + mapQueue * Исправлена серьезная ошибка насчет ^cnt
 
 REQUIRE {  ~ac\lib\locals.f
 
@@ -13,15 +15,17 @@ MODULE:  VocPrioritySupport
 
 \ priority: LowValueFirst
 
+( двунаправленный упорядоченный по priority список. )
 0  \ list element
-1 CELLS  -- ^next       
-1 CELLS  -- ^prev 
+1 CELLS  -- ^next
+1 CELLS  -- ^prev
 1 CELLS  -- ^priority
+      0  -- ^cnt  \ т.к. используется только у первого псевдо-элемента ( a )
 1 CELLS  -- ^value
 CONSTANT /elist
 
 : insert { newel elem -- }
-\ elem1 before ( at left) elem
+\ newelem before ( at left) elem
   elem ^prev @  DUP 
     newel ^prev  !
     newel SWAP ^next !
@@ -29,7 +33,7 @@ CONSTANT /elist
   newel elem ^prev !
 ;
 : remove { elem -- }
-  elem ^next @  elem ^prev @  ^next !  
+  elem ^next @  elem ^prev @  ^next !
   elem ^prev @  elem ^next @  ^prev !
   \ правлю только ссылки на elem 
   \ сам elem  не правлю.
@@ -45,9 +49,11 @@ EXPORT
   z a ^next !
   a z ^prev !
   -1 a ^priority !
+   0 z ^priority ! \ must be so !
+   ( граница - по граничному priority )
   a  z ^value !
   z
-;
+; \ queue = elem_z
 
 DEFINITIONS
 
@@ -59,6 +65,7 @@ EXPORT
 
 : Enterly { x pr  queue  \ newel  -- }
 \ включить элемент x в очередь queue с приоритетом pr
+  queue ^value @ ^cnt 1+!
   queue first ( elem )
   BEGIN pr OVER ^priority @ U< WHILE ^next @ REPEAT ( elem ) 
   /elist ALLOCATE THROW -> newel
@@ -68,15 +75,19 @@ EXPORT
 ;
 
 : LeaveLow  ( queue -- x true | false )
-\ исключить из очереди первый элемент (c наименьшим численным значением pr), 
+\ исключить из очереди первый элемент (c наименьшим численным значением pr),
 \ оставить элемент на стеке и true, в случае успеха
 \ или false в случае не успеха (пустая очередь).
-  { \ elem }
-  DUP ^prev @ -> elem
-  ^value @ elem = IF FALSE EXIT THEN
-  elem remove
-  elem ^value @  -1
-  elem FREE THROW 
+  { q \ elem }
+  q ^value @ ^cnt @ IF
+    -1 q ^value @ ^cnt +!
+    q ^prev @ -> elem
+    \ q ^value @ elem = IF FALSE EXIT THEN
+    elem remove
+    elem ^value @  -1
+    elem FREE THROW 
+    EXIT     
+  THEN  FALSE
 ;
 
 : Del-Queue ( queue -- )
@@ -86,10 +97,20 @@ EXPORT
 \ Include ( x pr  queue -- )
 \ ExcludeLow ( queue -- x true | false )
 
-: .queue { q -- }
+: queue. { q -- }
   q first BEGIN DUP q <> WHILE
     DUP .  DUP ^priority @ . DUP ^value @ . CR
     ^next @
+  REPEAT DROP
+;
+: Queue-Count ( q -- count )
+  ^value @  ^cnt @
+;
+
+: mapQueue { q xt \ e -- }  
+  q first BEGIN DUP q <> WHILE -> e
+    e ^value @ e ^priority  @ xt EXECUTE
+    e ^next @
   REPEAT DROP
 ;
 
@@ -97,12 +118,17 @@ EXPORT
 
  ( Example:
 New-Queue VALUE q
-10 5 q Enterly
+10 5 q Enterly  \ value pr queue -- 
 11 7 q Enterly
 12 3 q Enterly
-q .queue
+13 7 q Enterly
+q queue.
+q :NONAME DROP . ; mapQueue CR
+q Queue-Count . CR
 q LeaveLow . . CR
 q LeaveLow . . CR
 q LeaveLow . . CR
+q LeaveLow . . CR
+q LeaveLow . CR
 q Del-Queue
 \ )
