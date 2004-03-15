582f6e4fb8d7d09b3b53207e016cd587911c03d2
spf <>
2004-03-15 18:45:26 +0000

    reconnect (~ruv)

--- devel/~ac/lib/win/odbc/odbc2.f
+++ devel/~ac/lib/win/odbc/odbc2.f
@@ -20,30 +20,59 @@ VARIABLE DescriptionLen
   ServerName ServerNameLen @ TYPE ."  | "
   Description DescriptionLen @ TYPE CR
 ;
-: ConnectFile ( S" connect string" fodbc -- ior )
+: ConnectFile ( S" connect string" fodbc -- sql_ior )
 \ 09.01.2003: в параметре Driver= имя драйвера должно точно совпадать с тем,
-\ что написано в ODBC control panel, вплоть до пробелов. 
+\ что написано в ODBC control panel, вплоть до пробелов.
 \ В примерах MS часты опечатки!, осторожнее :)
   { fa fu fodbc \ sl2p mem }
+  fa fu fodbc odbcDrv 2!
+
   2048 ALLOCATE THROW -> mem
-  ( SQL_DRIVER_NOPROMPT) 0 ^ sl2p 2048 mem fu fa 
+  ( SQL_DRIVER_NOPROMPT) 0 ^ sl2p 2048 mem fu fa
   ( S" RichEdit20A" SPF_STDEDIT 0 Window ) 0
   fodbc odbcConn @
   SQLDriverConnect
 \ mem sl2p TYPE CR
   mem FREE THROW
 ;
-: ListTables ( fodbc -- ior )
+
+WARNING @ WARNING 0!
+
+: ReconnectSQL { fodbc -- ior }
+  fodbc odbcDrv 2@ DUP
+  IF  fodbc odbcConn @ SQLDisconnect DROP
+      fodbc ConnectFile
+  ELSE 2DROP
+      fodbc ReconnectSQL
+  THEN
+;
+
+WARNING  !
+
+: ListTables ( fodbc -- sql_ior )
   { fodbc }
   fodbc odbcStat  fodbc odbcConn @  SQLAllocStmt
   DUP SQL_OK?
   IF DROP
      S" TABLE,VIEW" SWAP  0 0 0 0 0 0
      fodbc odbcStat @ SQLTables
-     fodbc ResultCols fodbc odbcResultCols !
-     fodbc AffectedRows fodbc odbcAffectedRows !
-     fodbc IndexResultCols
-     fodbc RowSize fodbc odbcRowSize !
-     fodbc BindCols
+     fodbc cash-odbc-params
   THEN
+;
+: ExistTable ( a u fodbc -- flag sql_ior )
+  { a u fodbc }
+  fodbc odbcStat fodbc odbcConn @  SQLAllocStmt
+  DUP SQL_OK?
+  IF DROP
+     S" TABLE,VIEW" SWAP  u a  0 0 0 0
+     fodbc odbcStat @ SQLTables ( sql_ior ) \ is SQLRETURN
+     fodbc cash-odbc-params
+
+     fodbc NextRow SWAP
+  ELSE
+     FALSE SWAP
+  THEN
+;
+: EndSqlTrans { fodbc -- sql_ior }
+  0 ( SQL_COMMIT) fodbc odbcEnv @ 1 ( SQL_HANDLE_ENV) SQLEndTran
 ;
