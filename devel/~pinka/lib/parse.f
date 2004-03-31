c3704364106e5a8604506dd5f60a29d35a26388f
ruv <>
2004-03-31 01:12:54 +0000

    + NextSubstring ...

--- devel/~pinka/lib/parse.f
+++ devel/~pinka/lib/parse.f
@@ -1,4 +1,4 @@
-
+\ 2001
 
 : ParseFileName ( -- a u )
 \ разобрать им€ файла  из входного потока. 
@@ -7,4 +7,31 @@
     BL SKIP
     SOURCE DROP >IN @ + C@   [CHAR] " = IF [CHAR] " DUP SKIP ELSE BL THEN
     PARSE  2DUP + 0 SWAP C!
+;
+
+\ 31.Mar.2004 
+
+: IsCharSubs ( c -- f )
+  DUP [CHAR] " <> IF
+  DUP [CHAR] ' <> IF
+  DROP FALSE EXIT THEN 
+                  THEN
+  DROP TRUE
+;
+: NextSubstring ( <char1>ccc<char2> -- addr u)
+  SkipDelimiters
+  GetChar               IF
+  DUP IsCharSubs IF
+  >IN 1+!        ELSE
+  DROP BL        THEN
+  PARSE  EXIT           THEN
+  0 ( -- 0 0 )
+;
+: NextSubstring2 ( <char1>ccc<char2> -- addr u)
+\ отдает подстроку вместе с обрамл€ющими кавычками
+  NextSubstring DUP IF
+  OVER 1- C@
+  IsCharSubs IF 
+  SWAP 1- SWAP 2+ 
+  THEN              THEN
 ;
