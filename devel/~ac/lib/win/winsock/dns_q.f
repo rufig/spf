1a0543191d0113cbf826a545fa11256a935494a4
spf <>
2004-12-16 15:15:05 +0000

    Начинаем с первого сервера.

--- devel/~ac/lib/win/winsock/dns_q.f
+++ devel/~ac/lib/win/winsock/dns_q.f
@@ -272,7 +272,8 @@ CONSTANT /RL
 : SendDnsQuery
   DNS-SERVER @ 0= 
   IF GetDNS ?DUP 
-            IF DNS-COUNT @ 1 > IF COUNT + 1+ THEN DNS-SERVER !
+            IF DNS-SERVER !
+               \ DNS-COUNT @ 1 > IF COUNT + 1+ THEN DNS-SERVER !
             ELSE -11001 THROW THEN \ не найден DNS-сервер
   THEN
   BS @ 0= IF BsStartup THEN
