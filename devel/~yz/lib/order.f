e9a2ddbf9103eff2897e27d9e403b310dd351f5f
spf <>
2005-07-04 06:59:14 +0000

    WORDLIST:

--- devel/~yz/lib/order.f
+++ devel/~yz/lib/order.f
@@ -4,3 +4,11 @@
 : >ORDER ( wid -- ) ALSO CONTEXT ! ;
 : ORDER@ ( -- wid) CONTEXT @ ;
 : ORDER> ( -- wid) ORDER@ PREVIOUS ;
+
+: WORDLIST: ( ->bl; -- n)
+  WORDLIST DUP CONSTANT GET-CURRENT SWAP SET-CURRENT
+;
+
+: WORDLIST; ( n -- )
+  SET-CURRENT
+;
