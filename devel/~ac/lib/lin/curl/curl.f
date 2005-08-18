351cf5391a1b0f7933db2489a9a8ddf2098060c3
spf <>
2005-08-18 16:55:12 +0000

    GET_FILE вместо HTTP_GET, т.к. поддерживается и FTP.

--- devel/~ac/lib/lin/curl/curl.f
+++ devel/~ac/lib/lin/curl/curl.f
@@ -22,10 +22,10 @@ CURLOPT_FILE CONSTANT CURLOPT_WRITEDATA
   stream nmemb size ptr asize
 ; QUICK_WNDPROC CURL_CALLBACK
 
-: HTTP_GET_VIAPROXY { addr u paddr pu \ h data -- str }
+: GET_FILE_VIAPROXY { addr u paddr pu \ h data -- str }
 \ если прокси paddr pu - непустая строка, то явно используется этот прокси
 \ curl умеет использовать переменные окружения http_proxy, ftp_proxy
-\ поэтому можно не задавать прокси явно
+\ поэтому можно не задавать прокси явно.
 
   "" -> data
   0 curl_easy_init -> h
@@ -41,15 +41,15 @@ CURLOPT_FILE CONSTANT CURLOPT_WRITEDATA
   h 1 curl_easy_cleanup DROP
   data
 ;
-: HTTP_GET ( addr u -- str )
+: GET_FILE ( addr u -- str )
   \ без прокси или с заданным в переменной окружения http_proxy
-  S" " HTTP_GET_VIAPROXY
+  S" " GET_FILE_VIAPROXY
 ;
 
 \EOF
 : TEST
-  S" http://xmlsearch.yandex.ru/xmlsearch?query=sp-forth" HTTP_GET STYPE
-  S" http://xmlsearch.yandex.ru/xmlsearch?query=sp-forth" S" http://otradnoe:3128/" HTTP_GET_VIAPROXY STYPE
-  S" ftp://ftp.forth.org.ru/" HTTP_GET STYPE
+  S" http://xmlsearch.yandex.ru/xmlsearch?query=sp-forth" GET_FILE STYPE
+  S" http://xmlsearch.yandex.ru/xmlsearch?query=sp-forth" S" http://otradnoe:3128/" GET_FILE_VIAPROXY STYPE
+  S" ftp://ftp.forth.org.ru/" GET_FILE STYPE
 ;
 TEST
