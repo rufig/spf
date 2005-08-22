11c5bd41bb1f8a9a1dc71e2a70518351ba758d72
spf <>
2005-08-22 22:36:53 +0000

    Хак для удаления явнозаданных в файле кодировок, которые не меняются при перекодировках файла целиком.

---
+++ devel/~ac/lib/lin/tidy/delxmldecl.f
@@ -0,0 +1,42 @@
+REQUIRE BLANK         lib/include/string.f 
+REQUIRE STR@          ~ac/lib/str5.f
+
+: DelXmlDecl { a u \ len -- a u }
+  a u
+  BEGIN
+    S" <?xml version='1.0' encoding='windows-1251'?>" DUP -> len
+    SEARCH
+  WHILE
+    OVER len BLANK
+    len - 0 MAX SWAP len + SWAP
+  REPEAT
+  2DROP
+  a u
+  BEGIN
+    S" <?xml version='1.0' encoding='windows-1251' ?>" DUP -> len
+    SEARCH
+  WHILE
+    OVER len BLANK
+    len - 0 MAX SWAP len + SWAP
+  REPEAT
+  2DROP
+  a u
+  BEGIN
+    S' <META http-equiv="Content-Type" content="text/html; charset=UTF-16">' DUP -> len
+    SEARCH 
+  WHILE
+    OVER len BLANK
+    len - 0 MAX SWAP len + SWAP
+  REPEAT
+  2DROP
+  a u
+  BEGIN
+    S' <?xml version="1.0" encoding="UTF-16"?>' DUP -> len
+    SEARCH 
+  WHILE
+    OVER len BLANK
+    len - 0 MAX SWAP len + SWAP
+  REPEAT
+  2DROP
+  a u
+;
