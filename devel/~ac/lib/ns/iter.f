2b1dd72ad944e6e75152ec4d7f7db35fe185d1a0
spf <>
2005-09-14 13:51:36 +0000

    Передача "полного/пути" к словарю в XT в ForEachDirWRstr.

---
+++ devel/~ac/lib/ns/iter.f
@@ -0,0 +1,22 @@
+REQUIRE ForEachDirWR ~ac/lib/ns/iterators.f
+REQUIRE STR@         ~ac/lib/str5.f
+
+: ForEachDirWRstr { str xt wid \ s -- }
+\ xt: ( item wid -- )
+  wid CAR
+  BEGIN
+    DUP
+  WHILE
+    DUP wid W?VOC
+    IF 
+       DUP wid WNAME str STR@ ?DUP IF " {s}/{s}" ELSE DROP " {s}" THEN -> s
+       DUP s xt ROT wid ITEM>WID RECURSE
+       DUP s SWAP wid xt EXECUTE s STRFREE
+    THEN
+    wid WCDR
+  REPEAT DROP
+;
+: swid. ( str item wid -- ) WNAME TYPE ." :" STR@ TYPE CR ;
+
+\ " FORTH" ' swid. FORTH-WORDLIST ForEachDirWRstr
+
