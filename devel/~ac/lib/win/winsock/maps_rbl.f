34b57cd0ea01365bde49cf5137f24f88334fa9dd
spf <>
2005-10-18 14:37:24 +0000

    Добавлены примеры.

--- devel/~ac/lib/win/winsock/maps_rbl.f
+++ devel/~ac/lib/win/winsock/maps_rbl.f
@@ -18,12 +18,15 @@ REQUIRE CreateSocket ~ac/lib/win/winsock/sockets.f
 : IsOrdbBlockedRelay ( ip -- flag )
   S" relays.ordb.org" IsRblBlocked
 ;
+
 (
+: IsRblBlocked 2DUP TYPE SPACE IsRblBlocked ;
 : TEST
   SocketsStartup . S" 192.203.178.39" GetHostIP THROW
   IsMapsBlockedRelay .
   CR CR
   S" 80.254.168.54" GetHostIP THROW IsOrdbBlockedRelay . CR
+  S" 85.21.86.207" GetHostIP THROW S" cbl.abuseat.org" IsRblBlocked . CR
   S" 127.0.0.2" GetHostIP THROW S" proxies.relays.monkeys.com" IsRblBlocked . CR
   S" 127.0.0.2" GetHostIP THROW S" list.dsbl.org" IsRblBlocked . CR
   S" 127.0.0.2" GetHostIP THROW S" dnsbl.njabl.org" IsRblBlocked . CR
