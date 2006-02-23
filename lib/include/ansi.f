959278e000978461545c3f79614c9bd669337a48
ruv <>
2006-02-23 18:45:38 +0000

    совместимость к ANS from Ilya S. Potrepalov

---
+++ lib/include/ansi.f
@@ -0,0 +1,125 @@
+\ $Id$
+
+\ Originally
+\ From: "Ilya S. Potrepalov" <potrepalov@asc-ural.ru>
+\ Date: Thu, 23 Feb 2006 13:55:01 +0000 (UTC)
+\ Newsgroups: fido7.su.forth
+\ Message-ID: <dtkera$r89$93914@www.fido-online.com>
+
+\ ASNI'фикация SP-FORTH'а
+
+BASE @ DECIMAL
+
+REQUIRE CASE         LIB\EXT\CASE.F
+REQUIRE /STRING      LIB\INCLUDE\STRING.F
+REQUIRE [IF]         LIB\INCLUDE\TOOLS.F
+REQUIRE SAVE-INPUT   LIB\INCLUDE\CORE-EXT.F
+REQUIRE RENAME-FILE  DEVEL\~PINKA\LIB\FileExt.f
+REQUIRE D0<          lib\include\double.f 
+
+WARNING @  0 WARNING !
+
+DECIMAL
+
+: ?DUP  ?DUP ;  \ ?DUP в SP-FORTH'е state-smart, а это не по стандарту
+
+: CONVERT  ( ud1 c-addr1 -- ud2 c-addr2 )
+    \ from gforth
+    CHAR+ TRUE >NUMBER DROP
+;
+
+VARIABLE SPAN  0 SPAN !
+: EXPECT  ( a u -- )
+    \ это лучше, чем ничего
+    ACCEPT SPAN !
+;
+
+: D.R  ( d +n -- )
+    >R DUP >R  DABS  <# #S R> SIGN #>
+    R> OVER - 0 MAX SPACES  TYPE
+;
+
+\ DPANS 3.4.1.1 Delimiters
+\
+\ If the delimiter is the _space_ _character_, hex 20 (BL), control
+\ characters may be treated as delimiters. The set of conditions,
+\ if any, under which a space delimiter matches control characters
+\ is implementation defined. 
+\
+\
+\ DPANS 11.3.6 Parsing:
+\
+\ When parsing from a text file using a space delimiter, 
+\ control characters shall be treated the same as the space character.
+\
+: IS-DELIM  ( c delim -- f )
+    DUP BL =
+    IF
+        SOURCE-ID 0>
+        IF
+            DROP BL 1+ <
+            EXIT
+        THEN
+    THEN
+    =
+;
+
+: PARSE  ( "word<c>" c -- c-addr u )
+    >R  CharAddr 0
+    BEGIN
+        GetChar
+    WHILE
+        >IN 1+!
+        R@ IS-DELIM 0=
+    WHILE
+        1+
+    REPEAT
+    0
+    THEN
+    DROP R> DROP
+;
+
+: SKIP  ( "<cccc>" c -- )
+    BEGIN   GetChar
+    WHILE   OVER IS-DELIM
+    WHILE   >IN 1+!
+    REPEAT  0
+    THEN
+    2DROP
+;
+
+DECIMAL
+: WORD  ( c "<cccc>word<c>" -- c-addr )
+    DUP SKIP  PARSE 255 MIN
+    DUP PAD C!
+    DUP 1+ CHARS PAD +  BL SWAP C!
+    PAD CHAR+ SWAP CMOVE
+    PAD
+;
+    
+
+\ ANSI said: "If a system provides any standard word for accessing
+\ mass storage, it shall also implement the Block word set".
+\
+\ BLOCK word set
+\
+\ Table 9.2 - THROW code assignments
+\
+\ -35     invalid block number
+
+VARIABLE BLK  0 BLK !
+: BLOCK   -35 THROW ;
+: BUFFER  -35 THROW ;
+: FLUSH ;
+: LOAD    -35 THROW ;
+: SAVE-BUFFERS ;
+: UPDATE ;
+
+: BIN ;
+: FILE-STATUS  2DROP  0 -21 ; \ unsupported operation
+
+
+WARNING !
+BASE !
+
+\ END OF FILE
