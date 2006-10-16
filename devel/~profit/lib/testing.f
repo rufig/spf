0979396771e2c56195bf2d229d055b3acbb285a8
azekeprofit <>
2006-10-16 13:06:52 +0000

    correction

--- devel/~profit/lib/testing.f
+++ devel/~profit/lib/testing.f
@@ -1,3 +1,23 @@
 VARIABLE TESTING
 : /TEST TESTING @ 0= IF \EOF THEN ;
 TESTING 0!
+
+\EOF
+VOCABULARY tests:
+           ALSO tests DEFINITIONS
+: ;test PREVIOUS ;
+: NOTFOUND 2DROP ;
+ PREVIOUS DEFINITIONS
+
+\EOF
+REQUIRE (: ~yz/lib/inline.f
+
+: r
+&INTERPRET @
+(:
+BEGIN \ интерпретировать входной поток со знаками препинания
+NextWord DUP         WHILE 
+S" bla" COMPARE 0= WHILE
+?STACK               REPEAT THEN ;)
+ &INTERPRET !
+2DROP ;
