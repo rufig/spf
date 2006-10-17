9a3cb5b062a705322387c63f5fc94bf754b29857
forther <>
2006-10-17 10:23:34 +0000

    Added test of PARSE-NAME

--- samples/ans/tester.f
+++ samples/ans/tester.f
@@ -1036,4 +1036,24 @@ TESTING DICTIONARY SEARCH RULES
 
 { GDX -> 123 234 }
 
+\ ------------------------------------------------------------------------
+TESTING PARSE-NAME
 
+{ PARSE-NAME abcd S" abcd" COMPARE -> 0 }
+{ PARSE-NAME   abcde   S" abcde" COMPARE -> 0 }
+\ test empty parse area
+{ PARSE-NAME
+  NIP -> 0 }
+{ PARSE-NAME   
+  NIP -> 0 }
+
+{ : parse-name-test ( "name1" "name2" -- n )
+    PARSE-NAME PARSE-NAME COMPARE ; -> }
+{ parse-name-test abcd abcd -> 0 }
+{ parse-name-test  abcd   abcd   -> 0 }
+{ parse-name-test abcde abcdf -> -1 }
+{ parse-name-test abcdf abcde -> 1 }
+{ parse-name-test abcde abcde
+  -> 0 }
+{ parse-name-test abcde abcde  
+  -> 0 }
