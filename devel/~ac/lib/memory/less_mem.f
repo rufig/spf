5b09e2208f795d13d0b5d5d854dfc9ea3e2ad702
spf <>
2006-10-17 12:07:41 +0000

    Ticket #15.

--- devel/~ac/lib/memory/less_mem.f
+++ devel/~ac/lib/memory/less_mem.f
@@ -1,9 +1,9 @@
 \ idea of Svyatoslav Ushakov
 
 REQUIRE WinNT?            ~ac/lib/win/winver.f
+REQUIRE GetCurrentProcess ~ac/lib/win/process/pipes.f
 
 WINAPI: SetProcessWorkingSetSize KERNEL32.DLL
-\ WINAPI: GetCurrentProcess KERNEL32.DLL
 
 : ReduceMem
   WinNT?
