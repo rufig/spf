a65f5f9f75d6b9d996b502cde18cc60f116f5427
ygreks <>
2006-10-22 14:52:30 +0000

    reverted. So, leave it in src or move to lib? Decide!

--- src/compiler/spf_modules.f
+++ src/compiler/spf_modules.f
@@ -4,18 +4,15 @@
 
 : MODULE: ( "name" -- old-current )
 \ start a forth module
-   VOCABULARY GET-CURRENT
-   ALSO LATEST NAME> EXECUTE DEFINITIONS
-;
-
-: EXTEND-MODULE: ( "name" -- old-current )
 \ Если такой модуль уже существует, продолжить компиляцию в него
-  ' GET-CURRENT SWAP ALSO EXECUTE DEFINITIONS
-;
+  >IN @ 
+  ['] ' CATCH
+  IF >IN ! VOCABULARY LATEST NAME> ELSE NIP THEN
+  GET-CURRENT SWAP ALSO EXECUTE DEFINITIONS ;
 
 : EXPORT ( old-current -- old-current )
 \ export some module definitions
-   DUP SET-CURRENT
+  DUP SET-CURRENT 
 ;
 
 : ;MODULE ( old-current -- )
