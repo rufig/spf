7cd2e1264ea72e364d8cb7fbb7be3398597f0631
ygreks <>
2006-10-26 09:51:25 +0000

    + ?VOC checking in MODULE:

--- src/compiler/spf_modules.f
+++ src/compiler/spf_modules.f
@@ -3,12 +3,23 @@
 )
 
 : MODULE: ( "name" -- old-current )
-\ start a forth module
-\ Если такой модуль уже существует, продолжить компиляцию в него
-  >IN @ 
-  ['] ' CATCH
-  IF >IN ! VOCABULARY LATEST NAME> ELSE NIP THEN
-  GET-CURRENT SWAP ALSO EXECUTE DEFINITIONS ;
+\ Start a forth module
+\ If "name" is already defined: 
+\ check whether it is a vocabulary and continue compiling new words in this voc
+\ else THROW error
+   PARSE-NAME 
+   2DUP CONTEXT @ SEARCH-WORDLIST-NFA
+   IF
+     NIP NIP
+     DUP ?VOC 0= IF DROP -2011 THROW THEN  \ "unknown voc"
+   ELSE
+     ['] VOCABULARY EVALUATE-WITH
+     LATEST 
+   THEN
+   NAME> 
+   \ START-MODULE
+   GET-CURRENT SWAP ALSO EXECUTE DEFINITIONS
+;
 
 : EXPORT ( old-current -- old-current )
 \ export some module definitions
