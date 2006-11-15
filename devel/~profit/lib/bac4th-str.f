887721ec1a48d3134e16c9e2c416390c8cbe5598
azekeprofit <>
2006-11-15 18:28:17 +0000

    no message

--- devel/~profit/lib/bac4th-str.f
+++ devel/~profit/lib/bac4th-str.f
@@ -5,6 +5,8 @@
 
 REQUIRE /TEST ~profit/lib/testing.f
 REQUIRE CONT ~profit/lib/bac4th.f
+REQUIRE FREEB ~profit/lib/bac4th-mem.f
+REQUIRE compiledCode ~profit/lib/bac4th-closures.f
 REQUIRE STR@ ~ac/lib/str4.f
 
 MODULE: bac4th-str
@@ -17,17 +19,8 @@ MODULE: bac4th-str
 
 0 VALUE previousAddress
 
-
-VECT compare \ функция сравнения для find может быть любая у которой стек ( a b -- f )
-\ ' EXECUTE TO compare
-\ может быть попробовать? И тогда как параметр c надо будет указывать не символ, а 
-\ адрес функции сравнения со стеком ( a -- f ) ...
-
 : concat-sum ( addr u var -- ) @ STR+ ;
 : concat-suc ( var <--> s ) @  R> ENTER  STRFREE ;
-
-:NONAME ( a -- f ) DUP 10 = SWAP 13 = OR ; CONSTANT isSpaceBeforeWord
-:NONAME ( a -- f ) DUP 10 = SWAP 13 = OR ; CONSTANT isCRLF
 
 EXPORT
 
@@ -39,24 +32,25 @@ t l + e MIN
 t - t SWAP
 >STR CONT STRFREE ;
 
-: char-compare ['] = TO compare ;
-: func-compare ['] EXECUTE TO compare ;
-\ их надо ставить при каждой итерации цикла split (ошибка!)
-char-compare
-
+: byChar ( c <--> xt )  PRO S" LITERAL =" compiledCode FREEB CONT ;
+\ : byRows ( <--> xt ) PRO S" 2* 23 - ABS 3 ="  compiledCode CONT ;
+\ : byRows ( -- xt ) (: 2* 23 - ABS 3 = ;) ;
+:NONAME 2* 23 - ABS 3 = ; \ 13 или 10
+VALUE byRows 
 
 : find ( a u c <--> a1 )
 \ находит в строке a u все символы c и генерирует вызовы для каждого символа
 PRO { a u c -- } CR
-a u + a ?DO I C@ c compare IF I CONT DROP THEN LOOP ;
+a u + a ?DO I C@ c EXECUTE IF I CONT DROP THEN LOOP ;
 
 : divide ( a u c <--> s1 )
 \ разбивает строку a u первым символом c и генерирует два вызова со строками
 \ перед символом и после с автоматическим выделением и снятием памяти
 PRO { a u c -- }
-a u c CUT: find -CUT
+a u c PREDICATE find SUCCEEDS IF \ если есть искомый символ, то генерируем две строки
 DUP a - a SWAP >STR CONT STRFREE
-1+ a u + OVER - >STR CONT STRFREE ;
+1+ a u + OVER - >STR CONT STRFREE
+ELSE a u >STR CONT STRFREE THEN ; \ если нету -- одну
 
 : split ( a u c <--> s1  )
 \ разбивает строку a u символами c и генерирует вызов для каждой последовательности
@@ -81,51 +75,51 @@ previousAddress - previousAddress SWAP >STR CONT STRFREE \ обработаем и последне
 PRO FILE CONT DROP FREE THROW ;
 
 : iterateStrings ( addr u <--> s ) PRO \ пускаем цикл по строкам файла
-load-file 2DUP func-compare isCRLF ( функция проверки на перевод строки)
+load-file 2DUP byRows ( функция проверки на перевод строки)
 split notEmpty CONT ;
 
 ;MODULE
 
 /TEST
-: r
-S" sss" [CHAR] a divide DUP STR@ TYPE ; r
+: e S" forth" 3 2 copy DUP STR@ TYPE ; CR e
 
-: e S" mary434" 1 1000 copy DUP STR@ TYPE ; CR e
-
-: r S" mary has sheep" [CHAR] a find DUP C@ EMIT ;
+: r S" mary has sheep" [CHAR] a byChar find DUP C@ EMIT ;
 \ находим все символы 'a'
 \ CR r
 
-: r2 S"  mary  has a  sheep" BL split notEmpty ."  [" DUP STR@ TYPE ." ]" ;
+: r2 S"  mary  has a  sheep" BL byChar split notEmpty ."  [" DUP STR@ TYPE ." ]" ;
 \ делим на слова. Каждое слово -- не отрезок из главной строки, он перенесён в кучу 
 \ и автоматически при отходе назад из кучи снимается
 \ вывод: [mary] [has] [a] [sheep]
  CR r2
 
-: r3 S" antigua labrador abracadabra"  BL split DUP STR@ [CHAR] a split notEmpty ."  [" DUP STR@ TYPE ." ]" ;
+: r3 S" antigua labrador abracadabra"  BL byChar split DUP STR@ [CHAR] a byChar split notEmpty ."  [" DUP STR@ TYPE ." ]" ;
 \ делим на слова и на отрезки между буквам 'a' в словах.
 \ вывод: [ntigu] [l] [br] [dor] [br] [c] [d] [br]
 CR r3
 
-: r4 CUT: S" antigua labrador abracadabra"  BL split -CUT ."  [" DUP STR@ TYPE ." ]" ;
+: r4 S" antigua labrador abracadabra"  BL byChar CUT: split -CUT ."  [" DUP STR@ TYPE ." ]" ;
 \ только первая сгенерированная последовательность
 \ вывод: [antigua]
-\ внимание, в этом примере на стеке остаётся мусор
-CR r4
+\ внимание, в этом примере на стеке остаётся мусор, не говоря уже о том что он протекает по памяти
+\ так как CUT убирает освобождение памяти занятой внутри split со стека возвратов
+\ CR r4
 
 : r7 concat{ *> concat{ *> S" 2" <*> S" *2" <* }concat DUP STR@ <*> S" =4" <*> S" ?" <* }concat DUP STR@ TYPE ;
 CR r7
 
 \ вывод: 2*2=4?
 
-: r8 concat{ S"     mary  has  a  sheep  " BL split notEmpty DUP STR@ }concat ."  [" DUP STR@ TYPE ." ]" ;
+: r8 concat{ S"     mary  has  a  sheep  " BL byChar split notEmpty DUP STR@ }concat ."  [" DUP STR@ TYPE ." ]" ;
 \ убирает все пробелы в строке
 \ вывод: [maryhasasheep]
  CR r8
 
-: r9 S" a1=123==456" [CHAR] = divide DUP ."  [" STR@ TYPE ." ]" ;
+: r9 S" a1=123==456" [CHAR] = byChar divide DUP ."  [" STR@ TYPE ." ]" ;
 \ разбивает строку на две части
 \ вывод: [a1] [123==456]
-CR r9
+\ и снова CUT оставляет мусор
+\ CR r9
 
 : printFile  S" C:\lang\spf\devel\~profit\lib\bac4th-str.f" iterateStrings DUP STR@ CR TYPE ;
+\ вывод файла на печать
