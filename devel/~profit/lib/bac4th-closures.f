09cd9acaf87de6bd556ad6646a0470c9134dd364
azekeprofit <>
2006-11-17 12:44:37 +0000

    refactored

--- devel/~profit/lib/bac4th-closures.f
+++ devel/~profit/lib/bac4th-closures.f
@@ -1,27 +1,15 @@
 \ REQUIRE MemReport ~day/lib/memreport.f
-REQUIRE CONT ~profit/lib/bac4th-mem.f 
-REQUIRE FREEB ~profit/lib/bac4th-mem.f 
+REQUIRE CONT ~profit/lib/bac4th.f 
+REQUIRE FREEB ~profit/lib/bac4th-mem.f
+REQUIRE EVALUATED-HEAP ~profit/lib/evaluated.f
 
-: compiledCode ( addr u -- addr-code )  HERE >R
-TRUE STATE ! \ включаем компиляцию
-EVALUATE \ выполняем строку-параметр
-RET, \ заканчиваем определение
-STATE 0! \ выключаем компиляцию
-HERE \ текущее значение HERE, после компиляции
-R@ DP ! \ восстанавливаем HERE к изначальному значению
-R@ \ старое значение HERE, перед ней
-( новое старое )
-- ( новое-старое ) \ подсчитываем длину скомпилированной последовательности
-R> OVER ( длина старое длина )
-ALLOCATE THROW
-DUP >R
-( длина старое выделенная-память )
-ROT CMOVE R> ( адрес-участка-кода-в-куче ) ;
+: compiledCode ( addr u -- addr-code )
+PRO EVALUATED-HEAP FREEB CONT ;
 
 \EOF
 REQUIRE SEE lib/ext/disasm.f
 
-: s ( n -- ) S" LITERAL +" compiledCode FREEB REST ;
+: s ( n -- ) S" LITERAL +" compiledCode REST ;
 
 : r 4 0 DO CR CR I . I s LOOP ;
 r
