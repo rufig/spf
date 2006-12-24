2876d5c3d57e44c0ad3c0046f7fa9b89c9154de7
ruv <>
2006-12-24 18:05:43 +0000

    need for storage.f, was missed

---
+++ devel/~pinka/spf/enum-vocs.f
@@ -0,0 +1,22 @@
+\ 23.Dec.2006 Sat 21:59
+
+\ from quick-swl3.f
+
+: ENUM-VOCS ( xt -- )
+\ xt ( wid -- )
+  >R VOC-LIST @ BEGIN DUP WHILE
+    DUP CELL+ ( a wid ) R@ ROT >R
+    EXECUTE R> @
+  REPEAT DROP RDROP
+;
+
+: ENUM-FORTH-VOCS ( xt -- )
+\ перебор только обычных форт-словарей (у которых CLASS равен 0 или FORTH-WORDLIST )
+\ xt ( wid -- )
+  >R VOC-LIST @ BEGIN DUP WHILE
+    DUP CELL+ ( a wid ) 
+    DUP CLASS@ DUP 0= SWAP FORTH-WORDLIST = OR
+    IF R@ ROT >R  EXECUTE R> ELSE DROP THEN
+    @
+  REPEAT DROP RDROP
+;
