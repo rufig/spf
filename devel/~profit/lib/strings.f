d9af8b01f20f62dbf0708ecea0350fee5106b0ef
azekeprofit <>
2007-01-07 19:10:35 +0000

    robustness for len<=0

--- devel/~profit/lib/strings.f
+++ devel/~profit/lib/strings.f
@@ -1,3 +1,3 @@
-: lastChar ( addr len --> addr len-1 lastCh ) 1- 2DUP + C@ ;
+: lastChar ( addr len --> addr len-1 lastCh ) 1 CHARS - 0 MAX 2DUP + C@ ;
 : firstChar ( addr len --> addr len firstCh ) OVER C@ ;
-: restOfString ( addr len --> addr+1 len-1 ) SWAP 1+ SWAP 1- ;
+: restOfString ( addr len --> addr+1 len-1 ) DUP 0 > IF SWAP 1 CHARS + SWAP 1 CHARS - THEN ;
