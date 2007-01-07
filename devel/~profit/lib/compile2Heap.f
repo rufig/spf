88e58bce0902fa0cefe0f016707baa946e81978d
azekeprofit <>
2007-01-07 19:12:32 +0000

    renaming

--- devel/~profit/lib/compile2Heap.f
+++ devel/~profit/lib/compile2Heap.f
@@ -2,19 +2,19 @@
 \ Куски сцепляются jmp'ами.
 
 \ Создание виртуального кодофайла:
-\ <размер-кусков> _CREATE-CP ( n -- cp )
+\ <размер-кусков> _CREATE-VC ( n -- v_codefile )
 
 \ Или со значением по-умолчанию (по 4Кб):
-\ CREATE-CP ( -- cp )
+\ CREATE-VC ( -- v_codefile )
 
-\ CP-, CP-COMPILE, CP-LIT, CP-RET, аналогичны
+\ VC-, VC-COMPILE, VC-LIT, VC-RET, аналогичны
 \ ,    COMPILE,    LIT,    RET,
 \ только требуют дополнительный параметр -- cp
 
-\ Кроме того, слово CP- ( cp -- ) переключает все компилирующие 
-\ слова, идущие после него, на виртуальный кодофайл cp в пределах определения
+\ Кроме того, слово VC- ( vc -- ) переключает все компилирующие 
+\ слова, идущие после него, на виртуальный кодофайл vc в пределах определения
 
-\ Все куски развязываются и освобождаются DESTROY-CP ( cp -- )
+\ Все куски развязываются и освобождаются DESTROY-VC ( vc -- )
 
 
 \ REQUIRE MemReport ~day/lib/memreport.f
@@ -22,7 +22,7 @@ REQUIRE LOCAL ~profit/lib/static.f
 REQUIRE CONT ~profit/lib/bac4th.f
 
 MODULE: codepatches
-20 CONSTANT luft  \ сколько байт допуска
+30 CONSTANT luft  \ сколько байт допуска
 
 : __ CELL -- ;
 
@@ -35,7 +35,11 @@ __ limit      \ временная переменная, куда записывается граничный
               \ адрес, после пересечения которого надо начинать новый кусок
 CONSTANT codePatches
 
-: allocatePatch ( cp -- ) >R R@ block @ ALLOCATE THROW
+VARIABLE counter
+counter 0!
+
+: allocatePatch ( vc -- ) >R R@ block @ ALLOCATE THROW
+\ counter 1+!  counter @ CR . \ для контроля забора и отдачи памяти
 DUP 0! DUP R@ lastBlock !
 CELL+ DUP R@ there ! 
 R@ block @ + luft - R@ limit ! RDROP ;
@@ -45,61 +49,63 @@ R@ block @ + luft - R@ limit ! RDROP ;
               \ текущим адресом оптимизатор что для этого 
               \ применения нежелательно
 
+
 EXPORT
-: _CREATE-CP ( blockSize -- cp )
+: _CREATE-VC ( blockSize -- vc )
 codePatches ALLOCATE THROW >R
 R@ block !
 R@ allocatePatch
 R@ lastBlock @ R@ firstBlock !
 R> ;
 
-: CREATE-CP (  -- cp ) 1024 CELLS _CREATE-CP ; \ по-умолчанию куски по 4Кб
+: CREATE-VC (  -- vc ) 1 12 LSHIFT _CREATE-VC ; \ по-умолчанию куски по 4Кб
 
-: DESTROY-CP ( cp -- )
+: DESTROY-VC ( vc -- )
 DUP firstBlock @ SWAP FREE THROW
 BEGIN DUP @ 
 SWAP FREE THROW
+\ -1 counter +!  counter @ CR . \ для контроля забора и отдачи памяти
 DUP 0= UNTIL DROP ;
 
-: XT-CP ( cp -- xt ) firstBlock @ CELL+ ;
+: XT-VC ( vc -- xt ) firstBlock @ CELL+ ;
 
-: CP- ( ... cp -- ) PRO LOCAL cp cp !
+: VC- ( ... vc -- ) PRO LOCAL vc vc !
 LOCAL savedHere HERE savedHere !
-cp @ there @ DP !
+vc @ there @ DP !
 CONT
-HERE cp @ there !
-HERE cp @ limit @ >  IF
-cp @ lastBlock @
-cp @ allocatePatch
-cp @ lastBlock @ SWAP !
-cp @ there @ BRANCH, THEN
+HERE vc @ there !
+HERE vc @ limit @ >  IF
+vc @ lastBlock @
+vc @ allocatePatch
+vc @ lastBlock @ SWAP !
+vc @ there @ BRANCH, THEN
 savedHere @ DP ! ;
 
-: CP-COMPILE, ( xt cp -- ) CP- COMPILE, ;
-: CP-, ( n cp -- ) CP- , ;
-: CP-LIT, ( n cp -- ) CP- LIT, ;
-: CP-DLIT, ( du cp -- ) >R SWAP R@ CP-LIT, R> CP-LIT, ;
-: CP-RET, ( cp -- ) CP- RET, ;
+: VC-COMPILE, ( xt vc -- ) VC- COMPILE, ;
+: VC-, ( n vc -- ) VC- , ;
+: VC-LIT, ( n vc -- ) VC- LIT, ;
+: VC-DLIT, ( du vc -- ) >R SWAP R@ VC-LIT, R> VC-LIT, ;
+: VC-RET, ( vc -- ) VC- RET, ;
 \ компиляция команд в виде строки в кучу:
-: CP-COMPILED ( addr u cp -- ) CP- TRUE STATE B! EVALUATE ;
+: VC-COMPILED ( addr u cp -- ) VC- TRUE STATE B! EVALUATE ;
 ;MODULE
 
 \EOF
 REQUIRE SEE lib/ext/disasm.f
 0 VALUE t
 : r 
-1000 CELLS CREATE-CP TO t
-0 t CP-LIT,
-4 0 DO ['] 1+ t CP-COMPILE, LOOP
-['] . t CP-COMPILE,
-S" dfdsdfdf" t CP-DLIT,
-['] CR t CP-COMPILE,
-['] TYPE t CP-COMPILE,
-['] CR t CP-COMPILE,
-S" 1 1 + ." t CP-COMPILED
-t CP-RET,
-t XT-CP REST ;
+1000 CELLS CREATE-VC TO t
+0 t VC-LIT,
+4 0 DO ['] 1+ t VC-COMPILE, LOOP
+['] . t VC-COMPILE,
+S" dfdsdfdf" t VC-DLIT,
+['] CR t VC-COMPILE,
+['] TYPE t VC-COMPILE,
+['] CR t VC-COMPILE,
+S" 1 1 + ." t VC-COMPILED
+t VC-RET,
+t XT-VC REST ;
 
 r
-t DESTROY-CP
+t DESTROY-VC
 \ MemReport
