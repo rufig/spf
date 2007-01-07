9835e5a421927144a56d1fd38e596779f8555897
azekeprofit <>
2007-01-07 19:15:12 +0000

    S>STR , divide-patch , first-patch

--- devel/~profit/lib/bac4th-str.f
+++ devel/~profit/lib/bac4th-str.f
@@ -6,8 +6,10 @@
 REQUIRE /TEST ~profit/lib/testing.f
 REQUIRE CONT ~profit/lib/bac4th.f
 REQUIRE FREEB ~profit/lib/bac4th-mem.f
+REQUIRE iterateByBytes ~profit/lib/bac4th-iterators.f
 REQUIRE compiledCode ~profit/lib/bac4th-closures.f
 REQUIRE STR@ ~ac/lib/str4.f
+REQUIRE { ~ac/lib/locals.f
 REQUIRE LOCAL ~profit/lib/static.f
 
 MODULE: bac4th-str
@@ -16,22 +18,21 @@ MODULE: bac4th-str
 : >RESOLVE2 ( dest -- ) HERE SWAP ! ;
 
 
-: >STR ( a u -- s ) "" DUP >R STR+ R> ;
-
-0 VALUE previousAddress
-
 : concat-sum ( addr u var -- ) @ STR+ ;
 : concat-suc ( var <--> s ) @  R> ENTER  STRFREE ;
 
 EXPORT
 
-: copy ( a u i l <--> s1  ) \ l=-1 ?
-PRO { a u i l \ e t -- }
+: S> ( a u -- s ) "" DUP >R STR+ R> ;
+: S>STR ( a u --> s ) PRO S> CONT STRFREE ;
+
+: copy-patch { a u i l \ e t -- a+i l }
 a u + TO e
 a i + e MIN TO t
 t l + e MIN
-t - t SWAP
->STR CONT STRFREE ;
+t - t SWAP ;
+
+: copy ( a u i l --> s \ <-- s ) PRO copy-patch S>STR CONT ;
 
 \ Генерирует функцию сравнивающую на равенство с числом
 : byChar ( c <--> xt )  PRO S" LITERAL =" compiledCode CONT ;
@@ -45,32 +46,52 @@ CONSTANT byRows
 : find ( a u f <--> a1 )
 \ находит в строке a u все символы, на которых функция f даст TRUE и генерирует вызовы для каждого символа
 \ Функция f ( с -- 0|-1 ) получает на входе значение символа и выводит логическое значение
-PRO { a u f -- }
-a u + a ?DO I C@ f EXECUTE IF I CONT DROP THEN LOOP ;
+PRO LOCAL f f !
+iterateByBytes DUP C@ f @ EXECUTE IF CONT THEN ;
 
 : split-patch ( a u f <--> addr u  )
 \ разбивает строку a u символами, на которых функция f даст TRUE и генерирует 
 \ вызов для каждого *отрезка* в строке a u
-PRO { a u f -- }
-a TO previousAddress
-a u + a u f
+PRO LOCAL f f !
+OVER LOCAL p p ! \ p -- пред. отметка, перед запуском цикла равна началу строки
+2DUP + LOCAL e e ! \ последний символ, для передачи последнего отрезка
 START{
-find
-DUP previousAddress - previousAddress SWAP CONT 2DROP
-DUP 1+ TO previousAddress }EMERGE
-previousAddress - previousAddress SWAP CONT 2DROP \ обработаем и последний отрезок, не кончающийся на char
+f @ find ( addr ) \ получаем адреса где искомые символы
+DUP p @ -  p @ SWAP CONT 2DROP \ от предыдущего до текущего найденного символа строку выводим
+DUP CHAR+ p ! }EMERGE
+
+p @  e @ p @ - CONT 2DROP \ обработаем и последний отрезок, не кончающийся на char
 ;
+
+: first-patch ( a u f <--> addr u ) LOCAL f f !
+LOCAL len  DUP len !
+LOCAL start  OVER start !
+START{
+S| CUT: f @ split-patch len ! start ! -CUT
+}EMERGE
+2DROP      \ S| восстанавливает два значения на стеке, как было до запуска S| , нужно их убрать
+start @ len @ ; \ чтобы вытащить из "глубины" взятые нами значения
+\ заметьте: это слово не является генератором, это обычное слово
+
+: first ( a u f <--> s ) PRO first-patch S>STR CONT ;
+
+: divide-patch ( a u f -- addr1 u1 addr2 u2 )     \ Первая строка -- отрезок *после* первого символа на котором сработает f
+OVER >R first-patch 2DUP + OVER R> SWAP - 2SWAP ; \ Вторая -- до. Этот "обратный" порядок нужен для того чтобы,
+                                                  \ как правило обрабатываемое первым значение *до* было выше на стеке.
+
+: divide ( a u f --> s1 s2 \ <-- s1 s2 ) PRO LOCAL start LOCAL len
+divide-patch 2SWAP len ! start !  S>STR  start @ len @ S>STR CONT ;
 
 : split ( a u f <--> s1  )
 \ разбивает строку a u символами, на которых функция f даст TRUE и генерирует вызов для 
 \ каждой последовательности между этими символами с автоматическим выделением и снятием памяти
-PRO split-patch 2DUP >STR CONT STRFREE ;
+PRO split-patch 2DUP S>STR CONT ;
 
 : last-patch ( a u f <--> addr u ) PRO LOCAL a LOCAL len
 START{ split-patch DUP len ! OVER a ! }EMERGE a @ len @ CONT 2DROP ;
 
 : last ( a u f <--> s1  )
-PRO last-patch 2DUP >STR CONT STRFREE ;
+PRO last-patch 2DUP S>STR CONT ;
 
 : notEmpty ( s <--> s ) PRO DUP STR@ NIP ONTRUE CONT ; \ отфильтровывает пустые строки
 
@@ -122,11 +143,10 @@ CR r7
 \ вывод: [maryhasasheep]
  CR r8
 
-\ : r9 S" a1=123==456" [CHAR] = byChar divide DUP ."  [" STR@ TYPE ." ]" ;
+: r9 S" a1=123==456" [CHAR] = byChar divide-patch 2SWAP TYPE SPACE TYPE ;
 \ разбивает строку на две части
 \ вывод: [a1] [123==456]
-\ и снова CUT оставляет мусор
-\ CR r9
+CR r9
 
 : printFile  S" C:\lang\spf\devel\~profit\lib\bac4th-str.f" iterateStrings DUP STR@ CR TYPE ;
 \ вывод файла на печать
