662fd9a4d0aab18a5f43b215beacfc369d40ea28
azekeprofit <>
2007-01-08 18:50:28 +0000

    comments' fix

--- devel/~profit/lib/strings.f
+++ devel/~profit/lib/strings.f
@@ -1,3 +1,3 @@
-: lastChar ( addr len --> addr len-1 lastCh ) 1 CHARS - 0 MAX 2DUP + C@ ;
-: firstChar ( addr len --> addr len firstCh ) OVER C@ ;
-: restOfString ( addr len --> addr+1 len-1 ) DUP 0 > IF SWAP 1 CHARS + SWAP 1 CHARS - THEN ;
+: lastChar ( addr u -- addr u-1 lastCh ) 1 CHARS - 0 MAX  2DUP + C@ ;
+: firstChar ( addr u -- addr u firstCh ) OVER C@ ;
+: restOfString ( addr u -- addr+1 u-1 ) DUP 0 > IF SWAP 1 CHARS +  SWAP 1 CHARS - THEN ;
