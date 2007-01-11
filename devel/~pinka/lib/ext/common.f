8102bdf0fad86dffb9a2f5acfcde12aaf074bf42
ruv <>
2007-01-11 14:43:47 +0000

    the DISCARD word added ( alias of NDROP )

--- devel/~pinka/lib/ext/common.f
+++ devel/~pinka/lib/ext/common.f
@@ -1,3 +1,5 @@
+\ 2004
+
 REQUIRE [UNDEFINED] lib\include\tools.f
 
 [UNDEFINED] NDROP [IF]
@@ -5,5 +7,9 @@ REQUIRE [UNDEFINED] lib\include\tools.f
 \ Взять со стек n, затем убрать n значений со стека.
 
 : NDROP ( x*n n -- )  1+ CELLS SP@ + SP! ;
+
+\ 11.Jan.2007 Thu 17:40 added:
+\ see http://www.taygeta.com/forth/dpansa16.htm
+: DISCARD  ( x1 .. xu u -- ) NDROP ; \ Implementation factor DROP u+1 stack items
 
 [THEN]
