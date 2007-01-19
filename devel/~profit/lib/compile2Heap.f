4ba71ffe9e1b7afee3b15ee5ae7a53b6056cbb3b
azekeprofit <>
2007-01-19 20:44:34 +0000

    vc handle can now be EXECUTE'd directly examples improved

--- devel/~profit/lib/compile2Heap.f
+++ devel/~profit/lib/compile2Heap.f
@@ -9,15 +9,16 @@
 
 \ VC-, VC-COMPILE, VC-LIT, VC-RET, аналогичны
 \ ,    COMPILE,    LIT,    RET,
-\ только требуют дополнительный параметр -- cp
+\ только требуют дополнительный параметр -- vc
 
-\ Кроме того, слово VC- ( vc -- ) переключает все компилирующие 
+\ Кроме того, слово VC- ( vc --> \ <-- ) переключает все компилирующие 
 \ слова, идущие после него, на виртуальный кодофайл vc в пределах определения
 
 \ Все куски развязываются и освобождаются DESTROY-VC ( vc -- )
 
 
 \ REQUIRE MemReport ~day/lib/memreport.f
+REQUIRE /TEST ~profit/lib/testing.f
 REQUIRE LOCAL ~profit/lib/static.f
 REQUIRE CONT ~profit/lib/bac4th.f
 
@@ -26,14 +27,22 @@ MODULE: codepatches
 
 : __ CELL -- ;
 
+: --ALIGN CELL /MOD SWAP IF 1+ THEN CELL * ;
+
 0
+1 -- rlit     \ инструкция PUSH (0x64)
+__ firstBlock \ первый кусок занятой под кодофайл памяти
+1 -- ret      \ инструкция RET (0xC3)
+--ALIGN       \ выравниваем по ячейке
 __ block      \ размер кусков, которыми надо брать память для кода
 __ there      \ сохраняемый HERE в этом виртуальном кодофайле
-__ firstBlock \ первый кусок занятой под кодофайл памяти
 __ lastBlock  \ последний кусок
 __ limit      \ временная переменная, куда записывается граничный
               \ адрес, после пересечения которого надо начинать новый кусок
 CONSTANT codePatches
+
+: firstBlock! SWAP CELL+ SWAP firstBlock ! ;
+: firstBlock@ firstBlock @ CELL- ;
 
 VARIABLE counter
 counter 0!
@@ -53,23 +62,27 @@ R@ block @ + luft - R@ limit ! RDROP ;
 EXPORT
 : _CREATE-VC ( blockSize -- vc )
 codePatches ALLOCATE THROW >R
+0x68 R@ rlit C!
+0xC3 R@ ret C!
 R@ block !
 R@ allocatePatch
-R@ lastBlock @ R@ firstBlock !
+R@ lastBlock @ R@ firstBlock!
 R> ;
 
 : CREATE-VC (  -- vc ) 1 12 LSHIFT _CREATE-VC ; \ по-умолчанию куски по 4Кб
 
 : DESTROY-VC ( vc -- )
-DUP firstBlock @ SWAP FREE THROW
+DUP firstBlock@ SWAP FREE THROW
 BEGIN DUP @ 
 SWAP FREE THROW
 \ -1 counter +!  counter @ CR . \ для контроля забора и отдачи памяти
 DUP 0= UNTIL DROP ;
 
-: XT-VC ( vc -- xt ) firstBlock @ CELL+ ;
+: XT-VC ( vc -- xt ) firstBlock@ CELL+ ;
+\ : XT-VC ( vc -- xt ) ; \ можно и так, но дизассемблер будет показывать тогда только переход...
 
-: VC- ( ... vc -- ) PRO LOCAL vc vc !
+
+: VC- ( ... vc --> \ <-- ) PRO LOCAL vc vc !
 LOCAL savedHere HERE savedHere !
 vc @ there @ DP !
 CONT
@@ -87,14 +100,19 @@ savedHere @ DP ! ;
 : VC-DLIT, ( du vc -- ) >R SWAP R@ VC-LIT, R> VC-LIT, ;
 : VC-RET, ( vc -- ) VC- RET, ;
 \ компиляция команд в виде строки в кучу:
-: VC-COMPILED ( addr u cp -- ) VC- TRUE STATE B! EVALUATE ;
+: VC-COMPILED ( addr u vc -- ) VC- TRUE STATE B! EVALUATE ;
+\ Осторожно, так как компиляция проходит в один приём, команда
+\ VC- может прозевать выход за границы куска...
+\ Поэтому особенно много (в байтах выходной последовательности
+\ маш. кодов) не компилируйте.
 ;MODULE
 
-\EOF
+
+/TEST
 REQUIRE SEE lib/ext/disasm.f
 0 VALUE t
 : r 
-1000 CELLS CREATE-VC TO t
+CREATE-VC TO t
 0 t VC-LIT,
 4 0 DO ['] 1+ t VC-COMPILE, LOOP
 ['] . t VC-COMPILE,
@@ -104,8 +122,10 @@ S" dfdsdfdf" t VC-DLIT,
 ['] CR t VC-COMPILE,
 S" 1 1 + ." t VC-COMPILED
 t VC-RET,
-t XT-VC REST ;
+t XT-VC REST CR
+t ( XT-VC ) EXECUTE ;
 
-r
+>> r
+
 t DESTROY-VC
 \ MemReport
