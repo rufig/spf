bc3191de0d4a0181d25d9c45658ad8264fe8334c
azekeprofit <>
2007-01-19 20:32:59 +0000

    /TEST -- intelligent \EOF , analyzes INCLUDE-DEPTH

--- devel/~profit/lib/testing.f
+++ devel/~profit/lib/testing.f
@@ -1,23 +1,2 @@
-VARIABLE TESTING
-: /TEST TESTING @ 0= IF \EOF THEN ;
-TESTING 0!
-
-\EOF
-VOCABULARY tests:
-           ALSO tests DEFINITIONS
-: ;test PREVIOUS ;
-: NOTFOUND 2DROP ;
- PREVIOUS DEFINITIONS
-
-\EOF
-REQUIRE (: ~yz/lib/inline.f
-
-: r
-&INTERPRET @
-(:
-BEGIN \ интерпретировать входной поток со знаками препинания
-NextWord DUP         WHILE 
-S" bla" COMPARE 0= WHILE
-?STACK               REPEAT THEN ;)
- &INTERPRET !
-2DROP ;
+: /TEST INCLUDE-DEPTH @ 1 = 0= IF \EOF THEN ;
+: >>  0 WORD COUNT CR 2DUP ." >> " TYPE CR EVALUATE ;
