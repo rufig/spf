8c5394ec3550030cbb6325c901b9b0332c4fbd1f
azekeprofit <>
2007-01-19 20:34:12 +0000

    reverse directions' processing in 'iterateBy2' and 'reverse' word

--- devel/~profit/lib/bac4th-iterators.f
+++ devel/~profit/lib/bac4th-iterators.f
@@ -1,20 +1,39 @@
+\ Последовательный пробег по диапазону чисел. От одного до ста,
+\ от двух до пяти, от начала одной ячейки к памяти к другой.
+
+\ TODO: Разрешить обратные проходы с отрицательным шагом
+\ Вопрос: при отрицательном step, должен ли быть отрицательным len?
+\ Ответ. чтобы работали iterateByByteValues и прочие step не должен 
+\ быть отрицательным. Поэтому отрицательным
+\ должен становится len
+
 REQUIRE /TEST ~profit/lib/testing.f
 REQUIRE LOCAL ~profit/lib/static.f
 REQUIRE CONT ~profit/lib/bac4th.f
 REQUIRE compiledCode ~profit/lib/bac4th-closures.f
+
+: reverse ( start len -- start+len -len ) DUP NEGATE -ROT + 1- SWAP ;
 
 : iterateBy1  ( start len step --> i \ i <-- i ) PRO LOCAL step step !
 OVER + SWAP ?DO
 I CONT DROP
 step @ +LOOP ;
 
-: iterateBy2  ( start len step --> i \ i <-- i ) PRO LOCAL step step !
-OVER + 1- LOCAL end end !
+: iterateBy2  ( start len step --> i \ i <-- i ) PRO LOCAL step step ! 
+OVER +
+2DUP = IF 2DROP EXIT THEN
+LOCAL end DUP end !
+OVER > IF
 BEGIN
 CONT
 step @ +
-DUP end @ > UNTIL
-DROP ;
+DUP end @ < 0= UNTIL
+ELSE
+BEGIN
+CONT
+step @ -
+DUP end @ > 0= UNTIL
+THEN DROP ;
 
 \ Надо это потом переопределить через compiledCode пущей скорости ради
 
@@ -23,60 +42,66 @@ DROP ;
 : iterateBy3  ( start len step --> i \ i <-- i ) PRO
 OVER 0 > 0= IF 2DROP EXIT THEN \ если длина нулевая или меньше, значит делать больше нам нечего..
 >R
-OVER + 1- ( start end  R: step )
+OVER + ( start end  R: step )
 SWAP R> SWAP ( end step start )
 " PRO
-( start ) LITERAL
-BEGIN [ 2SWAP ] ( трах-ти-би-дох, ти-би-дох!.. Ну кто сказал что control-flow стек == стек параметров -- это хорошо !.?. )
+LITERAL
+BEGIN [ 2SWAP ]
 CONT
-( step ) LITERAL +
-DUP ( end ) LITERAL > UNTIL
+LITERAL +
+DUP LITERAL < 0= UNTIL
 DROP "
 STRcompiledCode ENTER CONT ;
 
 \ Но не только не летает, но оказывается *медленнее* чем DO LOOP !
 
 \ Хорошо, попробуем уже хаками:
-: t  ROT >R 2SWAP R> ;
-
 : iterateBy4  ( start len step --> i \ i <-- i )
 OVER 0 > 0= IF 2DROP EXIT THEN \ если длина нулевая или меньше, значит делать больше нам нечего..
 >R
-OVER + 1- ( start end  R: step )
+OVER + ( start end  R: step )
 SWAP R> SWAP ( end step start )
 R> SWAP ( end step succ-xt start )
 
 "
-( start ) LITERAL
-BEGIN [ t ]
-( succ-xt ) [ COMPILE, ]
-( step ) LITERAL +
-DUP ( end ) LITERAL > UNTIL
+LITERAL
+BEGIN [ ROT >R 2SWAP R> ]
+[ COMPILE, ]
+LITERAL +
+DUP LITERAL < 0= UNTIL
 DROP "
 STRcompiledCode ( xt )  >R ;
 
 \ Тож самое... Тормоза...
 
-: iterateBy ( start len step --> i \ i <-- i ) PRO
-2DUP 6 LSHIFT ( 2* 2* 2* 2* 2* 2* ) <
+: iterateBy ( start len step --> i \ i <-- i )
+2DUP 6 LSHIFT ( 2* 2* 2* 2* 2* 2* ) SWAP ABS >
 \ Решаем: если кол-во итераций в цикле будет меньше чем, скажем 64 (взято с потолка),
-IF iterateBy2 CONT EXIT THEN
+IF RUSH> iterateBy2 ELSE
 \ то циклуем статически,
-   iterateBy4 CONT ;
+   RUSH> iterateBy4 THEN ;
 \ иначе, если больше чем 64, -- то генерируем цикл и пускаем в нём
 
-: iterateByBytes ( addr u <--> caddr ) PRO 1 iterateBy CONT ;
-: iterateByWords ( addr u <--> waddr ) PRO 2 iterateBy CONT ;
-: iterateByCells ( addr u <--> addr )  PRO CELL iterateBy CONT ;
-: iterateByDCells ( addr u <--> qaddr ) PRO 2 CELLS iterateBy CONT ;
+\ : iterateBy POSTPONE iterateBy2 ; IMMEDIATE
+
+: iterateByBytes ( addr u <--> caddr )        1 RUSH> iterateBy ;
+: iterateByWords ( addr u <--> waddr )        2 RUSH> iterateBy ;
+: iterateByCells ( addr u <--> addr )      CELL RUSH> iterateBy ;
+: iterateByDCells ( addr u <--> qaddr ) 2 CELLS RUSH> iterateBy ;
 
 : iterateByByteValues ( addr n <--> caddr ) PRO       iterateByBytes DUP C@ CONT DROP ;
 : iterateByWordValues ( addr n <--> waddr ) PRO 2*    iterateByWords DUP W@ CONT DROP ;
 : iterateByCellValues ( addr n <--> addr )  PRO CELLS iterateByCells DUP @ CONT DROP ;
 
 /TEST
-: r S" abc" iterateByByteValues DUP EMIT ." _" ;
-r
+: printByOne iterateByByteValues DUP EMIT ." _" ;
+>> S" abc" printByOne
+
+: 10-3. 10 -3 1 iterateBy DUP . ;
+>> 10-3.
+
+: printByOneReverse reverse iterateByByteValues DUP EMIT ." _" ;
+>> S" abc" printByOneReverse
 
 : s 100 0 DO +{ 1 200000 1 iterateBy DUP }+ . LOOP ;
 \ ResetProfiles s .AllStatistic
