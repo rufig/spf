f635ab671f34418815316f9f83dab93180d145d2
ruv <>
2007-01-20 16:39:54 +0000

    + IS-FORTH-CLASS ( wid -- flag )

--- devel/~pinka/spf/enum-vocs.f
+++ devel/~pinka/spf/enum-vocs.f
@@ -10,12 +10,16 @@
   REPEAT DROP RDROP
 ;
 
+: IS-FORTH-CLASS ( wid -- flag )
+  CLASS@ DUP 0= SWAP FORTH-WORDLIST = OR
+;
+
 : ENUM-FORTH-VOCS ( xt -- )
 \ перебор только обычных форт-словарей (у которых CLASS равен 0 или FORTH-WORDLIST )
 \ xt ( wid -- )
   >R VOC-LIST @ BEGIN DUP WHILE
     DUP CELL+ ( a wid ) 
-    DUP CLASS@ DUP 0= SWAP FORTH-WORDLIST = OR
+    DUP IS-FORTH-CLASS
     IF R@ ROT >R  EXECUTE R> ELSE DROP THEN
     @
   REPEAT DROP RDROP
