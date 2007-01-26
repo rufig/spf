c3cb0c45e1d2d1c32153ec702b4270ddba366f61
azekeprofit <>
2007-01-26 13:23:35 +0000

    control-flow structures not interfering with stack (~mak/lib/a_if.f)

--- devel/~profit/lib/bac4th-closures.f
+++ devel/~profit/lib/bac4th-closures.f
@@ -8,6 +8,7 @@
 
 
 \ REQUIRE MemReport ~day/lib/memreport.f
+REQUIRE A_BEGIN ~mak/lib/a_if.f
 REQUIRE /TEST ~profit/lib/testing.f
 REQUIRE CONT ~profit/lib/bac4th.f
 REQUIRE FREEB ~profit/lib/bac4th-mem.f
@@ -15,22 +16,41 @@ REQUIRE FREEB ~profit/lib/bac4th-mem.f
 REQUIRE VC-COMPILED ~profit/lib/compile2Heap.f
 REQUIRE STR@ ~ac/lib/str4.f
 
-: compiledCode ( addr u --> addr-code \ <-- ) PRO LOCAL t
-CREATE-VC t !
-t @ VC-COMPILED
-t @ VC-RET,
-t @ XT-VC CONT
-t @ DESTROY-VC ;
+MODULE: bac4th-closures
 
+\ Используем структуры управления с отдельным control-flow stack
+: BEGIN [COMPILE] A_BEGIN ; IMMEDIATE
+: WHILE	[COMPILE] A_WHILE ; IMMEDIATE
+: AHEAD	[COMPILE] A_AHEAD ; IMMEDIATE
+: IF 	[COMPILE] A_IF ; IMMEDIATE
+: ELSE	[COMPILE] A_ELSE ; IMMEDIATE
+: THEN	[COMPILE] A_THEN ; IMMEDIATE
+: AGAIN	[COMPILE] A_AGAIN ; IMMEDIATE
+: REPEAT [COMPILE] A_REPEAT ; IMMEDIATE
+: UNTIL [COMPILE] A_UNTIL ; IMMEDIATE
+
+EXPORT
+
+: compiledCode ( addr u --> addr-code \ <-- ) PRO LOCAL t
+CREATE-VC t ! \ создаём виртуальный кодофайл
+ALSO bac4th-closures \ подключаем словарь с своими структурами управления
+t @ VC-COMPILED \ компилируем строку в виртуальный кодофайл
+PREVIOUS \ отключаем его по окончании компиляции
+t @ VC-RET, \ ставим команды выхода
+t @ XT-VC CONT \ берём исполняемый адрес начала кодофайла и кидаем его наверх
+t @ DESTROY-VC ; \ по окончании обработки очистить кодофайл
+
+\ То же самое что и compiledCode , но с динамическими строками из ~ac/lib/str4.f
+\ Это позволяет писать код в несколько строк
 : STRcompiledCode ( s --> ) PRO LOCAL s DUP s ! STR@ compiledCode s @ STRFREE CONT ;
+
+;MODULE
 
 /TEST
 REQUIRE SEE lib/ext/disasm.f
 
-: b HERE 1 2DROP ;
+: showMeTheCode ( addr u -- ) compiledCode REST CR CR ;
 
-: s ( n -- ) S" b LITERAL + " compiledCode REST CR CR ;
+$> 4 2 1 S" LITERAL LITERAL + + LITERAL * " showMeTheCode
 
-: r 4 0 DO I . I s LOOP ;
-r
-\ MemReport
+$> 100 S" 0 BEGIN DUP 10 < WHILE LITERAL . 1+ REPEAT DROP " showMeTheCode
--- devel/~profit/lib/bac4th-iterators.f
+++ devel/~profit/lib/bac4th-iterators.f
@@ -1,24 +1,26 @@
 \ Последовательный пробег по диапазону чисел. От одного до ста,
-\ от двух до пяти, от начала одной ячейки к памяти к другой.
+\ от двух до пяти, от начала одной ячейки к памяти к другой и т.д.
 
-\ TODO: Разрешить обратные проходы с отрицательным шагом
+\ Сделано: разрешение обратных проходов с отрицательным шагом.
 \ Вопрос: при отрицательном step, должен ли быть отрицательным len?
-\ Ответ. чтобы работали iterateByByteValues и прочие step не должен 
-\ быть отрицательным. Поэтому отрицательным
-\ должен становится len
+\ Ответ: чтобы работали iterateByByteValues и прочие, step не должен 
+\ быть отрицательным. Поэтому отрицательным должен становится len
 
 REQUIRE /TEST ~profit/lib/testing.f
 REQUIRE LOCAL ~profit/lib/static.f
 REQUIRE CONT ~profit/lib/bac4th.f
 REQUIRE compiledCode ~profit/lib/bac4th-closures.f
 
+\ Разворот начала и длины для прохода тех же самых значений в обратном порядке
 : reverse ( start len -- start+len -len ) DUP NEGATE -ROT + 1- SWAP ;
 
+\ Самый простой (и надёжный) вариант
 : iterateBy1  ( start len step --> i \ i <-- i ) PRO LOCAL step step !
 OVER + SWAP ?DO
 I CONT DROP
 step @ +LOOP ;
 
+\ Вариант посложнее, без DO LOOP и использования R-стека
 : iterateBy2  ( start len step --> i \ i <-- i ) PRO
 LOCAL step step !
 OVER +
@@ -35,24 +37,21 @@ step @ -
 DUP end @ > 0= UNTIL
 THEN DROP ;
 
-\ Надо это потом переопределить через compiledCode пущей скорости ради
-
-\ Переписал, должно летать:
-
+\ Вариант с динамической генерацией кода цикла
 : iterateBy3  ( start len step --> i \ i <-- i ) PRO
 OVER >R >R
-OVER + ( start end  R: step )
-SWAP R> SWAP ( end step start )
+OVER + ( start end  R: len step )
+SWAP R> SWAP ( end step start  R: len )
 R> 0 > IF
 " LITERAL
-BEGIN [ 2SWAP ]
+BEGIN
 [ R@ENTER, ]
 LITERAL +
 DUP LITERAL < 0= UNTIL
 DROP RDROP"
 ELSE
 " LITERAL
-BEGIN [ 2SWAP ]
+BEGIN
 [ R@ENTER, ]
 LITERAL -
 DUP LITERAL > 0= UNTIL
@@ -60,8 +59,8 @@ DROP RDROP"
 THEN
 STRcompiledCode ENTER CONT ;
 
-\ Но не только не летает, но оказывается *медленнее* чем DO LOOP !
-
+\ Главное слово итерирования, совершает пару проверок и решает какой из вариантов (2-й или 3-й)
+\ будет задействован
 : iterateBy ( start len step --> i \ i <-- i )
 OVER 0= IF 2DROP DROP RDROP EXIT THEN \ если длина нулевая или меньше, значит делать больше нам нечего..
 2DUP 6 LSHIFT ( 2* 2* 2* 2* 2* 2* ) SWAP ABS >
@@ -72,20 +71,30 @@ IF RUSH> iterateBy2 ELSE
 \ иначе, если больше чем 64, -- то генерируем цикл и пускаем в нём
 
   \ : iterateBy RUSH> iterateBy1 ;
-\ ^-- разбить в случае аварии программы
+\ ^-- "разбить в случае аварии" (с) (программы)
+\ Если будет глючить в итераторах, можно временно попробовать
+\ включить старый, добрый и простой как мычание iterateBy1
 
 : iterateByBytes ( addr u <--> caddr )        1 RUSH> iterateBy ;
+\ Только на первый взгляд бесмысленно использовать RUSH>
+\ Если писать iterateByBytes без безусловного перехода в RUSH> 
+\ пришлось бы для сохранения линии "успеха" (нырка), ставить 
+\ скобки PRO ... CONT и это мало того что бесполезно съело бы 
+\ значение на L-стеке, но и замедлило бы итерирование.
+
 : iterateByWords ( addr u <--> waddr )        2 RUSH> iterateBy ;
 : iterateByCells ( addr u <--> addr )      CELL RUSH> iterateBy ;
 : iterateByDCells ( addr u <--> qaddr ) 2 CELLS RUSH> iterateBy ;
 
-: iterateByByteValues ( addr n <--> caddr ) PRO       iterateByBytes DUP C@ CONT DROP ;
-: iterateByWordValues ( addr n <--> waddr ) PRO 2*    iterateByWords DUP W@ CONT DROP ;
-: iterateByCellValues ( addr n <--> addr )  PRO CELLS iterateByCells DUP @ CONT DROP ;
+: iterateByByteValues ( addr n <--> char ) PRO       iterateByBytes DUP C@ CONT DROP ;
+: iterateByWordValues ( addr n <--> word ) PRO 2*    iterateByWords DUP W@ CONT DROP ;
+: iterateByCellValues ( addr n <--> cell )  PRO CELLS iterateByCells DUP @ CONT DROP ;
 
 /TEST
 : printByOne iterateByByteValues DUP EMIT ." _" ;
-$> S" abc" printByOne
+$> S" abc" printByOne  S" ]" TYPE
+
+$> S" abc" reverse  printByOne  S" ]" TYPE
 
 : 10-3. 10 -3 1 iterateBy DUP . ;
 $> 10-3.
@@ -99,23 +108,5 @@ $> 150-50.
 : 0. 150 0 1 iterateBy DUP . ;
 $> 0.
 
-
-: printByOneReverse reverse iterateByByteValues DUP EMIT ." _" ;
-$> S" abc" printByOneReverse
-
 : s 100 0 DO +{ 1 200000 1 iterateBy DUP }+ . LOOP ;
 \ ResetProfiles s .AllStatistic
-
-\  Времени работы слова s на разных вариантах iterateBy
-
-\  1 (DO LOOP) __________________________________________________ 43,394,789,947
-\  2 (STRcompiledCode и CONT) ___________________________________ 56,321,774,280
-\  3 (STRcompiledCode с прямым внедрением вызова кода успеха) ___ 50,266,760,943
-\  3 c DIS-OPT (sic!) и ~pinka/spf/quick-swl.f __________________ 10,369,798,311
-\  _____________________________________________________________________________
-\  Итог: чёрти что
-
-\ Видимо дело в занятии памяти через WINAPI_функцию ALLOCATE глубоко внутри 
-\ STRcompiledCode
-
-\ Нет дело не в ALLOCATE, дело в EVALUATE
