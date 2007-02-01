f4462cf36bfce756a2eee6f898ce967e3022d087
azekeprofit <>
2007-02-01 13:35:21 +0000

    sqrt example added

--- devel/~profit/lib/binary-search.f
+++ devel/~profit/lib/binary-search.f
@@ -1,24 +1,27 @@
 REQUIRE /TEST ~profit/lib/testing.f
-REQUIRE LOCAL ~profit/lib/static.f
 
 \ Двоичный поиск по функции. a , b -- это начальный диапазон 
-\ целочисленных значений, аргументов функции f , которая 
+\ целочисленных значений аргументов функции f , которая 
 \ представлена xt соотв. процедуры
 \ При запуске процедура берёт одно проверочное значение со стека
 \ и выдаёт +1|0|-1 , совпадение -- ноль, +1 -- больше, -1 -- 
 \ меньше.
 \ На выходе флаг и найденное значение аргумента функции f при котором
-\ f=0, либо тоже 0 если такое значение аргумента не найдено.
-: binary-search ( a b f -- i 0|-1 ) LOCAL f f ! \ f ( i -- +1|0|-1)
+\ f=0, либо если не было найдено, то последнее приближение к тому
+
+\ Можно использовать и как для поиска по отсортированному массиву, так 
+\ и для вычисления обратной функции.
+
+: binary-search ( a b f -- i 0|-1 ) >R \ f ( i -- +1|0|-1)
 BEGIN 2DUP < WHILE
 2DUP + 2/
-DUP f @ EXECUTE ( +1|0|-1 ) \ теплее|попал|холоднее
-DUP 0= IF DROP NIP NIP TRUE EXIT THEN \ есть совпадение
+DUP R@ EXECUTE ( +1|0|-1 ) \ теплее|попал|холоднее
+DUP 0= IF DROP NIP NIP TRUE RDROP EXIT THEN \ есть совпадение
 0< IF NIP 1- ELSE ROT DROP 1+ SWAP THEN \ вилка
 REPEAT
 
-DUP f @ EXECUTE 0= IF NIP TRUE EXIT THEN \ доп. проверка на краевой случай
-2DROP 0 FALSE ;
+DUP R@ EXECUTE 0= IF NIP TRUE RDROP EXIT THEN \ доп. проверка на краевой случай
+DROP FALSE RDROP ;
 
 /TEST
 
@@ -36,9 +39,17 @@ HERE SWAP - CELL / VALUE len
 
 : 3DUP 2OVER 2OVER 3 ROLL DROP ;
 
-0 len
-:NONAME ( i -- f )  CELLS tmp + @ n - NEGATE SGN ;
-3DUP 3DUP
+
+:NONAME ( i -- f )  CELLS tmp + @ n - NEGATE SGN ; CONSTANT arrI
+0 len arrI
 $> 10 TO n binary-search . .
+0 len arrI
 $> 400 TO n binary-search . .
+0 len arrI
 $> 8 TO n binary-search . .
+
+:NONAME ( x -- f ) DUP * n - NEGATE SGN ; CONSTANT sqrF
+
+$> 1 1000 sqrF 400 TO n binary-search . .
+$> 1 1000 sqrF 9 TO n binary-search . .
+$> 1 1000 sqrF 1001 TO n binary-search . .
