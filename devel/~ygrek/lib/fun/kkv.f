0b0f5830189e4253fb99e74126febba5fdbf4683
ygreks <>
2007-02-11 16:49:13 +0000

    CVS keywords as Forth strings

---
+++ devel/~ygrek/lib/fun/kkv.f
@@ -0,0 +1,17 @@
+\ $Id$
+\
+\ CVS keywords parsed as Forth strings
+\ Just put $Revision$ somewhere in your source and it will transform to S" 1.1"
+\ After each CVS commit revision will be automatically increased by CVS itself 
+\ NB Keyword substitution is performed only if -kkv is specifed (it is the default for text files)
+
+: kkv-save [CHAR] $ PARSE -TRAILING S", ;
+: kkv-extract HERE >R kkv-save R> COUNT ;
+
+: $Date: kkv-extract ;
+: $Revision: kkv-extract ;
+
+\EOF
+
+
+$Revision$ TYPE
