772edf846ca2439a0fac8ec6882d686a85c1f1e5
spf <>
2007-02-20 22:27:17 +0000

    FileExists отправлен в ~ac/lib/win/file/file-exists.f, позволяя перегружать другие реализации этого слова, например FILE-EXISTS из SPF4.

--- devel/~ac/lib/ns/files.f
+++ devel/~ac/lib/ns/files.f
@@ -2,23 +2,7 @@
 
 REQUIRE FIND-FILES-R      ~ac/lib/win/file/findfile-r.f 
 REQUIRE ForEachDirWRstr   ~ac/lib/ns/iter.f
-
-: FileExists ( addr u -- flag )
-  R/O OPEN-FILE-SHARED ?DUP
-  IF NIP DUP 2 =
-        OVER 3 = OR
-        OVER 206 = OR 
-        SWAP 123 = OR
-        0=
-  ELSE CLOSE-FILE THROW TRUE
-  THEN
-;
-WINAPI: GetFileAttributesA KERNEL32.DLL
-
-: IsDirectory ( addr u -- flag )
-  DROP GetFileAttributesA DUP FILE_ATTRIBUTE_DIRECTORY AND
-  0<> SWAP -1 <> AND
-;
+REQUIRE FileExists        ~ac/lib/win/file/file-exists.f
 
 <<: FORTH FIL
 >> CONSTANT FIL-WL
