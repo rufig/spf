c445b71493136941a7d3038bf78bede00fecf2b4
azekeprofit <>
2007-03-27 15:37:32 +0000

    fixes tail-recursion cascaded colons

--- devel/~profit/lib/colorForth/photon.f
+++ devel/~profit/lib/colorForth/photon.f
@@ -23,12 +23,17 @@
 \ более видно... Например, указав # вначале строки (если уж добиваемся
 \ Питонообразия, то гулять-так-гулять...).
 
-\ TODO: Cascading definitions (использовать векторный SHEADER и
-\ векторный SFIND использования хэшей как словарей)
-\ BUG: Почему return компилируется?.. В _начало_ определений?..
+\ Комментарии. Пока в их отбражении разброд и шатание, так как
+\ я могу злоупотреблять тем что control-flow автоматически делают
+\ оставшуюся после себя строку комментарием
+
+\ TODO: Сделать определение хвостовой оптимизации более надёжным
+\ TODO: Сделать взаимное беспроблемное подключение кода SPF и Photon'а
+\ TODO: Нужна ли поддержка WARNING ?
 
 REQUIRE >L ~profit/lib/lstack.f
-
+REQUIRE NOT ~profit/lib/logic.f
+REQUIRE cascaded ~profit/lib/colorForth/cascaded.f
 MODULE: photon
 
 40 CONSTANT maxTabs
@@ -43,7 +48,7 @@ clearTabs
 : flushTabs ( i -- )
 CELLS tabsArr +  tabsArr maxTabs CELLS +  ( tabsArr[i] end )
 BEGIN
-2DUP > 0= WHILE
+2DUP <= WHILE
 DUP @ EXECUTE
 ['] NOOP OVER !
 CELL-
@@ -53,6 +58,7 @@ REPEAT
 : setTab ( xt i -- )  CELLS tabsArr + ! ;
 
 : OnTabulation ( -- flag ) GetChar SWAP 9 = AND ;
+: OnBlank ( -- flag ) EndOfChunk ;
 
 VARIABLE curTab
 curTab 0!
@@ -76,23 +82,42 @@ control-flow do
 :NONAME L> 1 [COMPILE] THEN ;
 control-flow if
 
-:NONAME RET, ;
+:NONAME HERE 5 - C@ 0xE8 = IF 0xE9 HERE 5 - C! \ заменяем CALL на JMP
+ELSE RET, \ или обычный EXIT
+THEN ; 
 :NONAME ;
 control-flow return
 
+ALSO dontHide \ чтобы подключить нужный :
+
 :NONAME ( -- )
+OnBlank        IF EXIT THEN
 OnTabulation   IF tabsCount DUP curTab ! flushTabs [COMPILE] ] INTERPRET_ EXIT THEN
 OnDelimiter    IF [COMPILE] [ INTERPRET_ EXIT THEN
-CREATE [COMPILE] \ DOES> EXECUTE ;
+[COMPILE] :
+[COMPILE] \ ;
 CONSTANT photonInterpreter
+
+PREVIOUS
 
 ;MODULE
 
-photonInterpreter &INTERPRET !
+ALSO cascaded NEW: photonWords
+\ режим каскадных определений выкидывает словарные структуры
+\ из кодофайла
+PREVIOUS
 
+: startPhoton
+ALSO photonWords DEFINITIONS \ включаем каскадные определения
+photonInterpreter &INTERPRET ! \ включаем собственный интерпретатор
+;
+
+/TEST
+startPhoton
  \ Лёгким движением руки Форт превращается...
  \ Превращается Форт... Форт превращается... 
  \ В изящный типо-Питон!..
+ \ ====================== Здесь начинается код =====================
 
 2x2.
 	2 2 * . return      Обратите внимание что после слов control-flow можно писать всё что угодно...
@@ -117,7 +142,28 @@ test
 
  CR .( 1-10. ) 1-10.
 
- \EOF
+ \ Рекурсивный вариант:
+fact ( n -- fact(n)
+	?DUP 0= if
+		1 return
+	DUP 1- fact * return
+
+ CR 3 DUP .  .( fact=) fact .
+
+ \ Факториал на цикле. Хотя в colorForth'ах циклы 
+ \ вырождаются в сочетание if'а (даже без else),
+ \ хвостовой оптимизации (она же -- GOTO) и
+ \ каскадных определений (они же -- метки).
+ \ Эдакое назад в будущее...
+fact ( n -- fact(n)
+	1 SWAP                \  Ставим аккумулятор на стек внутренней функции
+fact0 ( acc n )                  Это можно рассматривать и как функцию и как метку
+	?DUP 0= if               Краевой случай "рекурсии" (он же условие выхода из цикла)
+		return
+	TUCK * SWAP 1-
+	fact0 return             Это GOTO fact0 , по-сути
+
+ CR 6 DUP .  .( fact=) fact .
 
 array                DOES>-действие для массивов
 	R>
@@ -127,10 +173,10 @@ Arr ( -- arr )       Покажем, как обходятся в colorForth'ах без DOES>
 	array  \ <-- Это и есть DOES>-действие
  11 , 22 , 33 , 44 , 55 ,
 
+ \ То есть: ...-CODE-действия мы компилируем вручную
+
 getArr ( i -- arr[i] )
-	1- CELLS  Arr 1 +
+	1- CELLS  Arr +
 	return
 
- 1 getArr .
- SEE array
- SEE Arr
+ CR 4 DUP . .( arr=) getArr @ .
