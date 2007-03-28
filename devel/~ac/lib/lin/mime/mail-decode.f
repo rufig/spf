361dfded1f44a3c55fb0a3dff87cd9e2c3e52675
spf <>
2007-03-28 04:15:34 +0000

    Поддержка экзотической кириллической кодировки iso-8859-5.

--- devel/~ac/lib/lin/mime/mail-decode.f
+++ devel/~ac/lib/lin/mime/mail-decode.f
@@ -3,10 +3,23 @@ REQUIRE StripLwsp              ~ac/lib/string/mime-decode.f
 REQUIRE UNICODE>UTF8           ~ac/lib/win/com/com.f
 
 \ ================================= subject decoding ==================
+
+: iso-8859-5>UNICODE ( addr u -- addr2 u2 )
+\ специально для чтения писем ~yz :)
+  DUP 2* CELL+ ALLOCATE DROP UnicodeBuf !
+  SWAP >R
+  DUP 2* CELL+ UnicodeBuf @ ROT R> 0 ( flags) 28595 ( iso-8859-5)
+  MultiByteToWideChar
+  UnicodeBuf @ 
+  SWAP 2* 
+  2DUP + 0 SWAP W!
+;
+
 GET-CURRENT ALSO CHARSET-DECODERS DEFINITIONS
 : UTF-8 UTF8>UNICODE " {s}" STR@ UNICODE> ;
 : Utf-8 UTF8>UNICODE " {s}" STR@ UNICODE> ;
 : utf-8 UTF8>UNICODE " {s}" STR@ UNICODE> ;
+: iso-8859-5 iso-8859-5>UNICODE " {s}" STR@ UNICODE> ;
 PREVIOUS SET-CURRENT
 
 REQUIRE { ~ac/lib/locals.f
