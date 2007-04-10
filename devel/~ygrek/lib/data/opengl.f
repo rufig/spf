ec492cd0cb6cc1e42f248e7d2cbc4e67c3100887
ygreks <>
2007-04-10 14:45:44 +0000

    several more OpenGL WINAPI:'s

--- devel/~ygrek/lib/data/opengl.f
+++ devel/~ygrek/lib/data/opengl.f
@@ -14,9 +14,15 @@ WINAPI: wglCreateContext  OpenGL32.DLL
 WINAPI: wglSwapBuffers    OpenGL32.DLL
 WINAPI: glGetError        OpenGL32.DLL
 WINAPI: glTranslatef      OpenGL32.DLL
+WINAPI: glScalef          OpenGL32.DLL
 WINAPI: glBegin           OpenGL32.DLL
 WINAPI: glEnd             OpenGL32.DLL
 WINAPI: glVertex3f        OpenGL32.DLL
+WINAPI: glVertex3fv       OpenGL32.DLL
+WINAPI: glVertex3dv       OpenGL32.DLL
+WINAPI: glNormal3f        OpenGL32.DLL
+WINAPI: glNormal3fv       OpenGL32.DLL
+WINAPI: glNormal3dv       OpenGL32.DLL
 WINAPI: glColor3f         OpenGL32.DLL
 WINAPI: glRotatef         OpenGL32.DLL
 WINAPI: glGenTextures     OpenGL32.DLL
@@ -26,7 +32,14 @@ WINAPI: glTexParameteri   OpenGL32.DLL
 WINAPI: glTexCoord2f      OpenGL32.DLL
 WINAPI: glPixelStoref     OpenGL32.DLL
 WINAPI: glPixelStorei     OpenGL32.DLL
+WINAPI: glPushMatrix      OpenGL32.DLL
+WINAPI: glPopMatrix       OpenGL32.DLL
+WINAPI: glLightf          OpenGL32.DLL
+WINAPI: glLightfv         OpenGL32.DLL
+WINAPI: glCallList        OpenGL32.DLL
+WINAPI: glGenLists        OpenGL32.DLL
+WINAPI: glEndList         OpenGL32.DLL
+WINAPI: glNewList         OpenGL32.DLL
 
 WINAPI: gluPerspective    Glu32.DLL 
-WINAPI: gluOrtho2D         Glu32.DLL 
-
+WINAPI: gluOrtho2D        Glu32.DLL 
