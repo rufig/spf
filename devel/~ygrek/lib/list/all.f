8f5bd9df01735960722d3f0da15cec7d31964d48
ygreks <>
2007-04-12 17:24:19 +0000

    all together

---
+++ devel/~ygrek/lib/list/all.f
@@ -0,0 +1,5 @@
+REQUIRE cons ~ygrek/lib/list/core.f
+REQUIRE lst( ~ygrek/lib/list/ext.f
+REQUIRE write-list ~ygrek/lib/list/write.f
+REQUIRE reverse ~ygrek/lib/list/more.f
+REQUIRE qsort-list ~ygrek/lib/list/qsort.f
