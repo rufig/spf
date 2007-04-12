1dc6e91bf314180d01b3d28677b9d1aae2f8d753
ruv <>
2007-04-12 18:48:53 +0000

    Теперь все правила добавляются к микроядру прямо из rules-common.f.xml и rules-forth.f.xml.

--- devel/~pinka/spf/forthml/index.f
+++ devel/~pinka/spf/forthml/index.f
@@ -2,7 +2,7 @@
 ( “рансл€тор ForthML
 
 »спользование
-  S" url-of.f.xml" EMBODY
+  S" url-of.f.xml" EMBODY [ i*x c-addrz u -- j*x ]
   -- создает объект по указанной модели в текущем контексте
 
 
@@ -10,16 +10,26 @@
   -- пока трансл€тор не многопоточен.
 )
 
-\ REQUIRE EXC-DUMP2   ~pinka/spf/exc-dump.f
+REQUIRE EXC-DUMP2   ~pinka/spf/exc-dump.f
+
 REQUIRE [UNDEFINED]     lib/include/tools.f
 REQUIRE AsQName         ~pinka/samples/2006/syntax/qname.f \ пон€тие однословных строк в виде `abc
 REQUIRE getAttributeNS  ~pinka/lib/lin/xml/libxml2-dom.f
+REQUIRE SPLIT-          ~pinka/samples/2005/lib/split.f
 REQUIRE FINE-HEAD       ~pinka/samples/2005/lib/split-white.f
 REQUIRE Require         ~pinka/lib/ext/requ.f
 
+
 MODULE: forthml-support
 
+[UNDEFINED] &
+
 REQUIRE GERM-A  ~pinka/spf/compiler/index.f
+
+        [IF]    EXPORT
+: & & ;
+                DEFINITIONS
+        [THEN]
 
 VARIABLE cnode-a
 
@@ -31,15 +41,25 @@ Require gtNUMBER aliases.f \ набор синонимов
 
 DEFINITIONS
 
-Include ttext-index.auto.f \ в виде простейшего форт-текста
-\ предоставл€ет T-TEXT -- слово дл€ трансл€ции текста,
-\ €дро дл€ трансл€ции xml-дерева и переменную STATE
+?C-JMP TRUE TO ?C-JMP  \ включение хвостовой оптимизации: [CALL XXX][RET] --> [JMP XXX]
+                       \ актуально дл€ цепочки обработчиков.
 
-Include ~pinka/fml/forthml-rules.f \ в виде простейшего форт-текста
-\ набор правил дл€ трансл€ции ForthML 
+Include ttext-index.auto.f \ в виде простейшего форт-текста
+\ предоставл€ет T-PLAIN -- слово дл€ трансл€ции текста,
+\ €дро дл€ трансл€ции xml-дерева, переменные состо€ни€ M и STATE
+
+UNIX-LINES
+Include ~pinka/fml/forthml-core.f \ базоый набор слов (правил) ForthML
+DOS-LINES
+
+`~pinka/fml/src/rules-common.f.xml FIND-FULLNAME EMBODY
+`~pinka/fml/src/rules-forth.f.xml  FIND-FULLNAME EMBODY
+
+TO ?C-JMP
 
 EXPORT
 
 `EMBODY `EMBODY aka
 
-;MODULE
+;MODULE 
+\ ALSO forthml-support
--- devel/~pinka/spf/forthml/make-ttext.cmd
+++ devel/~pinka/spf/forthml/make-ttext.cmd
@@ -1,2 +1,3 @@
 @echo off
 call saxonB8  ttext-index.f.xml  ../../fml/forthml.xsl  >ttext-index.auto.f
+echo %ERRORLEVEL%
