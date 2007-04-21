bf61d7062f6fd361b4f32c97ee5e044751c6b122
spf <>
2007-04-21 19:20:37 +0000

    FILE-SIZEA - получение размера файла, заданного именем, а не хэндлом. Должно работать быстрее, и не требует прав на открытие файла.

---
+++ devel/~ac/lib/win/file/filesize.f
@@ -0,0 +1,17 @@
+REQUIRE FIND-FILES       ~ac/lib/win/file/findfile.f
+
+: FILE-SIZEA { addr u \ data id -- ud ior }
+\ ud - размер в символах файла, идентифицируемом именем addr u.
+\ ior - определенный реализацией код результата ввода/вывода.
+\ Эта операция не влияет на значение, возвращаемое FILE-POSITION.
+\ ud неопределен, если ior не ноль.
+
+  0 addr u + C!
+  /WIN32_FIND_DATA ALLOCATE THROW -> data
+  data /WIN32_FIND_DATA ERASE
+  data addr FindFirstFileA -> id
+  id -1 = IF data FREE DROP 0 0 GetLastError EXIT THEN
+  data nFileSizeLow @ data nFileSizeHigh @ 0
+  id FindClose DROP
+  data FREE DROP
+;
