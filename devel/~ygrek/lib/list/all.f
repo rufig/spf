4007c4ef8e177ac62b9276a0439bb40d503d499e
ygreks <>
2007-04-25 13:09:42 +0000

    more.f also

--- devel/~ygrek/lib/list/all.f
+++ devel/~ygrek/lib/list/all.f
@@ -3,3 +3,4 @@ REQUIRE lst( ~ygrek/lib/list/ext.f
 REQUIRE write-list ~ygrek/lib/list/write.f
 REQUIRE reverse ~ygrek/lib/list/more.f
 REQUIRE qsort-list ~ygrek/lib/list/qsort.f
+REQUIRE reduce-this ~ygrek/lib/list/more.f
