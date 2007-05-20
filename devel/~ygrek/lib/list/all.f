f4e31789a72e6d7db02df4d676bfa2eaefcd5b96
ygreks <>
2007-05-20 16:38:14 +0000

    minor. fix REQUIRE

--- devel/~ygrek/lib/list/all.f
+++ devel/~ygrek/lib/list/all.f
@@ -1,6 +1,5 @@
 REQUIRE cons ~ygrek/lib/list/core.f
 REQUIRE lst( ~ygrek/lib/list/ext.f
 REQUIRE write-list ~ygrek/lib/list/write.f
-REQUIRE reverse ~ygrek/lib/list/more.f
 REQUIRE list-qsort ~ygrek/lib/list/qsort.f
 REQUIRE reduce-this ~ygrek/lib/list/more.f
