e5d4b3d91e8e09c43cfa77df7c0e60576e7c35b4
azekeprofit <>
2007-05-27 12:43:15 +0000

    tests with ~pinka\lib\list.f

---
+++ devel/~profit/misc/pinka list.f
@@ -0,0 +1,11 @@
+REQUIRE list+ ~pinka\lib\list.f
+: list=> ( list --> value \ <-- ) R> SWAP List-ForEach ;
+
+VARIABLE r
+
+
+2 CELLS ALLOCATE THROW DUP . r list+
+2 CELLS ALLOCATE THROW DUP . r list+
+
+CR
+:NONAME r list=> . ; EXECUTE
