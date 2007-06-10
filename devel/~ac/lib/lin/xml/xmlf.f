2328106528ade15f7b91ff12a5d3d34c99e83538
spf <>
2007-06-10 00:01:18 +0000

    Комментарии и примеры.

--- devel/~ac/lib/lin/xml/xmlf.f
+++ devel/~ac/lib/lin/xml/xmlf.f
@@ -1,5 +1,7 @@
 \ Упрощение работы с xpath-функциями, выдающими float-результаты.
 \ Библиотека подключает sprintf из C runtime windows-зависимым способом.
+\ 'Легчепортабельной' альтернативой является обертка возвращаемого числового 
+\ значения xpath-функцией string(), если результат не nodeset.
 
 REQUIRE XML_XPATH      ~ac/lib/lin/xml/xml.f
 
@@ -10,5 +12,6 @@ WINAPI: sprintf MSVCRT.DLL
 :NONAME xpo.floatval ( 12 DUMP) CELL+ 2@ P.0> TYPE ; xpathTypes 2 CELLS + !
 :NONAME xpo.floatval CELL+ 2@ P.0> ;                xpathTypes@ 2 CELLS + !
 
-\EOF
+\EOF пример
 S" xml-rpc.xml" S" count(//member)" XML_XPATH
+S" xml-rpc.xml" S" string(count(//member))" XML_XPATH
