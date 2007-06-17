a53ecbd2de350408776bdc53bcbceff3b87094a0
azekeprofit <>
2007-06-17 15:57:03 +0000

    fix

--- devel/~profit/lib/for-next.f
+++ devel/~profit/lib/for-next.f
@@ -1,13 +1,16 @@
 \ Цикл FOR ... NEXT. Имеет только одну переменную цикла,
 \ которая от начального значения снижается к нулю и потом выходит
 
+\ Из-за реализации слова I в SPF брать значение цикловой переменной
+\ тем же словом I не получится, надо брать через R@
+
 REQUIRE /TEST ~profit/lib/testing.f
 
 : FOR POSTPONE >R HERE ;  IMMEDIATE
 
 : NEXT  ?COMP
-POSTPONE R> POSTPONE 1- POSTPONE >R
-POSTPONE R@ POSTPONE 0= ?BRANCH,
+POSTPONE R> POSTPONE DUP POSTPONE 1- POSTPONE >R
+POSTPONE 0= ?BRANCH,
 POSTPONE RDROP
 ;  IMMEDIATE
 DECIMAL
