9bfc463a1c593ea862871cdd7ae9619eb9aad6cc
pians <>
2007-07-09 11:06:53 +0000

    test graph.f and wincon.f

--- devel/~pi/lib/wincon/test.f
+++ devel/~pi/lib/wincon/test.f
@@ -1,10 +1,13 @@
 REQUIRE Point	~pi/lib/wincon/graph.f
 
-50 50 100 70 Line
-10 10 Draw
+\ S" disketa.bmp" 10 70 Image
+\ S" yes.bmp" 30 70 Image
 
 10 10 50 20 20 RSquare
 0x000000FF Color
 0x00FF0000 Background
 20 20 50 20 20 RSquare
+0x0000FF00 Color
+50 50 100 70 Line
+10 10 Draw
 
