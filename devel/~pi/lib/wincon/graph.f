380b32dd16d0fec494854e2bdb56ff0757d47269
pians <>
2007-07-09 11:06:01 +0000

    graphic for wincon.f

--- devel/~pi/lib/wincon/graph.f
+++ devel/~pi/lib/wincon/graph.f
@@ -2,9 +2,9 @@
 \ __          ___       ____ ___
 \ \ \        / (_)     |___ \__ \   graph for Windows
 \  \ \  /\  / / _ _ __   __) | ) |  pi@alarmomsk.ru
-\   \ \/  \/ / | | '_ \ |__ < / /   ┴шсышюЄхър т√тюфр уЁрЇшъш эр уЁрЇ. ъюэёюы№
+\   \ \/  \/ / | | '_ \ |__ < / /   Библиотека вывода графики на граф. консоль
 \    \  /\  /  | | | | |___) / /_   Pretorian 2007
-\     \/  \/   |_|_| |_|____/____|  v 1.2
+\     \/  \/   |_|_| |_|____/____|  v 1.3
 \ -----------------------------------------------------------------------------
 REQUIRE wincon	~pi/lib/wincon/wincon.f
 
@@ -13,57 +13,85 @@ WINAPI: MoveToEx		GDI32.DLL
 WINAPI: LineTo			GDI32.DLL
 WINAPI: Ellipse			GDI32.DLL
 WINAPI: RoundRect		GDI32.DLL
+WINAPI: LoadImageA		USER32.DLL
+WINAPI: StretchBlt		GDI32.DLL
+WINAPI: GetObjectA		GDI32.DLL
 
-\ ╧хЁхьх∙рхЄ Єюўъє эрўрыр Ёшёютрэш  фы  Draw
+0
+CELL -- bmType
+CELL -- bmWidth
+CELL -- bmHeight
+CELL -- bmWidthBytes
+2 -- bmPlanes
+2 -- bmBitsPixel
+CELL -- bmBits
+CONSTANT BITMAP
+0 VALUE bitmap
+BITMAP ALLOCATE THROW TO bitmap
+
+
+\ Перемещает точку начала рисования для Draw
 : MoveTo ( x y -> )
 	0 -ROT SWAP phdc MoveToEx DROP ;
 
-\ ╨шёєхЄ ышэш■ юЄ Єхъє∙х Єюўъш Ёшёютрэш 
+\ Рисует линию от текуще точки рисования
 : Draw ( x y -> )
 	SWAP phdc LineTo DROP ;
 
-\ ═рЁшёютрЄ№ Єюўъє
+\ Нарисовать точку
 : Point ( x y -> )
 	SWAP color -ROT phdc SetPixel DROP ;
 
-\ ═рЁшёютрЄ№ ышэш■
+\ Нарисовать линию
 : Line ( x y x1 y1 -> )
 	2SWAP MoveTo Draw ;
 
-\ ═рЁшёютрЄ№ ъЁєу
+\ Нарисовать круг
 : Circle ( x y d -> )
 	>R SWAP 2DUP R@ + SWAP R> + SWAP 2SWAP phdc Ellipse DROP ;
 
-\ ═рЁшёютрЄ№ ¤ышяё
+\ Нарисовать элипс
 : Ellips ( x y x1 y1 -> )
 	SWAP 2SWAP SWAP phdc Ellipse DROP ;
 
-\ ═рЁшёютрЄ№ ътрфЁрЄ
+\ Нарисовать квадрат
 : Square ( x y l -> )
 	>R SWAP 2DUP R@ + SWAP R> + SWAP 2SWAP phdc Rectangle DROP ;
 
-\ ═рЁшёютрЄ№ яЁ ьюєуюы№эшъ
+\ Нарисовать прямоугольник
 : Rect ( x y x1 y1 -> )
          SWAP 2SWAP SWAP phdc Rectangle DROP ;
 
-\ ═рЁшёютрЄ№ яЁ ьюєуюы№эшъ c ъЁєуыхээ√ьш ъюэЎрьш
+\ Нарисовать прямоугольник c кругленными концами
 : RRect ( x y x1 y1 ll lh -> )
          SWAP 2SWAP SWAP 2>R 2SWAP SWAP 2R> 2SWAP phdc RoundRect DROP ;
 
-\ ═рЁшёютрЄ№ ътрфЁрЄ  c ъЁєуыхээ√ьш ъюэЎрьш
+\ Нарисовать квадрат  c кругленными концами
 : RSquare ( x y l ll lh-> )
 	SWAP 2SWAP >R -ROT 2SWAP SWAP 2DUP R@ + SWAP R> + SWAP 2SWAP phdc RoundRect DROP ;
 
+\ Вывести изображение bmp из файла на консоль
+: Image ( addr u x y -> )
+	{ x y | hbitmap bhdc pbhdc }
+	DROP >R 0x10 0 0 0 R> hwdwin LoadImageA TO hbitmap
+	bitmap 24 hbitmap GetObjectA DROP
+	phdc CreateCompatibleDC TO bhdc
+	hbitmap bhdc SelectObject TO pbhdc
+	0xCC0020 bitmap bmHeight @ bitmap bmWidth @ 0 0 bhdc
+	bitmap bmHeight @ bitmap bmWidth @ y x phdc StretchBlt DROP
+	hbitmap DeleteObject DROP
+	bhdc DeleteDC DROP ;
 
 \EOF
 
-Point		( x y -> ) - эрЁшёютрЄ№ Єюўъє
-MoveTo		( x y -> ) - яхЁхьх∙рхЄ Єюўъє эрўрыр Ёшёютрэш  фы  Draw
-Draw		( x y -> ) - ЁшёєхЄ ышэш■ юЄ Єхъє∙х Єюўъш Ёшёютрэш 
-Line		( x y x1 y1 -> ) - эрЁшёютрЄ№ ышэш■
-Square		( x y l -> ) - эрЁшёютрЄ№ ътрфЁрЄ
-Rect 		( x y x1 y1 -> ) - эрЁшёютрЄ№ яЁ ьюєуюы№эшъ
-Ellips		( x y x1 y1 -> ) - эрЁшёютрЄ№ ¤ышяё
-Circle		( x y d -> ) - эрЁшёютрЄ№ ъЁєу
-RRect		( x y x1 y1 h l -> ) - эрЁшёютрЄ№ яЁ ьюєуюы№эшъ c ъЁєуыхээ√ьш ъюэЎрьш
-RSquare		( x y l ll lh-> ) - эрЁшёютрЄ№ ътрфЁрЄ  c ъЁєуыхээ√ьш ъюэЎрьш
+Point		( x y -> ) - нарисовать точку
+MoveTo		( x y -> ) - перемещает точку начала рисования для Draw
+Draw		( x y -> ) - рисует линию от текуще точки рисования
+Line		( x y x1 y1 -> ) - нарисовать линию
+Square		( x y l -> ) - нарисовать квадрат
+Rect 		( x y x1 y1 -> ) - нарисовать прямоугольник
+Ellips		( x y x1 y1 -> ) - нарисовать элипс
+Circle		( x y d -> ) - нарисовать круг
+RRect		( x y x1 y1 h l -> ) - нарисовать прямоугольник c кругленными концами
+RSquare		( x y l ll lh-> ) - нарисовать квадрат  c кругленными концами
+Image		( addr u x y -> ) - вывести изображение bmp из файла на консоль
