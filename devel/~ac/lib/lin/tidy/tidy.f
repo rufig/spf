3bec7df5a3bfb6fc44b1d93804d5661c9bc9cfb4
spf <>
2007-07-11 02:07:02 +0000

    utf-8 тоже будем тайдить.

--- devel/~ac/lib/lin/tidy/tidy.f
+++ devel/~ac/lib/lin/tidy/tidy.f
@@ -52,6 +52,34 @@ ALSO DLL NEW: tidy.dll
   h CLOSE-FILE THROW
   ^ buf tidyBufFree DROP
 ;
+: TIDY_HTML { addr u \ doc  _buf0 buf0 errlen errbuf  _buf1 buf1 len buf  _buf2 buf2 ilen ibuf -- addr2 u2 }
+  tidyCreate -> doc
+  1 TidyXhtmlOut doc tidyOptSetBool DROP
+  1 TidyXmlDecl doc tidyOptSetBool DROP
+  1 TidyNumEntities doc tidyOptSetBool DROP
+  0 TidyDoctypeMode doc tidyOptSetInt DROP
+  S" utf16le" DROP TidyInCharEncoding doc tidyOptSetValue DROP
+  S" utf8" DROP TidyOutCharEncoding doc tidyOptSetValue DROP
+  ^ errbuf doc tidySetErrorBuffer DROP
+\ Tidy не умеет работать с кириллицей, поэтому конвертируем в юникод.
+  addr u DelXmlDecl
+  DUP -> ilen 2+ -> buf2 -> ibuf ^ ibuf doc tidyParseBuffer DROP
+  doc tidyCleanAndRepair DROP
+  doc tidyRunDiagnostics DROP
+  1 TidyForceOutput doc tidyOptSetBool DROP
+  ^ buf doc tidySaveBuffer DROP
+\  errbuf errlen TYPE CR
+  ^ errbuf tidyBufFree DROP
+  buf len
+;
+: TIDY_HTML_SAVE { addr u fa fu \ h _buf1 buf1 len buf -- }
+  fa fu R/W CREATE-FILE THROW -> h
+  addr u TIDY_HTML 2DUP DUP -> len -> buf1 -> buf
+  h WRITE-FILE THROW
+  h CLOSE-FILE THROW
+  ^ buf tidyBufFree DROP
+;
+
 \ S" <title>Foo</title><p>О рус!" TIDY_CYR_HTML TYPE
 \ S" <title>Foo</title><p>О рус!" S" test.xml" TIDY_CYR_HTML_SAVE
 \ S" test.xml" FILE S" test2.xml" TIDY_CYR_HTML_SAVE
