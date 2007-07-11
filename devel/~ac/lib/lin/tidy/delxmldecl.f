e565b62adb3d11ee24c42ae9eab996f98eac0159
spf <>
2007-07-11 02:10:12 +0000

    В libxslt неудобно конвертить html, если у него указан xmlns. Сделаем возможность удаления xmlns (применять DelXhtmlNs перед XSLTm).

--- devel/~ac/lib/lin/tidy/delxmldecl.f
+++ devel/~ac/lib/lin/tidy/delxmldecl.f
@@ -39,4 +39,25 @@ REQUIRE STR@          ~ac/lib/str5.f
   REPEAT
   2DROP
   a u
+  BEGIN
+    S' <?xml version="1.0" encoding="UTF-8"?>' DUP -> len
+    SEARCH 
+  WHILE
+    OVER len BLANK
+    len - 0 MAX SWAP len + SWAP
+  REPEAT
+  2DROP
+  a u
+;
+: DelXhtmlNs { a u \ len -- a u }
+  a u
+  BEGIN
+    S' xmlns="http://www.w3.org/1999/xhtml"' DUP -> len
+    SEARCH
+  WHILE
+    OVER len BLANK
+    len - 0 MAX SWAP len + SWAP
+  REPEAT
+  2DROP
+  a u
 ;
