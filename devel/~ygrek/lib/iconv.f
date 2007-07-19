ac9084ea19e172e3ae82a800a88622cbd767b7ef
ygreks <>
2007-07-19 19:30:17 +0000

    S" UTF-8" S" CP1251" iconv: UTF>WIN

--- devel/~ygrek/lib/iconv.f
+++ devel/~ygrek/lib/iconv.f
@@ -2,30 +2,64 @@
 \ Обёртка к iconv
 \ more to do
 
-REQUIRE SO ~ac/lib/ns/so-xt.f
+REQUIRE CAPI: ~af/lib/c/capi.f
 REQUIRE STR@ ~ac/lib/str5.f
 REQUIRE /TEST ~profit/lib/testing.f
 
-ALSO SO NEW: iconv.dll
-( libiconv_open libiconv libiconv_close)
+5 CAPI: libiconv       iconv.dll
+2 CAPI: libiconv_open  iconv.dll
+1 CAPI: libiconv_close iconv.dll
 
-: UTF8>CP1251 { a u | a2 u2 cd -- s } 
-   S" UTF-8" DROP S" CP1251" DROP 2 libiconv_open DUP -1 = ABORT" No such conversion"
-   -> cd
-   u 1 * -> u2
-   PAD -> a2
-   ^ u2 ^ a2 ^ u ^ a cd 5 libiconv -1 = ABORT" Conversion failed"
-   cd 1 libiconv_close ABORT" cd close failed"
-   "" >R
-   PAD a2 OVER - R@ STR!
-   R> ;
+WINAPI: _errno MSVCRT.DLL
 
+7 CONSTANT E2BIG
+22 CONSTANT EINVAL
+84 CONSTANT EILSEQ
 
-PREVIOUS
+MODULE: iconv
+
+: convert { a u cd | buf addr size u2 s -- s }
+   u 10 MAX -> size \ для слишком короткой входной строки гарантируем достаточный размер буфера
+   size ALLOCATE THROW -> buf
+   "" -> s
+   0 0 0 0 cd libiconv DROP \ set to initial state
+   BEGIN
+    size -> u2
+    buf -> addr
+    ^ u2 ^ addr ^ u ^ a cd libiconv -1 = 
+    buf addr OVER - s STR+ \ 
+   WHILE
+    _errno @ E2BIG <> ABORT" iconv cant convert"
+   REPEAT 
+   buf FREE THROW 
+   s ;
+
+EXPORT
+
+: iconv: ( a1 u1 a2 u2 "name" -- ) 
+\ name ( a u -- s )
+  CREATE
+    2SWAP S", 0 C, S", 0 C,
+   DOES> ( a u addr -- s )
+    >R R@ COUNT R> + 2 + COUNT DROP libiconv_open DUP -1 = ABORT" No such conversion"
+    >R 
+    R@ convert
+    R> libiconv_close ABORT" iconv close failed"
+   ;
+
+;MODULE   
+
+S" UTF-8" S" CP1251" iconv: UTF>WIN
+S" CP1251" S" UTF-8" iconv: WIN>UTF
 
 \ -------------------------------------------------
 
 /TEST
 
-S" Привет" UTF8>CP1251 CR STYPE
+REQUIRE TESTCASES ~ygrek/lib/testcase.f
+
+TESTCASES UTF>WIN and back                                                                                          
+(( S" ����" WIN>UTF DUP STR@ S" Тест" TEST-ARRAY STRFREE -> ))                                                      
+(( S" Тест" UTF>WIN DUP STR@ S" ����" TEST-ARRAY STRFREE -> ))                                                      
+END-TESTCASES
 
