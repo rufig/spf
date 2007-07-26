5a43e98f7a9d797078b76d5743837ce8e7d82ee1
ruv <>
2007-07-26 16:01:05 +0000

    Перенос исправлений EXC-DUMP в ядро -- более правильное вычисление области регистров (теперь верно работает под wine и x64), подправлена эвристика вычисления значения ESP на момент исключения,  добавлена цепочка AT-EXC-DUMP, добавлен вывод стека данных, шестнадцатиричный дамп регистров убран (добавляется через AT-EXC-DUMP).

--- src/spf_init.f
+++ src/spf_init.f
@@ -1,3 +1,4 @@
+\ $Id$
 ( Инициализация USER-переменных.
   Copyright [C] 1992-1999 A.Cherezov ac@forth.org
   Сентябрь 1999
@@ -83,40 +84,48 @@ VARIABLE IN-EXCEPTION
       ELSE DUP U. WordByAddr TYPE CR THEN
 ;
 
+: AT-EXC-DUMP ( addr -- addr ) ... ;
+\ example: ."  REGISTERS:" DUP 12 CELLS DUMP CR
+
 : EXC-DUMP1 ( exc-info -- ) 
   IN-EXCEPTION @ IF DROP EXIT THEN
   TRUE IN-EXCEPTION !
-  BASE @ SWAP
-  HEX
-  ." EXCEPTION! " 
-  DUP @ ."  CODE:" U. 
-  DUP 3 CELLS + @ ."  ADDRESS:" DUP U. ."  WORD:" WordByAddr TYPE SPACE
-  ."  REGISTERS:"
-  DUP 4 CELLS + @ CELLS + \ может быть указано смещение структуры на 2 CELLS
-  176 + DUP 12 CELLS DUMP CR
+  BASE @ >R HEX
+
+  ." EXCEPTION! "
+  DUP @ ."  CODE:" U.
+  DUP 3 CELLS + @ ."  ADDRESS:" DUP U.  ."  WORD:" WordByAddr TYPE CR
+
+  ( DispatcherContext ContextRecord EstablisherFrame ExceptionRecord  ExceptionRecord )
+  DROP 2 PICK
+
+  8 CELLS 80 + \ FLOATING_SAVE_AREA
+    11 CELLS + \ сдвиг оносительно контекст к регистрам, начиная с edi
+  + \ вычисление базового адреса образа регистров (~ygrek)
+
+  AT-EXC-DUMP ( addr -- addr )
+
   ." USER DATA: " TlsIndex@ U. ." THREAD ID: " 36 FS@ U.
-  ." HANDLER: " HANDLER @ U.
+  ." HANDLER: " HANDLER @ U. CR
+  ." STACK: "
+  DUP 6 CELLS + @ ( ebp )
+  DUP 5 CELLS + BEGIN DUP ['] @ CATCH IF DROP ELSE 8 .0 SPACE THEN CELL- 2DUP U> UNTIL 2DROP
+  ." ["
+  DUP 5 CELLS + @ ( eax ) 8 .0 ." ]" CR
+
   ." RETURN STACK:" CR
-  6 CELLS + DUP @ \ DUP 65000 > IF NIP ELSE DROP CELL+ CELL+ @ THEN
-  DUP HANDLER @ U< IF NIP ELSE DROP 4 CELLS + @ THEN
-  DUP HANDLER @ U<
-  IF
-    25 >R
-    BEGIN
-      DUP HANDLER @ CELL+ U< R@ 0 > AND
-    WHILE
-      STACK-ADDR.
-      CELL+ R> 1- >R
-    REPEAT DROP RDROP
-  ELSE
-    DROP
-    50 0 DO 
-      R0 @ I 1+ CELLS - STACK-ADDR. DROP
-    LOOP
-  THEN
-  ." END OF EXCEPTION REPORT" CR  
-  BASE !
-  FALSE IN-EXCEPTION !
+  HANDLER @ DUP 0= IF DROP R0 @ THEN ( up-border ) >R
+  6 CELLS + DUP @  SWAP  4 CELLS + @ ( a1 a2 )
+  \ берем ближайший снизу к up-border:
+  2DUP U> IF SWAP THEN ( min max ) DUP R@ U< IF NIP ELSE DROP THEN ( low-border ) R>
+  ( low-border up-border )
+  2DUP U< IF OVER 25 CELLS + UMIN SWAP ELSE 2DROP R0 @ DUP 50 CELLS - THEN
+  ( up low )
+  BEGIN 2DUP U< 0= WHILE STACK-ADDR. CELL+ REPEAT 2DROP
+
+  
+  ." END OF EXCEPTION REPORT" CR
+  R> BASE !  FALSE IN-EXCEPTION !
 ;
 ' EXC-DUMP1 (TO) <EXC-DUMP>
 
