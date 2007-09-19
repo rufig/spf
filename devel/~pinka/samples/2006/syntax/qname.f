2d56398749d6a97f897266f69f44f4ae3b2ec806
ruv <>
2007-09-19 13:34:03 +0000

    Отдано преимущество транслятору однословных строк (через preemptNOTFOUND), а то невозможно было задать "`::" — ругался -2011 Unknown vocabulary.

--- devel/~pinka/samples/2006/syntax/qname.f
+++ devel/~pinka/samples/2006/syntax/qname.f
@@ -1,10 +1,11 @@
 \ 10.Feb.2006 Fri 20:05
+\ quoted name
 
 REQUIRE enqueueNOTFOUND ~pinka\samples\2006\core\trans\nf-ext.f 
 
 REQUIRE AsQName ~pinka\samples\2006\syntax\qname.core.f
 
-' AsQName enqueueNOTFOUND
+' AsQName preemptNOTFOUND
 
 \ ”пращение, вместо записи S" name" теперь можно записать `name
 \ (отличать от 'name ! )
