1117e25dd8e473f579765150933e03b2080fed59
ruv <>
2007-09-19 13:24:48 +0000

    + preemptNOTFOUND — добавиляет заданный в начало списка трансляторов 'NOTFOUND'

--- devel/~pinka/samples/2006/core/trans/nf-ext.f
+++ devel/~pinka/samples/2006/core/trans/nf-ext.f
@@ -1,7 +1,8 @@
 \ 10.Feb.2006 Fri 20:44
+\ Unobtrusive 'NotFound' Extension
 
 : enqueueNOTFOUND ( xt -- )
-\ добавить в конец списка трансляторов
+\ добавить в конец списка трансляторов слова
 \ транслятор, заданный xt
 \ xt ( a u -- a u false | n*x true )
 
@@ -23,3 +24,19 @@
 \ where AsQName stack notation
 \   on STATE0 is ( i*x  a-text u-text -- j*x  true | i*x  a-text u-text false )
 \   otherwise is ( a-text u-text -- true | a-text u-text false )
+
+
+: preemptNOTFOUND ( xt -- )
+\ добавить в начало списка трансляторов 'NOTFOUND'
+\ транслятор, заданный xt
+\ xt ( a u -- a u false | n*x true )
+
+  S" NOTFOUND" SFIND 0= IF 2DROP 0 THEN
+    WARNING @ >R WARNING 0!
+  S" NOTFOUND" CREATED , ,
+    R> WARNING !
+  DOES> ( a u  a1 )
+    DUP >R CELL+ @ EXECUTE IF RDROP EXIT THEN
+    R> @ DUP IF EXECUTE EXIT THEN
+    -2003 THROW
+;
