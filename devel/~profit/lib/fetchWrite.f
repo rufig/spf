326bd04dbb5e886c7bdde905ec2948bd093b715a
azekeprofit <>
2007-10-17 13:56:49 +0000

    testcases added

--- devel/~profit/lib/fetchWrite.f
+++ devel/~profit/lib/fetchWrite.f
@@ -1,13 +1,12 @@
 REQUIRE /TEST ~profit/lib/testing.f
 
-\ TODO: ѕочему на таких простых словах стопаритс€ оптимизатор?..
-
 \ ѕоследовательное чтение и запись в пам€ть с бегунком, хран€щим адрес текущей €чейки
 \ ѕеременные-бегунки -- хороша€ замена дл€ регистра-аккумул€тора с функци€ми @+ !+
+\ (см. MachineForth)
 
-\ Ѕегунками можно (даже нужно) делать локальные переменные, только тогда нужно
-\ использовать их слегка не так как обычно (см. пример move), чтобы локальлыне 
-\ переменные были обычными, а не VALUE-переменными.
+\ Ѕегунки можно (даже нужно) делать локальными переменными, только тогда нужно
+\ использовать их слегка не так как обычно (см. пример move), чтобы локальные
+\ переменные были обычными VARIABLE, а не VALUE-переменными.
 
 : fetchByte ( addr -- b ) DUP  @ C@ SWAP 1+! ;
 : writeByte ( n addr -- ) TUCK @ C!      1+! ;
@@ -15,44 +14,75 @@ REQUIRE /TEST ~profit/lib/testing.f
 : fetchWord ( addr -- b ) DUP  @ W@ SWAP 2 SWAP +! ;
 : writeWord ( n addr -- ) TUCK @ W!      2 SWAP +! ;
 
-: fetchCell ( addr -- b ) DUP  @ W@ SWAP CELL SWAP +! ;
+: fetchCell ( addr -- b ) DUP  @ @  SWAP CELL SWAP +! ;
 : writeCell ( n addr -- ) TUCK @ !       CELL SWAP +! ;
 
 
 /TEST
 
-CREATE m 10 ALLOT
+REQUIRE TESTCASES ~ygrek/lib/testcase.f
+
+REQUIRE { lib/ext/locals.f
+
+TESTCASES fetchWrite tests
+
+CREATE m 20 ALLOT
 
 VARIABLE a
 
 m a !
-1 a writeByte
-2 a writeByte
-3 a writeByte
-
-
+01 a writeByte
+22 a writeByte
+03 a writeByte
 
 m a !
-a fetchByte .
-a fetchByte .
-a fetchByte .
+(( a fetchByte -> 1 ))
+(( a fetchByte -> 22 ))
+(( a fetchByte -> 3 ))
 
-REQUIRE { lib/ext/locals.f
-REQUIRE FOR ~profit/lib/for-next.f
+m a !
+01 a writeWord
+22 a writeWord
+03 a writeWord
 
+m a !
+(( a fetchWord -> 1 ))
+(( a fetchWord -> 22 ))
+(( a fetchWord -> 3 ))
+
+m a !
+01 a writeCell
+22 a writeCell
+03 a writeCell
+
+m a !
+(( a fetchCell -> 1 ))
+(( a fetchCell -> 22 ))
+(( a fetchCell -> 3 ))
+
+m a !
+01 a writeByte
+22 a writeWord
+03 a writeCell
+
+m a !
+(( a fetchByte -> 1 ))
+(( a fetchWord -> 22 ))
+(( a fetchCell -> 3 ))
+
+\ ѕишем аналог слова MOVE с помощью бегунков:
 : move ( src len dest -- ) { \ [ CELL ] A [ CELL ] B -- }
-B !  SWAP A ! 0 DO 
-A fetchByte DUP CR EMIT ( x ) B writeByte
+B !  SWAP A ! 0 DO
+A fetchByte ( x ) B writeByte
 LOOP ;
 
-CREATE tmp 1000 ALLOT ALIGN
-: r S" check00" tmp move ;
-r
-tmp 20 DUMP
+CREATE tmp 1000 ALLOT
+: check S" check00" ;
+check tmp move
 
-lib\ext\disasm.f
-SEE writeByte
-SEE writeWord
-VARIABLE s
-:NONAME s writeByte ; REST
-:NONAME s writeWord ; REST
+check tmp OVER TEST-ARRAY
+
+END-TESTCASES
+
+\ lib/ext/disasm.f
+\ SEE move
--- devel/~profit/lib/utf8.f
+++ devel/~profit/lib/utf8.f
@@ -6,6 +6,8 @@ REQUIRE <= ~profit/lib/logic.f
 REQUIRE { lib/ext/locals.f
 REQUIRE fetchByte ~profit/lib/fetchWrite.f
 
+MODULE: utf8
+
 BASE @  2 BASE !
 
 : byte ( n -- n1 ) 11111111 AND ;
@@ -44,12 +46,19 @@ R@ fetchByte 111111 AND +
        THEN
 RDROP ;
 
-: utf8Move ( addr -- addr wchar ) SP@ utf8Next ;
-
 BASE !
 
+EXPORT
+
+
+: utf8Next ( addr -- wchar ) utf8Next ;
+
+
+: utf8Move ( addr -- addr wchar ) SP@ utf8Next ;
+
+
 \ addr u -- UTF-8 последовательность
-\ buf -- буфер, в который складываютс€ символы в виде двубайтого Unicode
+\ buf -- буфер, в который складываютс€ символы в виде двубайтного Unicode
 \ “ак как мы не знаем до декодировани€ кол-ва символов в UTF-8 последовательности,
 \ то дл€ buf следует занимать u*2 байтов. ѕосле вычислени€, получив end, буфер
 \ можно сократить
@@ -63,20 +72,88 @@ REPEAT
 B @ 2 - ; \ end -- адрес введЄнного последнего символа
 
 
+
+\ Ќа входе: utf8 последовательность addr1 u1
+\ Ќа выходе: вз€тый из кучи отрезок строки addr2 u2 равный Unicode (16-битовому) представлению строки на входе
+\ ¬нимание: размер вз€той из кучи пам€ти всегда равен (u1*2)+2, хот€, как правило, она занимает меньше места.
+\ +2 -- это дл€ конечного нулевого 16-битного слова (чтобы выходна€ последовательность €вл€лась Wide ASCIIZ-строкой)
+\ —амо собой, что убирать выходную строку из кучи за вас никто не будет.
+: utf8>uni ( addr1 u1 -- addr2 u2 )
+DUP 1+ 2* DUP ALLOCATE THROW TUCK + CELL- 0!
+DUP >R utf8Decode R> TUCK - ;
+
+;MODULE
+
 /TEST
-2 BASE !
-CREATE a
-1010 C, 
-11001010 C, 10101010 C,
-11101010 C, 10101010 C, 10101010 C,
-11110101 C, 10101010 C, 10101010 C, 10101010 C,
-DECIMAL
 
-CREATE b 3 2* ALLOT
+REQUIRE TESTCASES ~ygrek/lib/testcase.f
 
-a 3 b utf8Decode DROP
-b 20 DUMP
+TESTCASES utf8>uni test
 
-lib\ext\disasm.f 
-SEE utf8Decode
-SEE utf8Next
+\ simple ansi text
+
+: uni CHAR C, 0 C, ;
+
+HERE
+uni f uni o uni o
+HERE OVER - ( addr u )
+S" foo" utf8>uni TEST-ARRAY
+
+
+\ russian text
+
+HERE
+208 C, 159 C, 209 C, 128 C, 208 C, 184 C, 208 C, 178 C, 208 C, 181 C, 209 C,
+130 C, 44 C, 32 C, 208 C, 186 C, 209 C, 131 C, 45 C, 208 C, 186 C, 209 C, 131 C,
+33 C, 46 C, 46 C,
+HERE OVER - ( addr u ) utf8>uni
+
+HERE
+31 C, 4 C, 64 C, 4 C, 56 C, 4 C, 50 C, 4 C, 53 C, 4 C, 66 C, 4 C, 44 C, 0 C, 32
+C, 0 C, 58 C, 4 C, 67 C, 4 C, 45 C, 0 C, 58 C, 4 C, 67 C, 4 C, 33 C, 0 C, 46 C,
+0 C, 46 C, 0 C,
+HERE OVER -
+
+TEST-ARRAY
+
+
+\ kazakh text
+HERE
+208 C, 156 C, 208 C, 181 C, 208 C, 189 C, 32 C, 210 C, 155 C, 208 C, 176 C, 208
+C, 183 C, 208 C, 176 C, 210 C, 155 C, 32 C, 210 C, 155 C, 209 C, 139 C, 208 C, 183 C,
+208 C, 180 C, 208 C, 176 C, 209 C, 128 C, 209 C, 139 C, 208 C, 189 C, 208 C,
+176 C, 208 C, 189 C, 32 C, 210 C, 155 C, 208 C, 176 C, 208 C, 185 C, 209 C, 128 C,
+208 C, 176 C, 208 C, 189 C, 32 C, 210 C, 155 C, 208 C, 176 C, 208 C, 187 C,
+208 C, 176 C, 208 C, 188 C,
+HERE OVER - ( addr u ) utf8>uni
+
+HERE
+28 C, 4 C, 53 C, 4 C, 61 C, 4 C, 32 C, 0 C, 155 C, 4 C, 48 C, 4 C, 55 C, 4 C,
+48 C, 4 C, 155 C, 4 C, 32 C, 0 C, 155 C, 4 C, 75 C, 4 C, 55 C, 4 C, 52 C, 4 C,
+48 C, 4 C, 64 C, 4 C, 75 C, 4 C, 61 C, 4 C, 48 C, 4 C, 61 C, 4 C, 32 C, 0 C, 155 C,
+4 C, 48 C, 4 C, 57 C, 4 C, 64 C, 4 C, 48 C, 4 C, 61 C, 4 C, 32 C, 0 C, 155 C,
+4 C, 48 C, 4 C, 59 C, 4 C, 48 C, 4 C, 60 C, 4 C,
+HERE OVER - 
+
+TEST-ARRAY
+
+
+\ chinese text
+HERE
+233 C, 157 C, 158 C, 230 C, 136 C, 152 C, 228 C, 184 C, 141 C, 229 C, 177 C, 136 C,
+HERE OVER - ( addr u ) utf8>uni
+
+HERE
+94 C, 151 C, 24 C, 98 C, 13 C, 78 C, 72 C, 92 C,
+HERE OVER - ( addr u )
+
+TEST-ARRAY
+
+END-TESTCASES
+
+\EOF
+
+\ lib\ext\disasm.f
+\ SEE utf8Decode
+\ SEE utf8Next
+\ SEE utf8>uni
