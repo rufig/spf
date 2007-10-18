8cd65b787f60d73763dfa7b32d21a7e630dccf05
azekeprofit <>
2007-10-18 13:06:55 +0000

    testcase added

--- devel/~profit/lib/for-next.f
+++ devel/~profit/lib/for-next.f
@@ -16,6 +16,21 @@ POSTPONE RDROP
 DECIMAL
 
 /TEST
+
+
+REQUIRE TESTCASES ~ygrek/lib/testcase.f
+REQUIRE TYPE>STR ~ygrek/lib/typestr.f
+
+
+TESTCASES for next
+
+:NONAME 10 ." [" FOR R@ . NEXT ." ]" ; TYPE>STR STR@
+S" [10 9 8 7 6 5 4 3 2 1 0 ]" TEST-ARRAY
+
+END-TESTCASES
+
+\EOF
+
 : r 10 FOR R@ . NEXT ;
 
 REQUIRE SEE lib/ext/disasm.f
