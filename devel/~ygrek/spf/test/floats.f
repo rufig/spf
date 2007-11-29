ce55f269fc39a9790436652dde9936716e0267c5
ygreks <>
2007-11-29 21:24:33 +0000

    Testcases for core spf words. A start.

---
+++ devel/~ygrek/spf/test/floats.f
@@ -0,0 +1,13 @@
+\ $Id$
+
+lib/include/float2.f
+~ygrek/lib/testcase.f
+
+TESTCASES floats
+(( 0.99e 1.00e F< -> TRUE ))
+(( 1.00e 1.00e F< -> FALSE ))
+(( 1.01e 1.00e F< -> FALSE ))
+(( 12.3e FLOAT>DATA DEPTH NIP NIP FDEPTH -> 2 0 ))
+(( 12.3e FLOAT>DATA32 DEPTH NIP FDEPTH -> 1 0 ))
+END-TESTCASES
+
