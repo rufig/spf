11ecdba4d50b9cfe565a00c884dd81bb24d36477
spf <>
2007-12-05 23:03:00 +0000

    Тоже преобразуем результат в форт-строку и чистим ресурсы libxml/xsl сразу.

--- devel/~ac/lib/lin/xml/xslt.f
+++ devel/~ac/lib/lin/xml/xslt.f
@@ -6,24 +6,48 @@ REQUIRE UNICODE>UTF8  ~ac/lib/win/com/com.f
 WARNING !
 
 ALSO SO NEW: libxslt.dll
+ALSO libxml2.dll
 
 : XSLT { xaddr xu saddr su \ style doc res len str -- addr u }
 \ преобразовать XML-файл с именем/url xaddr xu с использованием XSL-файла saddr su
   saddr 1 xsltParseStylesheetFile DUP -> style 0= IF 60002 THROW THEN
   xaddr xu XML_READ_DOC DUP -> doc 0= IF 60003 THROW THEN
   0 doc style 3 xsltApplyStylesheet DUP -> res 0= IF 60004 THROW THEN
-  style res ^ len ^ str 4 xsltSaveResultToString DROP str len
+  style res ^ len ^ str 4 xsltSaveResultToString DROP str len " {s}" STR@
 \  0 style res S" test11.xml" DROP 4 xsltSaveResultToFilename .
+  style 1 xsltFreeStylesheet DROP
+  res 1 xmlFreeDoc DROP
+  doc 1 xmlFreeDoc DROP
+  0 xsltCleanupGlobals DROP
+  0 xmlCleanupParser DROP
 ;
 : XSLTm { xaddr xu saddr su \ style doc res len str -- addr u }
 \ преобразовать XML-строку xaddr xu с использованием XSL-файла saddr su
   saddr 1 xsltParseStylesheetFile DUP -> style 0= IF 60002 THROW THEN
   xaddr xu XML_READ_DOC_MEM DUP -> doc 0= IF 60005 THROW THEN
   0 doc style 3 xsltApplyStylesheet DUP -> res 0= IF 60004 THROW THEN
-  style res ^ len ^ str 4 xsltSaveResultToString DROP str len
+  style res ^ len ^ str 4 xsltSaveResultToString DROP str len " {s}" STR@
 \  0 style res S" test11.xml" DROP 4 xsltSaveResultToFilename .
+  style 1 xsltFreeStylesheet DROP
+  res 1 xmlFreeDoc DROP
+  doc 1 xmlFreeDoc DROP
+  0 xsltCleanupGlobals DROP
+  0 xmlCleanupParser DROP
 ;
-PREVIOUS
+: XSLTmm { xaddr xu saddr su \ sdoc style doc res len str -- addr u }
+\ преобразовать XML-строку xaddr xu с использованием XSL-СТРОКИ saddr su
+  saddr su XML_READ_DOC_MEM DUP -> sdoc 0= IF 60006 THROW THEN
+  sdoc 1 xsltParseStylesheetDoc DUP -> style 0= IF 60002 THROW THEN
+  xaddr xu XML_READ_DOC_MEM DUP -> doc 0= IF 60005 THROW THEN
+  0 doc style 3 xsltApplyStylesheet DUP -> res 0= IF 60004 THROW THEN
+  style res ^ len ^ str 4 xsltSaveResultToString DROP str len " {s}" STR@
+  style 1 xsltFreeStylesheet DROP
+  res 1 xmlFreeDoc DROP
+  doc 1 xmlFreeDoc DROP
+  0 xsltCleanupGlobals DROP
+  0 xmlCleanupParser DROP
+;
+PREVIOUS PREVIOUS
 
 \ S" 1.xml" S" 1.xsl" XSLT
 \ Здесь запись имя:пароль поддерживается через curl, а сам libxml не умеет.
