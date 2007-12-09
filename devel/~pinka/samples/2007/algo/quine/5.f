3339cab71b7644e695dd69e0def692c058a14d6b
ruv <>
2007-12-09 21:27:22 +0000

    + вариант, полностью отложенный в слово Q

---
+++ devel/~pinka/samples/2007/algo/quine/5.f
@@ -0,0 +1,2 @@
+: T TYPE ; : E EMIT ; : B SPACE ; : S 83 E 34 E B T 34 E B ; : O 2DUP T CR  58 E B 81 E B  S  79 E B 59 E B 81 E ;
+: Q S" : T TYPE ; : E EMIT ; : B SPACE ; : S 83 E 34 E B T 34 E B ; : O 2DUP T CR  58 E B 81 E B  S  79 E B 59 E B 81 E ;" O ; Q
