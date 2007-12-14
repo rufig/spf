f9375169af63dd89d3bbbd8a5d510eb2619d2bea
moleg <>
2007-12-14 13:52:34 +0000

    no message

---
+++ devel/~moleg/samples/2007-12-10/mak4444.f
@@ -0,0 +1,43 @@
+\
+\
+\
+
+REQUIRE { devel\~mak\locals4.f
+
+\ вернуть количество бит в стандартной ячейке
+: bits/cell ( --> u ) 0 -1 BEGIN TUCK WHILE 1+ SWAP 2* REPEAT NIP ;
+
+        0 VALUE buffer \ адрес места, где хранится результирующий массив
+
+\ выделение памяти под массив и его инициализация
+ 1 bits/cell 4 / LSHIFT CELLS CELL + ALLOCATE THROW DUP TO buffer 0!
+
+\
+: aCount ( addr --> addr # ) DUP CELL + SWAP @ ;
+
+\ отправить значение в буфер
+: ->buf ( u --> )
+        buffer aCount + !
+        CELL buffer +! ;
+
+
+: MINBIT
+  DUP
+  INVERT 1+
+  XOR
+ INVERT 1+ 1 RSHIFT
+;
+
+: TEMPLATE! { tmpl \ it nt -- }
+tmpl INVERT TO it
+tmpl MINBIT TO nt
+0
+BEGIN
+ it OR
+ nt +
+ tmpl AND DUP ->buf
+ DUP 0=
+UNTIL DROP
+;
+
+: combs ( um --> addr # ) 0 buffer ! TEMPLATE! buffer aCount ;
