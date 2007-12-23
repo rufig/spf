fbe7d23166d3f638adbcb75a1748f8ec98ace131
ygreks <>
2007-12-23 08:53:43 +0000

    get rid of WINAPI: CharLower, numeric constants converted to meaningful equivalents

--- lib/ext/caseins.f
+++ lib/ext/caseins.f
@@ -1,3 +1,4 @@
+\ $Id$
 ( Case insensitivity for SP-FORTH )
 ( CASE-INS - case sensitivity switcher )
 ( just include this lib :)
@@ -5,21 +6,11 @@
 REQUIRE REPLACE-WORD lib\ext\patch.f
 REQUIRE ON           lib\ext\onoff.f
 REQUIRE [IF]         lib\include\tools.f \ должно быть подключено до caseins-варианта
+REQUIRE COMPARE-U    ~ac/lib/string/compare-u.f
 REQUIRE [else]       lib\ext\caseins-tools.f
 
 VARIABLE CASE-INS \ switcher
 CASE-INS ON
-
-WINAPI: CharLowerA USER32.DLL
-
-: UCOMPARE ( addr1 u1 addr2 u2 -- 0 | -1 )
-  ROT OVER <> IF DROP 2DROP -1 EXIT THEN
-  0 DO
-      2DUP C@ CharLowerA SWAP C@ CharLowerA <>
-      IF UNLOOP 2DROP -1 EXIT THEN
-      1+ SWAP 1+
-    LOOP 2DROP 0
-;
 
 : USEARCH-WORDLIST ( c-addr u wid -- 0 | xt 1 | xt -1 )
   CASE-INS @ 0= IF
@@ -30,7 +21,7 @@ WINAPI: CharLowerA USER32.DLL
     DUP
   WHILE
     >R 2DUP
-    R@ COUNT UCOMPARE 0= 
+    R@ COUNT COMPARE-U 0= 
     IF 2DROP R@ NAME> R> ?IMMEDIATE IF 1 ELSE -1 THEN EXIT THEN
     R> CDR
   REPEAT DROP 2DROP 0
@@ -38,21 +29,22 @@ WINAPI: CharLowerA USER32.DLL
 
 ' USEARCH-WORDLIST TO SEARCH-WORDLIST
 
-: UDIGIT ( C, N1 -> N2, TF / FF ) 
+: UDIGIT ( C N1 -- N2 -1|0 ) 
 \ N2 - значение литеры C как
 \ цифры в системе счисления по основанию N1
 \ hex-цифры могут быть строчными
   SWAP
-  DUP 0x3A < OVER 0x2F > AND
+  DUP [CHAR] 0 [CHAR] 9 1+ WITHIN
   IF \ within 0..9
-     0x30 -
+     [CHAR] 0 -
   ELSE
-     DUP 0x40 >
+     DUP [CHAR] A 1- >
      IF
-       DUP 0x60 > IF
-         CASE-INS @ IF 0x57 ELSE 2DROP 0 EXIT THEN
-       ELSE 0x37 THEN
-       -
+       DUP [CHAR] a 1- > 
+       IF 
+         CASE-INS @ IF [CHAR] a ELSE 2DROP 0 EXIT THEN
+       ELSE [CHAR] A THEN
+       - 10 +
      ELSE 2DROP 0 EXIT THEN
   THEN
   TUCK > DUP 0= IF NIP THEN
