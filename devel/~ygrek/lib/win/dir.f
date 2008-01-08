ad419f9e611446f140c62a5ffb9501875e9c3c1d
ygreks <>
2008-01-08 17:16:46 +0000

    DELETE-FILE-ALWAYS

--- devel/~ygrek/lib/win/dir.f
+++ devel/~ygrek/lib/win/dir.f
@@ -2,10 +2,11 @@
 
 REQUIRE ANSI-FILE lib/include/ansi-file.f
 REQUIRE ITERATE-FILES ~profit/lib/iterate-files.f
+REQUIRE ?WINAPI: ~ygrek/lib/win/winapi.f
 
-\ FIXME DELETE-FILE fails to delete read-only files
-
-WINAPI: RemoveDirectoryA KERNEL32.DLL
+?WINAPI: RemoveDirectoryA KERNEL32.DLL
+?WINAPI: GetFileAttributesA KERNEL32.DLL
+?WINAPI: SetFileAttributesA KERNEL32.DLL
 
 ALSO ANSI-FILE
 
@@ -13,6 +14,27 @@ ALSO ANSI-FILE
 : DELETE-EMPTY-DIR ( a u -- ior ) >ZFILENAME DROP RemoveDirectoryA ERR ;
 
 PREVIOUS
+
+5 CONSTANT ERROR_ACCESS_DENIED
+1 CONSTANT FILE_ATTRIBUTE_READONLY
+
+\ If the file deletion fails due to r/o attribute
+\ try to remove r/o and delete again (if it fails again - attribute is restored)
+: DELETE-FILE-ALWAYS ( a u -- ior )
+   { | name attrs }
+   ANSI-FILE::>ZFILENAME DROP -> name
+   name DeleteFileA IF 0 EXIT THEN
+   GetLastError DUP ERROR_ACCESS_DENIED <> IF ( error ) EXIT THEN DROP
+   \ probably because of r/o attribute?
+   name GetFileAttributesA -> attrs
+   attrs FILE_ATTRIBUTE_READONLY AND 0= IF ERROR_ACCESS_DENIED EXIT THEN
+   \ if so - try to remove it
+   attrs FILE_ATTRIBUTE_READONLY INVERT AND 
+   name SetFileAttributesA 0= IF ERROR_ACCESS_DENIED EXIT THEN
+   name DeleteFileA ERR
+   \ failed to delete even without r/o - restore it
+   DUP IF attrs FILE_ATTRIBUTE_READONLY OR name SetFileAttributesA DROP THEN 
+   ( ior ) ;
 
 \ Remove directory recursively
 : DELETE-DIR ( a u -- )
@@ -22,7 +44,7 @@ PREVIOUS
     IF
      RECURSE
     ELSE
-     DELETE-FILE DROP 
+     DELETE-FILE-ALWAYS DROP 
     THEN
    }EMERGE 
    DELETE-EMPTY-DIR DROP ;
