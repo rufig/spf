46d76db7e6a8fd4fd1aee4319ca93cd41e4e3e36
ruv <>
2008-01-13 15:43:27 +0000

    * CEQUAL-U -- делать >CHARS ненадо, там и так в символах длина

--- lib/ext/caseins-tools.f
+++ lib/ext/caseins-tools.f
@@ -4,7 +4,7 @@ REQUIRE CHAR-UPPERCASE ~ac/lib/string/uppercase.f
 \ TRUE - strings are equal ignoring case
 : CEQUAL-U ( a1 u1 a2 u2 -- flag )
   ROT TUCK <> IF DROP 2DROP FALSE EXIT THEN
-  >CHARS 0 ?DO ( a1i a2i ) 2DUP
+  0 ?DO ( a1i a2i ) 2DUP
   C@ CHAR-UPPERCASE SWAP C@ CHAR-UPPERCASE <> IF 2DROP UNLOOP FALSE EXIT THEN
   SWAP CHAR+ SWAP CHAR+
   LOOP 2DROP TRUE
