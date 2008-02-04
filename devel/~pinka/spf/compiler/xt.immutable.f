43cc5fee63b0e8f341b3c6be608b12ac61ba6766
ruv <>
2008-02-04 19:47:55 +0000

    Рефакторинг (выделено native-wordlist.f).

--- devel/~pinka/spf/compiler/xt.immutable.f
+++ devel/~pinka/spf/compiler/xt.immutable.f
@@ -10,29 +10,9 @@
   RET, GERM  CS> GERM!
   \ AT-BIRTH ( xt -- xt ) \ is event
 ;
-: NAMING ( c-addr u xt -- ) \ NAMED GIVE-NAME ?
-\ дает имя и зачисляет его в текущую книгу имен :)
-\ сразу после этого имя находимо.
-
-  HERE SWAP ,  LAST-CFA !
-  0 C,     \ flags
-  \ +SWORD was here
-  HERE >R S",  R@ GET-CURRENT DUP @ , !
-  R> LAST !
+: MAKE-CONST ( x -- xt ) \ xt ( -- x )
+  CONCEIVE LIT, BIRTH
 ;
-
-[DEFINED] QuickSWL-Support  [IF]
-WARNING @  WARNING 0!
-: NAMING NAMING LAST @ GET-CURRENT QuickSWL-Support::update1-wlhash ;
-WARNING !                   [THEN]
-
-: NAMING- ( xt c-addr u -- ) ROT NAMING ;
-
-: ALIAS ( xt c-addr u -- ) NAMING- ;
-
-\ : DEFER-NAMING ( c-addr u -- entry ) 0 , 0 C, HERE -ROT S", 0 , ;
-\ ~ BIND-NAME ( xt entry -- ) TUCK 5 - ! .....  ;
-
 
 \EOF
 \ дополнительные (в процессе поиска формы), пока не используются:
@@ -45,12 +25,4 @@ WARNING !                   [THEN]
 ;
 : MAKE-SLOT ( -- xt )  \ xt ( -- addr )
   ALIGN HERE 0 , MAKE-LIT
-;
-
-: DEVELOP ( wid -- ) ( CS: -- wid-prev )
-  GET-CURRENT >CS
-  ALSO CONTEXT ! DEFINITIONS
-;
-: FURL ( -- ) ( CS: wid-prev -- )
-  PREVIOUS CS> SET-CURRENT
 ;
