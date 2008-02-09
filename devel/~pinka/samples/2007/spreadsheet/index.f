228a03fa2f38ac3a3e1d9ff981c952bc1688c065
ruv <>
2008-02-09 23:31:57 +0000

    Экспериментальный вариант пары STHROW, SCATCH -- которые оперируют исключением, заданным в виде строки (поверх кода -2). Халтурная пара StoN и NtoS, проверка на 0 в "/" -- с броском исключения, если что не так.

--- devel/~pinka/samples/2007/spreadsheet/index.f
+++ devel/~pinka/samples/2007/spreadsheet/index.f
@@ -4,4 +4,26 @@
 
 REQUIRE EMBODY ~pinka/spf/forthml/index.f
 
+: STHROW ( addr u -- )  ER-U ! ER-A ! -2 THROW ;
+
+: SCATCH ( -- addr u true | false ) 
+  CATCH
+  DUP 0EQ IF EXIT THEN
+  DUP -2 NEQ IF THROW THEN
+  DROP ER-A @ ER-U @ TRUE
+;
+
+: 2NIP 2SWAP 2DROP ;
+
+: StoN ( c-addr u -- x )
+  forthml-hidden::I-LIT IF EXIT THEN `#NaN STHROW
+;
+: NtoS ( x -- a u ) 
+  S>D (D.) \ HERE OVER 2SWAP S,
+;
+
+WARNING @ WARNING 0!
+: / ( a b -- c ) DUP 0= IF `#zerro-div STHROW THEN / ;
+WARNING !
+
 S" mini.f.xml" FIND-FULLNAME2 EMBODY
