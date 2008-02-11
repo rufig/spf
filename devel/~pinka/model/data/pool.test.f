50ccd2758d1e66a3fa64c3db0a82c625f0d5b171
ruv <>
2008-02-11 19:41:58 +0000

    Проверка работы пула. (пока, уровня 1, pool.L1.f.xml)

---
+++ devel/~pinka/model/data/pool.test.f
@@ -0,0 +1,34 @@
+REQUIRE EMBODY    ~pinka/spf/forthml/index.f
+
+REQUIRE Wait      ~pinka/lib/multi/Synchr.f
+REQUIRE CreateSem ~pinka/lib/multi/Semaphore.f
+REQUIRE CREATE-CS ~pinka/lib/multi/Critical.f
+
+\ WINAPI: InterlockedIncrement KERNEL32.DLL
+\ WINAPI: InterlockedDecrement KERNEL32.DLL
+
+
+\ paths is relatively to current directory(!)
+
+`list-plain.f.xml EMBODY
+
+`event-plain.f.xml EMBODY
+
+`events-common.f.xml EMBODY
+
+`pool.L1.f.xml EMBODY
+
+
+ startup FIRE-EVENT
+
+ 1 release-worker pool-idle . CR
+ 2 release-worker pool-idle . CR
+ 3 release-worker pool-idle . CR
+
+ hire-worker . pool-idle . CR
+ hire-worker . pool-idle . CR
+ hire-worker . pool-idle . CR
+
+ 900 wait-worker . CR
+
+ \ shutdown FIRE-EVENT
