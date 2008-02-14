39b284d87075882117e92ef5b75d38016d23b4a5
ruv <>
2008-02-14 23:13:43 +0000

    associative list of ( a u ) pairs

---
+++ devel/~pinka/model/data/alist-fixed.test.f
@@ -0,0 +1,24 @@
+REQUIRE EMBODY    ~pinka/spf/forthml/index.f
+
+`list-plain.f.xml EMBODY
+
+`event-plain.f.xml EMBODY
+
+`events-common.f.xml EMBODY
+
+`alist-fixed.f.xml EMBODY
+
+
+
+ startup FIRE-EVENT
+
+ `some-value-1 `key-1 save-pair
+ `some-value-2 `key-2 save-pair
+ 
+ `key-1 2DUP TYPE .(  -- ) obtain-value TYPE CR
+ `key-2 2DUP TYPE .(  -- ) obtain-value TYPE CR
+ `key-3 2DUP TYPE .(  -- ) obtain-value TYPE CR
+  clear-alist
+ `key-1 2DUP TYPE .(  -- ) obtain-value TYPE CR
+ 
+ \ shutdown FIRE-EVENT
