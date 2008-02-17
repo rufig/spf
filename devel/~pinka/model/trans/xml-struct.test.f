94bf1788c20214951158eed317bde8096ca2fbbf
ruv <>
2008-02-17 19:58:52 +0000

    испытание xi:model и xi:inlcude (xml-struct.f.xml)

---
+++ devel/~pinka/model/trans/xml-struct.test.f
@@ -0,0 +1,15 @@
+REQUIRE EMBODY    ~pinka/spf/forthml/index.f
+
+
+`../data/list-plain.f.xml       EMBODY
+`../data/event-plain.f.xml      EMBODY
+`../data/events-common.f.xml    EMBODY
+
+`xml-struct.f.xml EMBODY
+
+
+  
+  startup FIRE-EVENT
+
+  `xml-struct.test.f.xml EMBODY
+
