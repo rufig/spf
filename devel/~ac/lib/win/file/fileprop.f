25ec54a24f70e69393c8e266f64048323f5cf2b3
spf <>
2008-02-18 18:53:46 +0000

    Переименование и оптимизация (ruv).

--- devel/~ac/lib/win/file/fileprop.f
+++ devel/~ac/lib/win/file/fileprop.f
@@ -10,30 +10,29 @@
 
 REQUIRE ftLastWriteTime ~ac/lib/win/file/findfile-r.f 
 
-: GET-FILE-PROPS ( addr u xt -- )
+: FOR-FILE1-PROPS ( addr u xt -- )
 
 \ addr u - имя файла или каталога
 \ xt ( addr u data -- ) - процедура вызываемая для 
 \                         обработки данных файла/каталога
 \ Обрабатывается только один файл, явно заданный по addr u.
 
-  { addr u xt \ data id }
-  /WIN32_FIND_DATA ALLOCATE THROW -> data
-  data /WIN32_FIND_DATA ERASE
-  data addr FindFirstFileA -> id
-  id -1 = IF data FREE DROP EXIT THEN
-  data cFileName ASCIIZ>
-  data xt EXECUTE
-  id FindClose DROP
-  data FREE DROP
-;
+  NIP SWAP
+  /WIN32_FIND_DATA RALLOT >R
+  R@ /WIN32_FIND_DATA ERASE
+  R@ SWAP FindFirstFileA ( xt id )
+  DUP -1 = IF 2DROP ELSE FindClose DROP ( xt )
+  R@ cFileName ASCIIZ> ( xt a u ) ROT R@ SWAP EXECUTE
+  THEN RDROP
+  /WIN32_FIND_DATA RFREE
+; 
 : (GET-FILETIME-WRITE-S) ( 0 0 addr u data -- filetime )
   ftLastWriteTime 2@ SWAP 2>R
   2DROP 2DROP ( убрали addr u и 0 0 )
   2R>
 ;
 : GET-FILETIME-WRITE-S  ( addr u -- filetime ) \ UTC
-  0 0 2SWAP ['] (GET-FILETIME-WRITE-S) GET-FILE-PROPS
+  0 0 2SWAP ['] (GET-FILETIME-WRITE-S) FOR-FILE1-PROPS
 ;
 
 \EOF
