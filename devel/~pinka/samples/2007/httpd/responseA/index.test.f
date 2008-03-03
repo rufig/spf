5d195d3591471daf3c47aedac1b320eeaa6239d9
ruv <>
2008-03-03 19:57:41 +0000

    испытание подхода.

---
+++ devel/~pinka/samples/2007/httpd/responseA/index.test.f
@@ -0,0 +1,15 @@
+REQUIRE EMBODY    ~pinka/spf/forthml/index.f
+
+`lib-r.f.xml EMBODY
+
+`~pinka/model/data/data-space.f.xml FIND-FULLNAME EMBODY
+
+ 50000 ALLOCATED DATASPACE!
+
+: put TYPE ;
+
+`index.f.xml EMBODY
+
+
+ S" test passed" Reply200txt CR
+
