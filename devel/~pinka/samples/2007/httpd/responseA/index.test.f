79184ac4ceb470b1763ea9a1eba36061af666afd
ruv <>
2008-03-05 19:32:53 +0000

    Явно определено пространство данных для формирования заголовков.

--- devel/~pinka/samples/2007/httpd/responseA/index.test.f
+++ devel/~pinka/samples/2007/httpd/responseA/index.test.f
@@ -1,15 +1,24 @@
 REQUIRE EMBODY    ~pinka/spf/forthml/index.f
+REQUIRE STHROW    ~pinka/spf/sthrow.f
 
 `lib-r.f.xml EMBODY
 
-`~pinka/model/data/data-space.f.xml FIND-FULLNAME EMBODY
+\ `~pinka/model/data/data-space.f.xml FIND-FULLNAME EMBODY
 
- 50000 ALLOCATED DATASPACE!
+\ 50000 ALLOCATED DATASPACE!
 
 : put TYPE ;
 
+
+`~pinka/model/data/list-plain.f.xml  FIND-FULLNAME EMBODY
+
+`~pinka/model/data/event-plain.f.xml FIND-FULLNAME EMBODY
+
+`~pinka/model/data/events-common.f.xml FIND-FULLNAME EMBODY
+
 `index.f.xml EMBODY
 
+ startup FIRE-EVENT
 
  S" test passed" Reply200txt CR
 
