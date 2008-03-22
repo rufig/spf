a49b038fd3fca4783059cfe7fbddbcc49186ab33
ygreks <>
2008-03-22 11:42:38 +0000

    TIME&DATE for posix

--- lib/include/facil.f
+++ lib/include/facil.f
@@ -1,3 +1,11 @@
+\ $Id$
+\ 
+\ Get current local date and time
+
+REQUIRE [IF] lib/include/tools.f
+
+[DEFINED] WINAPI: [IF]
+
 \ _SYSTEMTIME
 0
 2 -- wYear
@@ -23,3 +31,49 @@ WINAPI: GetTickCount KERNEL32.DLL
   SYSTEMTIME wMonth W@
   SYSTEMTIME wYear W@
 ;
+
+[ELSE]
+
+: NSYM: ( num "name" -- ) PARSE-NAME 2DUP CREATED symbol-lookup , , DOES> DUP CELL+ @ SWAP @ symbol-call ;
+
+1 NSYM: time
+2 NSYM: localtime_r
+
+\ tm struct
+0
+CELL -- tm_sec
+CELL -- tm_min
+CELL -- tm_hour
+CELL -- tm_mday
+CELL -- tm_mon
+CELL -- tm_year
+CELL -- tm_wday
+CELL -- tm_yday
+CELL -- tm_isdst_nouse
+\       The glibc version of struct tm has additional fields
+\
+\              long tm_gmtoff;           /* Seconds east of UTC */
+\              const char *tm_zone;      /* Timezone abbreviation */
+\
+\       defined when _BSD_SOURCE was set before including <time.h>.  This is a
+\       BSD extension, present in 4.3BSD-Reno.
+CELL -- tm_gmtoff
+CELL -- tm_zone
+CONSTANT /TM
+
+USER-CREATE TM /TM USER-ALLOT
+
+: TIME&DATE ( -- sec min hr day mt year )
+  0 time 
+  SP@ TM localtime_r DROP 
+  DROP
+  TM tm_sec @
+  TM tm_min @
+  TM tm_hour @
+  TM tm_mday @
+  TM tm_mon @ 1 +
+  TM tm_year @ 1900 +
+;
+
+[THEN]
+
