f2f68df2eff2359ef44fdd760e80bd324b4526fc
ruv <>
2008-03-25 01:01:50 +0000

    * slashes in path

--- devel/~pinka/lib/lambda.f
+++ devel/~pinka/lib/lambda.f
@@ -4,7 +4,7 @@
 \ Ruvim,  06.01.2000
 \ 14.May.2007 добавлена установка LAST-NON дл€ RECURSE (true-grue, ygrek)
 
-REQUIRE AHEAD lib\include\tools.f 
+REQUIRE AHEAD lib/include/tools.f 
 
 : LAMBDA{  ( -- )
 \ врем€ компил€ции  ( -- orig1 xt )
