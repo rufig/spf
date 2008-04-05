4967b45bbc86941627640bdd79f6dfb92bd6fa3b
ruv <>
2008-04-05 11:23:58 +0000

    приязка к событиям тут еще не оправдана; + comment: todo

--- devel/~pinka/model/trans/xml-struct.test.f
+++ devel/~pinka/model/trans/xml-struct.test.f
@@ -2,14 +2,26 @@ REQUIRE EMBODY    ~pinka/spf/forthml/index.f
 
 
 `../data/list-plain.f.xml       EMBODY
-`../data/event-plain.f.xml      EMBODY
-`../data/events-common.f.xml    EMBODY
+\ `../data/event-plain.f.xml      EMBODY
+\ `../data/events-common.f.xml    EMBODY
 
 `xml-struct.f.xml EMBODY
 
-
   
-  startup FIRE-EVENT
+  \ startup FIRE-EVENT
+  xml-struct-hidden::start
 
   `xml-struct.test.f.xml EMBODY
 
+\EOF
+
+ToDo
+  В оригинале объявления xi:model локальны в пределах любого 
+  родительского элемента, а здесь -- только в пределах 
+  элементов f:forth, xi:include и xi:model (которые сохраняют
+  чистоту по эффекту на список _list). 
+  Надо бы подправить (привязать списко к родительскому элементу),
+  или наложить ограничение.
+
+  Поддержка атрибута advice для xi:model
+  
