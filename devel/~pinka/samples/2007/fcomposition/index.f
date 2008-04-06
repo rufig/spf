e17497c8fe13616c9975f27b84e07b901025be86
ruv <>
2008-04-06 18:40:31 +0000

    + Переопределение inline-слов (слов с собственными "комплияторами").

--- devel/~pinka/samples/2007/fcomposition/index.f
+++ devel/~pinka/samples/2007/fcomposition/index.f
@@ -4,6 +4,20 @@ REQUIRE EMBODY    ~pinka/spf/forthml/index.f
 `import-words.f.xml     FIND-FULLNAME2 EMBODY
 `tc-host.f.xml          FIND-FULLNAME2 EMBODY
 
+
+WARNING @ WARNING 0!
+: EXECUTE  EXECUTE ;
+: ?DUP     ?DUP    ;
+: >R       R> 2>R  ;
+: R>       2R> >R  ;
+: RDROP    R> RDROP >R ;
+
+\ Данные слова переопределены, т.к. они имеют собственные компиляторы,
+\ и эти компиляторы откладывают код конкретно в штатное хранилище,
+\ а нам надо откладывть в целевое, никак здесь со штатным не связанное.
+WARNING !
+
+
 \ TC-WL NLIST
 \ TC-WL ALSO!
 
