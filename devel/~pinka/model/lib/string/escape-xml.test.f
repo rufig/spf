6418dcd883fef247eef0d11d506d208cd8a66cfe
ruv <>
2008-04-28 22:49:28 +0000

    refactoring, renaming, more correctly.

--- devel/~pinka/model/lib/string/escape-xml.test.f
+++ devel/~pinka/model/lib/string/escape-xml.test.f
@@ -5,5 +5,8 @@ REQUIRE EMBODY    ~pinka/spf/forthml/index.f
 `escape-xml.f.xml EMBODY
 
   ' TYPE 
-  S" 'AT&T'. 1<2 & 2<3 & 3<4 & 4<5  " PIPELINE-XML-ESCAPING-
+  S" 'AT&T'. 1<2 & 2<3 & 3<4 & 4<5  <[[-<>-]]>" ESCAPE-XML-PER-
+  CR
+  ' TYPE
+  S" 1&lt;2 &amp; 2&lt;3 &amp; 3&lt;4 &amp; 4&lt;5  &lt;[[-&lt;>-]]&gt;" UNESCAPE-XML-PER-
   CR
