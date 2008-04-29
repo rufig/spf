7a66246dc706cd5ddade47f2c66034dc60c6b25d
ruv <>
2008-04-29 10:02:51 +0000

    + parse by chunks functions

--- devel/~pinka/lib/lin/xml/libxml2-doc.f
+++ devel/~pinka/lib/lin/xml/libxml2-doc.f
@@ -22,3 +22,51 @@
   5 xmlReadMemory
   DUP 0= IF  60005 THROW THEN 
 ; 
+
+
+
+\ m.b.
+
+: CreatePushParserCtxt ( d-first-chunk d-filename -- ctxt|0 )
+  DROP -ROT SWAP 0 0
+  5 xmlCreatePushParserCtxt
+  DUP IF EXIT THEN
+  ABORT" error on CreatePushParserCtxt"
+;
+: ParseChunk ( d-chunk ctxt -- )
+  >R
+  DUP 0= -ROT SWAP R> \ ( terminate size chunk ctxt )
+  4 xmlParseChunk 0= IF EXIT THEN
+  ABORT" error on xmlParseChunk"
+;
+: ClearPushParserCtxt ( ctxt -- )
+  >R 
+  0 \ encoding
+  0 \ filename
+  0 \ size
+  0 \ chunk
+  R> \ ctxt
+  5 xmlCtxtResetPush 0= IF EXIT THEN
+  ABORT" error on xmlCtxtResetPush"
+;
+: FreeParserCtxt ( ctxt -- )
+  1 xmlFreeParserCtxt DROP
+  \ the parsed document in ctxt->myDoc is not freed.
+;
+: XmlDocFromCtxt ( ctxt -- doc )
+  2 CELLS + @ 
+;
+: DumpDoc ( doc -- )
+  XML_SERIALIZE TYPE
+;
+
+\EOF
+
+: DumpDoc ( doc -- )
+  H-STDOUT \ see http://xmlsoft.org/examples/io1.c
+  2 xmlDocDump
+  -1 <> IF EXIT THEN
+  ABORT" error on xmlDocDump"
+  \ ???  not work
+;
+
