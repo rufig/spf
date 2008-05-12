f3b0a92542b12b761f5aedc9d94986af84679f00
ygreks <>
2008-05-12 17:17:57 +0000

    cgi sample

---
+++ devel/~ygrek/prog/web/help.cgi/cgi.f
@@ -0,0 +1,50 @@
+\ $Id$
+\ 
+\ sample CGI
+\ see it in action at http://ygrek.org.ua/exec/help.cgi?q=*SWAP*
+
+: wordsfile S" words" ;
+
+S" ~ygrek/prog/web/help.cgi/load.f" INCLUDED
+
+REQUIRE FINE-TAIL ~pinka/samples/2005/lib/split-white.f
+REQUIRE uname name.f
+REQUIRE $Revision ~ygrek/lib/fun/kkv.f
+REQUIRE cat ~ygrek/lib/cat.f
+REQUIRE replace-str- ~pinka/samples/2005/lib/replace-str.f
+REQUIRE DumpParams ~ac/lib/string/get_params.f
+
+: uname ( ? -- s )
+  S" /proc/sys/kernel/ostype" cat 
+  S" /proc/sys/kernel/osrelease" cat OVER S+
+  SWAP IF S" /proc/sys/kernel/version" cat OVER S+ THEN
+  DUP " {EOLN}" "  " replace-str- ;
+
+: get-params S" QUERY_STRING" ENVIRONMENT? 0= ABORT" Not a CGI" GetParamsFromString ;
+
+: revision $Revision$ SLITERAL ;
+: spf-version VERSION 1000 / 100 /MOD " SP-Forth {n}.{n} (http://spf.sf.net)" ;
+
+: EMPTY? NIP 0= ;
+
+: text/plain S" Content-type: text/plain" TYPE CR CR ;
+
+: main
+  get-params
+  S" q" GetParam EMPTY? IF S" set 'q' param" TYPE CR EXIT THEN
+  wordsfile load { l }
+  S" q" GetParam l find show ;
+
+:NONAME
+  text/plain
+  main
+  CR
+  FALSE uname { os }
+  os STR@ FINE-TAIL "  help.cgi r{revision} ({s})" STYPE CR
+  os STRFREE
+  "  Powered by " STYPE spf-version STYPE CR
+  BYE 
+; 
+MAINX !
+
+S" help.cgi" SAVE BYE
---
+++ devel/~ygrek/prog/web/help.cgi/load.f
@@ -0,0 +1,29 @@
+\ $Id$
+
+REQUIRE [IFDEF] ~nn/lib/ifdef.f \ for ~mak/A_IF.F
+REQUIRE as-list ~ygrek/lib/list/more.f
+REQUIRE FileLines=> ~ygrek/lib/filelines.f
+REQUIRE LIKE ~pinka/lib/like.f
+
+: extract
+  %[
+  LAMBDA{ PARSE-NAME >STR %s PARSE-NAME >STR %s -1 PARSE >STR %s } EVALUATE-WITH
+  ]% ;
+  
+: load% FileLines=> DUP STR@ extract %l ;
+
+\ file format :
+\ word file stack
+: load ( a u -- l ) %[ load% ]% ;
+
+\ I am lazy - l' points to elements in l - can't be FREEd
+: find ( a u l -- l' )
+   %[ LAMBDA{ >R R@ car STR@ 2OVER LIKE IF R> %l ELSE RDROP THEN } SWAP mapcar 2DROP ]% ;
+
+: show ( l -- )
+  LAMBDA{
+  >R
+  R@ car STR@ TYPE SPACE
+  R@ cdr cdr car STR@ TYPE ."  \ "
+  R> cdr car STR@ TYPE CR } SWAP mapcar ;
+
