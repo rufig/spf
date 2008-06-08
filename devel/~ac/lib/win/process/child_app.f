b547b529532cb8c7e11d2b123f561dd25757bc67
spf <>
2008-06-08 22:12:43 +0000

    Переменные определены, а использующая их функция была забыта :)

--- devel/~ac/lib/win/process/child_app.f
+++ devel/~ac/lib/win/process/child_app.f
@@ -1,6 +1,6 @@
 REQUIRE {                     ~ac/lib/locals.f
 REQUIRE StartApp              ~ac/lib/win/process/process.f
-\ REQUIRE DUP-HANDLE-INHERITED  ~ac/lib/win/process/pipes.f
+REQUIRE DUP-HANDLE-INHERITED  ~ac/lib/win/process/pipes.f
 
 USER StdinRH
 USER StdinWH
@@ -8,6 +8,25 @@ USER StdoutRH
 USER StdoutWH
 USER StderrRH
 USER StderrWH
+
+: CreateStdPipes ( -- i o e )
+\ Создать пайпы для передачи дочернему процессу в качестве stdin/out/err
+\ и вернуть их хэндлы.
+\ Хэндлы родительских концов труб остаются в переменных:
+\ StdinWH (туда пишется то, что попадет дочке в stdin),
+\ StdoutRH (оттуда читать ответ дочки)
+\ StderrRH (оттуда читать ошибки дочки)
+
+  0 0 StdinWH StdinRH CreatePipe ERR THROW
+  0 0 StdoutWH StdoutRH CreatePipe ERR THROW
+  0 0 StderrWH StderrRH CreatePipe ERR THROW
+  StdinRH @ DUP-HANDLE-INHERITED THROW \ StdInput !
+  StdinRH @ CLOSE-FILE THROW
+  StdoutWH @ DUP-HANDLE-INHERITED THROW \ StdOutput !
+  StdoutWH @ CLOSE-FILE THROW
+  StderrWH @ DUP-HANDLE-INHERITED THROW \ StdErr !
+  StderrWH @ CLOSE-FILE THROW
+;
 
 : ChildAppErr ( input-handle output-handle err-handle a u -- p-handle ior )
   { i o e a u \ pi si res }
@@ -40,6 +59,36 @@ USER StderrWH
 : ChildApp ( input-handle output-handle a u -- p-handle ior )
   2>R DUP 2R> ChildAppErr
 ;
+
+
+\EOF
+~ac/lib/str5.f
+
+: TEST
+  CreateStdPipes S" F:\spf4\spf4.exe" ChildAppErr THROW
+  CLOSE-FILE DROP 
+
+  StdoutRH @ PipeLine >R
+
+  S" WORDS" StdinWH @ WRITE-FILE THROW
+  CRLF StdinWH @ WRITE-FILE THROW
+
+\ вторая команда может не прочитаться в spf, т.к. READ-LINE заточен на
+\ чтение файлов, а не пайпов:
+
+  S" 5 5 + ." StdinWH @ WRITE-FILE THROW
+  CRLF StdinWH @ WRITE-FILE THROW
+
+  StdinWH @ CLOSE-FILE THROW
+
+  BEGIN
+    R@ PipeReadLine ." =>" TYPE ." <=" CR
+  AGAIN
+
+  RDROP
+  StdoutRH @  CLOSE-FILE THROW
+;
+' TEST CATCH .
 
 (
 REQUIRE fsockopen ~ac/lib/win/winsock/ws2/psocket.f
