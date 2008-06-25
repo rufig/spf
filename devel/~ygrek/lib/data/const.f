d6639676bac950a9dda7f727f32c2e82fa77633d
ygreks <>
2008-06-25 19:57:30 +0000

    dump const file contents

---
+++ devel/~ygrek/lib/data/const.f
@@ -0,0 +1,16 @@
+\ $Id$
+\
+\ Dump .const file contents (see lib/ext/const.f)
+
+REQUIRE STR@ ~ac/lib/str5.f
+
+: DUMP-CONST-FILE ( a u -- )
+  FILE 3 CELLS < IF ." Bad const file" CR DROP EXIT THEN { a }
+  a 2 CELLS + @ 0 ?DO
+   a 3 I + CELLS + @ a + DUP @ . SPACE ." CONSTANT " CELL+ COUNT TYPE CR
+  LOOP
+  a FREE THROW ;
+
+\ S" lib/win/winconst/windows.const" +ModuleDirName DUMP-CONST-FILE
+\ S" devel/~yz/cons/windows.const" +ModuleDirName DUMP-CONST-FILE
+\ S" devel/~ygrek/lib/data/curl.const" +ModuleDirName DUMP-CONST-FILE
