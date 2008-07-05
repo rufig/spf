389ab34df55de67080a03297f533b4bc372dcb34
spf <>
2008-07-05 00:48:01 +0000

    Для всех окон, а не только поточных.

--- devel/~ac/lib/win/window/browser.f
+++ devel/~ac/lib/win/window/browser.f
@@ -82,23 +82,6 @@ WINAPI: AtlAxGetControl ATL.dll
 ;
 VARIABLE AtlInitCnt
 
-: BrowserWindow { addr u style parent_hwnd -- hwnd }
-\ создать окно браузера и загрузить URL addr u в него.
-  AtlInitCnt @ 0= IF AtlAxWinInit 0= IF 0x200A EXIT THEN AtlInitCnt 1+! THEN
-  0 0 0 parent_hwnd 0 CW_USEDEFAULT 0 CW_USEDEFAULT style addr S" AtlAxWin" DROP 0
-  CreateWindowExA
-;
-: BrowserInterface { hwnd \ iu bro -- iwebbrowser2 ior }
-  ^ iu hwnd AtlAxGetControl DUP 0=
-  IF
-    DROP
-    ^ bro IID_IWebBrowser2 iu ::QueryInterface DUP 0=
-    IF
-      bro SWAP
-    ELSE ." Can't get browser" DUP . 0 SWAP THEN
-  ELSE ." AtlAxGetControl error" DUP . 0 SWAP THEN
-;
-
 : BrowserSetIcon1 { addr u h -- }
 \ можно в зависимости от урла выбирать иконку, но в stub'е не используется
   1 LoadIconResource16 GCL_HICON h SetClassLongA DROP
@@ -111,12 +94,28 @@ VECT vBrowserSetIcon ' BrowserSetIcon1 TO vBrowserSetIcon
 ;
 VECT vBrowserSetTitle ' BrowserSetTitle1 TO vBrowserSetTitle
 
+: BrowserWindow { addr u style parent_hwnd -- hwnd }
+\ создать окно браузера и загрузить URL addr u в него.
+  AtlInitCnt @ 0= IF AtlAxWinInit 0= IF 0x200A EXIT THEN AtlInitCnt 1+! THEN
+  0 0 0 parent_hwnd 0 CW_USEDEFAULT 0 CW_USEDEFAULT style addr S" AtlAxWin" DROP 0
+  CreateWindowExA
+  DUP addr u ROT vBrowserSetTitle
+  DUP addr u ROT vBrowserSetIcon
+;
+: BrowserInterface { hwnd \ iu bro -- iwebbrowser2 ior }
+  ^ iu hwnd AtlAxGetControl DUP 0=
+  IF
+    DROP
+    ^ bro IID_IWebBrowser2 iu ::QueryInterface DUP 0=
+    IF
+      bro SWAP
+    ELSE ." Can't get browser" DUP . 0 SWAP THEN
+  ELSE ." AtlAxGetControl error" DUP . 0 SWAP THEN
+;
 : Browser { addr u \ h bro -- ior }
 
   addr u WS_OVERLAPPEDWINDOW 0 BrowserWindow -> h
   h 0= IF 0x200B EXIT THEN
-  addr u h vBrowserSetTitle
-  addr u h vBrowserSetIcon
 
 \ PAD bro ::get_AddressBar .
   h BrowserInterface ?DUP IF NIP EXIT THEN -> bro
