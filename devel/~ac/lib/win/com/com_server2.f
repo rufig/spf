40e3f4810d43948a65fa1add8e659c555ac9cacc
spf <>
2008-07-06 23:58:03 +0000

    Легким движением руки SPF древний SPF.IDispatch научился принимать извещения о событиях от COM-объектов, поддерживающих IConnectionPoint. Например, от браузера...

--- devel/~ac/lib/win/com/com_server2.f
+++ devel/~ac/lib/win/com/com_server2.f
@@ -8,6 +8,7 @@ VARIABLE ForthComClassObject
 
 VARIABLE FCNT
 VARIABLE LOCKCNT
+VARIABLE COM-DEBUG \ TRUE COM-DEBUG !
 
 VECT vSPF.Application
 VECT vSPF.IDispatch
@@ -18,27 +19,27 @@ IID_IUnknown
 Class: SPF.IUnknown {C6DFBA32-DF7B-4829-AA3B-EE4F90ED5961}
 
 : ::QueryInterface ( ppvObject iid oid - hresult )
-\  Class.
+  COM-DEBUG @ IF Class. SPACE THEN
   OVER 16 IID_IUnknown 16 COMPARE 0= 
-          IF ( ." QI:Unknown,") 2DROP SPF.IUnknown SWAP ! 0 EXIT THEN
+          IF COM-DEBUG @ IF ." QI:Unknown," THEN 2DROP SPF.IUnknown SWAP ! 0 EXIT THEN
   OVER 16 IID_IClassFactory 16 COMPARE 0= 
-          IF ( ." QI:IClassFactory,") 2DROP vSPF.Application SWAP ! 0 EXIT THEN
+          IF COM-DEBUG @ IF ." QI:IClassFactory," THEN 2DROP vSPF.Application SWAP ! 0 EXIT THEN
   OVER 16 IID_IDispatch 16 COMPARE 0= 
-          IF ( ." QI:IDispatch,") 2DROP vSPF.IDispatch SWAP ! 0 EXIT THEN
+          IF COM-DEBUG @ IF ." QI:IDispatch," THEN 2DROP vSPF.IDispatch SWAP ! 0 EXIT THEN
   OVER 16 vSPF.Application Class 16 COMPARE 0= 
-          IF ( ." QI:IForth,") 2DROP vSPF.Application SWAP ! 0 EXIT THEN
-\  ." QI:"
-  DROP CLSID>String THROW 2DROP ( UNICODE> TYPE ." ;") 0!
+          IF COM-DEBUG @ IF ." QI:IForth," THEN 2DROP vSPF.Application SWAP ! 0 EXIT THEN
+  COM-DEBUG @ IF ." QI:" THEN
+  DROP CLSID>String THROW COM-DEBUG @ IF  UNICODE> TYPE ." ;" ELSE 2DROP THEN 0!
   E_NOINTERFACE
 ; METHOD
 
 : ::AddRef ( oid -- cnt )
-\  Class.
+  COM-DEBUG @ IF Class. THEN
   DROP FCNT 1+! FCNT @
 ; METHOD
 
 : ::Release ( oid -- cnt )
-\  Class.
+  COM-DEBUG @ IF Class. THEN
   DROP FCNT @ 1- DUP FCNT !
 ; METHOD
 
@@ -51,12 +52,12 @@ Extends SPF.IUnknown
 
 
 : ::CreateInstance ( ppvObject riid pUnkOuter oid -- hresult )
-\  Class.
+  COM-DEBUG @ IF Class. THEN
   DROP DROP DROP ( ForthIForth) SPF.Application SWAP ! 0
 ; METHOD
 
 : ::LockServer ( fLock oid -- hresult )
-\  Class.
+  COM-DEBUG @ IF Class. THEN
   DROP
   IF LOCKCNT 1+! ELSE LOCKCNT @ 1- LOCKCNT ! THEN
   0
@@ -77,29 +78,46 @@ USER uFlags
 \ Это лишает некоторых возможностей, но зато логически чище,
 \ и удобнее использовать.
   >R
-\  ." TYPE=" R@ W@ .
-  R@ W@ 2 = IF R> 2 CELLS + @ ( DUP . ." ,") EXIT THEN
-  R@ W@ 3 = IF R> 2 CELLS + @ ( DUP . ." ,") EXIT THEN
-  R@ W@ 8 = IF R> 2 CELLS + @ BSTR> ( 2DUP TYPE ." ,") EXIT THEN
-  R@ W@ 0x400C = IF R> 2 CELLS + @ ( ." recurse variant:") RECURSE EXIT THEN
-\ ." UNKNOWN PTYPE=" R@ W@ . R> 2 CELLS + @ 16 DUMP
+  COM-DEBUG @ IF CR ." TYPE=" R@ W@ . THEN
+  R@ W@ 0 = IF R> 2 CELLS + @ COM-DEBUG @ IF DUP . ." -0," THEN EXIT THEN
+  R@ W@ 2 = IF R> 2 CELLS + @ COM-DEBUG @ IF DUP . ." ," THEN EXIT THEN
+  R@ W@ 3 = IF R> 2 CELLS + @ COM-DEBUG @ IF DUP . ." ," THEN EXIT THEN
+  R@ W@ 11 = IF R> 2 CELLS + @ COM-DEBUG @ IF DUP . ." -b," THEN EXIT THEN \ bool
+  R@ W@ 9 = IF R> 2 CELLS + @ COM-DEBUG @ IF DUP . ." -d," THEN EXIT THEN \ idisp
+  R@ W@ 8 = IF R> 2 CELLS + @ BSTR> COM-DEBUG @ IF 2DUP TYPE ." ," THEN EXIT THEN
+  R@ W@ 0x400B = IF R> 2 CELLS + @ COM-DEBUG @ IF DUP . ." -?," THEN EXIT THEN \ idisp
+  R@ W@ 0x400C = IF R> 2 CELLS + @ COM-DEBUG @ IF ." recurse variant:" THEN RECURSE EXIT THEN
+  COM-DEBUG @ IF ." UNKNOWN PTYPE=" R@ W@ . R> 2 CELLS + @ 16 DUMP THEN
   RDROP
 ;
 : params@ ( dispid -- ... )
-\  ." params="
-  DUP @ uParams ! 2 CELLS + @ ( DUP .) 0 ?DO
+  COM-DEBUG @ IF ." params=" THEN
+  DUP @ uParams ! 2 CELLS + @ COM-DEBUG @ IF DUP . THEN 0 ?DO
     uParams @ I 4 * CELLS + param@
   LOOP
 ;
 : IsVariable@ ( xt -- flag )
   DUP 1+ @ + CFL + ['] _CREATE-CODE =
   uFlags @ DISPATCH_PROPERTYPUT AND 0= AND
-  DUP IF ( ."  VARIABLE@ ") THEN
+  DUP IF COM-DEBUG @ IF ."  VARIABLE@ " THEN THEN
 ;
+
+VECT vExecuteByID
+
+: DropXtParams ( ... xt -- )
+  \ при вызове неизвестного id, например необрабатываемых events
+  \ просто снимаем параметры и выходим, иначе можно уронить вызывающего
+  uSPInvoke @ SP!  
+; ' DropXtParams TO vExecuteByID
+
 : EXECUTE-COM ( ... xt -- ... )
+  DUP WordByAddr COM-DEBUG @ IF ." Method=" 2DUP TYPE CR THEN
+  DROP 2 S" <?" COMPARE 0= IF vExecuteByID EXIT THEN
   DUP IsVariable@ IF EXECUTE @ EXIT THEN
   EXECUTE
 ;
+USER INVOKE_VOID
+
 \ ===================== IDispatch ===================================
 
 IID_IClassFactory
@@ -107,15 +125,18 @@ Class: SPF.IDispatch {C6DFBA32-DF7B-4829-AA3B-EE4F90ED5961}
 Extends SPF.IUnknown
 
 : ::GetTypeInfoCount ( pctinfo oid -- 0 | 1 )
-  ( Class.) 2DROP 0
+  COM-DEBUG @ IF Class. THEN
+  2DROP 0
 ; METHOD
 
 : ::GetTypeInfo      ( ppTInfo lcid iTInfo oid -- hresult )
-  ( Class.) 2DROP 2DROP 1
+  COM-DEBUG @ IF Class. THEN
+  2DROP 2DROP 1
 ; METHOD
 
 : ::GetIDsOfNames    ( rgDispId lcid cNames rgszNames riid oid -- hresult )
-  ( Class.) 2DROP
+  COM-DEBUG @ IF Class. THEN
+  2DROP
   ( ." rgszNames=") @ UASCIIZ> UNICODE> ( 2DUP) 2>R ( TYPE)
 \  ."  cNames=" .
 DROP
@@ -128,19 +149,17 @@ DROP
 ; METHOD
 
 : ::Invoke           ( puArgErr pExcepInfo pVarResult pDispParams wFlags lcid riid dispIdMember oid -- hresult )
-\  Class.
-  ( ." oid=" .)
-DROP
-  ( ." dispIdMember=" DUP .) >R
+  COM-DEBUG @ IF Class. THEN
+  COM-DEBUG @ IF ." oid=" . ELSE DROP THEN
+  COM-DEBUG @ IF ." dispIdMember=" DUP . THEN >R
   DROP ( reserved)
-\  ." lcid=" .
-DROP
-  ( ." wFlags=" DUP .) uFlags !
-  ( ." pDispParams=" DUP .) >R \ R@ 20 DUMP
-  ( ." pVarResult=" DUP .) 2R> ROT >R 2>R
-  ( ." pExcepInfo=" DUP .) uExcep !
-\  ." puArgErr=" .
-DROP
+  COM-DEBUG @ IF ." lcid=" . ELSE DROP THEN
+  COM-DEBUG @ IF ." wFlags=" DUP . THEN uFlags !
+  COM-DEBUG @ IF ." pDispParams=" DUP . THEN >R \ R@ 20 DUMP
+  COM-DEBUG @ IF ." pVarResult=" DUP . THEN
+  DUP 0= IF DROP INVOKE_VOID THEN 2R> ROT >R 2>R
+  COM-DEBUG @ IF ." pExcepInfo=" DUP . THEN uExcep !
+  COM-DEBUG @ IF ." puArgErr=" . ELSE DROP THEN
   SP@ DUP uSPInvoke ! S0 !
   R> params@
   R> ['] EXECUTE-COM CATCH ?DUP 
@@ -148,7 +167,7 @@ DROP
         uSPInvoke @ SP! RDROP
         DISP_E_EXCEPTION EXIT
      THEN
-  SP@ uSPInvoke @ - \ DUP .
+  SP@ uSPInvoke @ -   COM-DEBUG @ IF ." DD=" DUP . THEN
   DUP -4 = IF DROP
               3 R@ ! ( uFlags @ 3 = IF @ THEN) \ бейсик не делает разницы между method и property_get
               R> 2 CELLS + ! 0 EXIT
@@ -160,7 +179,7 @@ DROP
            THEN \ строка
   0 = IF RDROP 0 EXIT THEN \ нет результатов
   uSPInvoke @ SP! RDROP 
-  DISP_E_BADPARAMCOUNT
+  DISP_E_BADPARAMCOUNT COM-DEBUG @ IF ." res=DISP_E_BADPARAMCOUNT" CR THEN
 ; METHOD
 
 Class;
