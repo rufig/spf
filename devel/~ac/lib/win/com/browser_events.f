aa945916be6118ba1f619f5bca2e713b69b3b129
spf <>
2008-07-07 03:52:16 +0000

    Явно экспортируем IID_IWebBrowserEvents2, чтобы отличать от прочих IDispatch-вызовов.

--- devel/~ac/lib/win/com/browser_events.f
+++ devel/~ac/lib/win/com/browser_events.f
@@ -6,7 +6,21 @@ REQUIRE EnumConnectionPoints ~ac/lib/win/com/events.f
 
 IID_IDispatch
 Interface: IID_IWebBrowserEvents2 {34A715A0-6587-11D0-924A-0020AFC7AC4D}
+\ методы этого интерфейса не вызываются, т.к. он "входящий" -
+\ при поступлении событий ID этих событий передаются нашему IDispatch::Invoke
 Interface;
+
+IID_IWebBrowserEvents2
+Class: SPF.IWebBrowserEvents2 {C6DFBA32-DF7B-4829-AA3B-EE4F90ED5961} \ свой clsid общий
+Extends SPF.IDispatch
+\ ORDER
+Class;
+
+: {34A715A0-6587-11D0-924A-0020AFC7AC4D} ( ppvObject iid oid -- hresult )
+\ Экспортируем реализацию. См. ::QueryInterface
+  COM-DEBUG @ IF ." SPF.IWebBrowserEvents2 - OK!" THEN
+  2DROP SPF.IWebBrowserEvents2 SWAP ! 0
+;
 
 \ А эти интерфейсы подключения к событиям (connection points) экспортирует IE7
 
