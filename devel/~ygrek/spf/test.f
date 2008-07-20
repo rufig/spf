68c4e722108ab52d5e83071270a3ed784489dfb6
ygreks <>
2008-07-20 14:16:44 +0000

    run all tests easily

---
+++ devel/~ygrek/spf/test.f
@@ -0,0 +1,8 @@
+\ $Id$
+
+S" ~profit/spf/test/alignment.f" INCLUDED
+S" ~profit/spf/test/search.f" INCLUDED
+S" ~ygrek/spf/test/core.f" INCLUDED
+S" ~ygrek/spf/test/floats.f" INCLUDED
+S" ~ygrek/spf/test/locals.f" INCLUDED
+S" ~ygrek/spf/test/extra.f" INCLUDED
