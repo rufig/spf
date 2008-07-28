fd55f800146ec36442a1bdd77ede9faf286dd79e
spf <>
2008-07-28 16:48:49 +0000

    Поиск дочернего окна с заданным классом.

--- devel/~ac/lib/win/window/enumwindows.f
+++ devel/~ac/lib/win/window/enumwindows.f
@@ -2,8 +2,10 @@ WINAPI: EnumWindows              USER32.DLL
 WINAPI: EnumChildWindows         USER32.DLL
 WINAPI: GetWindowTextA           USER32.DLL
 WINAPI: GetForegroundWindow      USER32.DLL
+WINAPI: GetDesktopWindow         USER32.DLL
 WINAPI: GetWindowModuleFileNameA USER32.DLL \ NT4SP3, Win98
 WINAPI: IsWindowVisible          USER32.DLL
+WINAPI: GetClassNameA            USER32.DLL
 
 : (EnumWindow1) ( xt hwnd -- flag )
 \ flag=true - continue enumeration
@@ -28,13 +30,36 @@ WINAPI: IsWindowVisible          USER32.DLL
   ['] EnumChildWindow1 SWAP EnumChildWindows ERR
 ;
 
+USER uGCWCa
+USER uGCWCu
+USER uGCWCwnd
+
+: (GetChildWithClass1) ( tls hwnd -- flag )
+\ flag=true - continue enumeration
+\ flag=false - stop enumeration
+  TlsIndex@ >R
+  SWAP TlsIndex!
+  DUP 512 PAD ROT GetClassNameA PAD SWAP uGCWCa @ uGCWCu @ COMPARE 0=
+  IF uGCWCwnd ! FALSE ELSE DROP TRUE THEN  
+  R> TlsIndex!
+;
+' (GetChildWithClass1) 8 CALLBACK: GetChildWithClass1
+
+: GetChildWithClass ( addr u hwnd -- hwnd2 )
+  SWAP uGCWCu ! SWAP uGCWCa ! uGCWCwnd 0!
+  TlsIndex@ ['] GetChildWithClass1 ROT EnumChildWindows DROP \ "Return Value - Not used."
+  uGCWCwnd @
+;
+
 \EOF
+\ S" MSCTFIME UI" GetDesktopWindow GetChildWithClass .
 
 : PWND
   DUP .
   GetForegroundWindow OVER = IF ." FOREGROUND: " THEN
   DUP IsWindowVisible IF ." VISIBLE: " THEN
   DUP 512 PAD ROT GetWindowTextA PAD SWAP ?DUP IF TYPE ELSE DROP THEN
+  DUP 512 PAD ROT GetClassNameA PAD SWAP ?DUP IF ."  [" TYPE ." ]" ELSE DROP THEN
   DUP 512 PAD ROT GetWindowModuleFileNameA PAD SWAP ?DUP 
   IF ."  (" TYPE ." )" ELSE DROP THEN
   DROP CR
@@ -43,3 +68,4 @@ WINAPI: IsWindowVisible          USER32.DLL
 : TEST
   ['] PWND ForEachWindow DROP
 ;
+\ ' PWND GetDesktopWindow ForEachChildWindow
