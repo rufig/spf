6ff3976d10329672f8bf6fc77abe2062700fd8c4
ruv <>
2008-08-03 23:39:33 +0000

    Вариант общей основы для ffi,  испытание.

---
+++ devel/~pinka/spf/ffi/core.f
@@ -0,0 +1,92 @@
+\ 03.Aug.2008
+
+REQUIRE DLOPEN          ~ac/lib/ns/dlopen.f
+REQUIRE NAMING-         ~pinka/spf/compiler/native-wordlist.f
+REQUIRE AsQName         ~pinka/samples/2006/syntax/qname.f \ понятие однословных строк в виде `abc
+REQUIRE STRHOW          ~pinka/spf/sthrow.f
+
+\ MODULE: ffi-support
+
+\ some code from ~pinka/model/codegen/spf4-ffi.f.xml
+\ see also ~ac/lib/ns/ns.f
+
+: DLOPEN-SURE ( a u -- h ) 
+  2DUP DLOPEN DUP IF  NIP NIP EXIT THEN 
+  DROP CR TYPE CR `#lib-not-found STHROW
+; 
+: DLSYM-SURE ( a u h -- a ) 
+  >R 2DUP R> DLSYM DUP IF  NIP NIP EXIT THEN 
+  DROP CR TYPE CR `#func-not-found STHROW
+; 
+
+: EXEC-FOREIGN-C1 ( i*x n-in entry-point -- x ) 
+  SWAP DUP 2+ CELLS DRMOVE R> 0 R> EXECUTE (  [ n-in x ]  ) 
+  SWAP RFREE
+; 
+: EXEC-FOREIGN-P1 ( i*x n-in entry-point -- x ) 
+  SWAP 1+ CELLS DRMOVE 0 R> EXECUTE (  [ x ]  ) 
+; 
+
+
+: CREATE-DLPOINT ( parent -- p )
+  ALIGN HERE >R
+  0 , \ handler (need to be erased on startup or before save)
+    , \ parent
+  0 , \ ref to name
+  R>
+;
+: DLPOINT-NAME! ( c-addr p -- )  2 CELLS + ! ;
+: DLPOINT-NAME@ ( p -- c-addr )  2 CELLS + @ ;
+: DLPOINT-NAME ( p -- a u ) DLPOINT-NAME@ DUP IF COUNT EXIT THEN 0 ;
+: DLPOINT-PARENT ( p1 -- p2 ) CELL+ @ ;
+: SET-DLPOINT-NAME ( a u p -- ) ALIGN HERE SWAP DLPOINT-NAME! S", 0 C, ;
+
+: CACHE-DL ( p -- )
+  DUP DLPOINT-NAME DLOPEN-SURE SWAP !
+;
+: DLPOINT-LIB ( p -- h )
+  DUP @ DUP IF NIP EXIT THEN DROP
+  DUP CACHE-DL @
+;
+: CACHE-DL-FUNC ( p -- )
+  DUP >R DLPOINT-NAME R@ DLPOINT-PARENT DLPOINT-LIB DLSYM-SURE R> !
+;
+: DLPOINT-FUNC ( p -- a )
+  DUP @ DUP IF NIP EXIT THEN DROP
+  DUP CACHE-DL-FUNC @
+;
+
+: CALL-DLPOINT-C1 ( i*x i p -- x )
+  DLPOINT-FUNC EXEC-FOREIGN-C1
+;
+: CALL-DLPOINT-P1 ( i*x i p -- x )
+  DLPOINT-FUNC EXEC-FOREIGN-P1
+;
+\ Возможно, стоит зашить формат вызова прямо в DLPOINT?
+\ (но тогда, почему бы не зашить и туда числа входящих аргументов?)
+
+
+\ ;MODULE
+
+\ test
+\ определяем слово "WAPI:" на определенной выше базе.
+
+: CREATE-WAPI ( d-func d-lib -- )
+  0  CREATE-DLPOINT DUP >R SET-DLPOINT-NAME
+  R> CREATE-DLPOINT DUP >R SET-DLPOINT-NAME
+  :NONAME R@ LIT, ['] CALL-DLPOINT-P1 COMPILE, POSTPONE ;
+  R> DLPOINT-NAME NAMING-
+;
+: WAPI:
+  NextWord NextWord CREATE-WAPI
+;  
+
+\ испытание в работе
+
+WAPI: MessageBoxA  user32.dll
+0 S" test" DROP S" test passed" DROP 0  4 MessageBoxA . CR
+
+\EOF
+\ а так выглядит по старому
+WINAPI: MessageBoxA  user32.dll
+0 S" test" DROP S" test passed" DROP 0   MessageBoxA . CR
