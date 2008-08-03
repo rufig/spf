19391f3cd55a9347ecd3071c155349feb55b0fdd
ruv <>
2008-08-03 01:04:25 +0000

    Сравнение реализации функции NormalizeURIPath (на форте и си). Может, пригодится в отсутствии libxml2 ;)

---
+++ devel/~pinka/model/lib/string/normalize-pathname.test.f
@@ -0,0 +1,59 @@
+REQUIRE EMBODY    ~pinka/spf/forthml/index.f
+
+`match.f.xml EMBODY
+
+`normalize-pathname.f.xml EMBODY
+
+ALSO SO NEW: libxml2.dll
+
+: normalizeURI ( addr-z u1 -- addr u2 )
+  OVER >R `: SEARCH NIP IF CHAR+ THEN \ cut out a scheme
+  1 xmlNormalizeURIPath THROW \ work for pathnames only 
+  R> ASCIIZ>
+;
+: normalizeURI ( addr-z u1 -- addr u2 )
+  OVER
+  1 xmlNormalizeURIPath THROW \ work for pathnames only 
+  DROP ASCIIZ>
+;
+
+PREVIOUS
+
+: _NORMALIZE-PATHNAME  \ for test
+  `/../asdf SPLIT DROP
+\  2DUP OVER SWAP CMOVE
+\  2DUP OVER SWAP MOVE
+   2DUP OVER SWAP CMOVE-CERTAIN
+;
+
+  S" aaa/bbb/ccc/../../../ddd/eee/fff/../../ooo" normalizeURI TYPE CR
+  S" aaa/bbb/ccc/../../../ddd/eee/fff/../../ooo" NORMALIZE-PATHNAME TYPE CR
+
+~pinka\lib\Tools\profiler.f
+
+: t-(libxml2)
+  100000 0 DO
+  S" aaa/bbb/ccc/../../../ddd/eee/fff/../../ooo" PAD OVER SEATED
+  normalizeURI 2DROP
+  LOOP
+;
+: t-(native)
+  100000 0 DO
+  S" aaa/bbb/ccc/../../../ddd/eee/fff/../../ooo" PAD OVER SEATED
+  NORMALIZE-PATHNAME 2DROP
+  LOOP
+;
+profile off
+
+: test
+  300 PAUSE
+  t-(native)
+  300 PAUSE
+  t-(libxml2)
+  .AllStatistic
+;
+
+\EOF
+
+\ Делать копию через >STR ... STRFREE  -- в два раза дольше тест работает
+\ (~450 вместо ~200)
