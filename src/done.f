8652f728b547185745383ae8e823f8f638a685b4
ygreks <>
2008-08-07 08:43:39 +0000

    path to spf.fres

--- src/done.f
+++ src/done.f
@@ -8,7 +8,7 @@ IMAGE-BASE 1034 + AOLL @ !
 IMAGE-BASE 1038 + AOGPA @ !
 DECIMAL
 
-S" spf4.exe" S" src\spf.fres" src\tsave.f
+S" spf4.exe" S" src\win\res\spf.fres" src\tsave.f
 .(  The system has been saved)
 
 BYE
