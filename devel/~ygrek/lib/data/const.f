af9ef9cdafcc63a8d21574e8c5d817c084c9d896
ygreks <>
2008-08-08 09:27:02 +0000

    factor out iterator over const files

--- devel/~ygrek/lib/data/const.f
+++ devel/~ygrek/lib/data/const.f
@@ -1,17 +1,27 @@
 \ $Id$
-\
+\ 
 \ Dump .const file contents (see lib/ext/const.f)
 
 REQUIRE STR@ ~ac/lib/str5.f
+REQUIRE LAMBDA{ ~pinka/lib/lambda.f
 
-: DUMP-CONST-FILE ( a u -- )
-  FILE DROP { a }
+\ Execute xt for each constant in file a u
+\ NB no bounds checking
+: DUMP-CONST-FILE-WITH-XT ( a u xt -- ) \ xt ( value name-a name-u -- )
+  { xt | a }
+  FILE DROP -> a
   a 4 S" CONS" COMPARE 0 <> IF ." Not a const file" CR a FREE THROW EXIT THEN
   a 2 CELLS + @ 0 ?DO
-   a 3 I + CELLS + @ a + DUP @ . SPACE ." CONSTANT " CELL+ COUNT TYPE CR
+   a 3 I + CELLS + @ a + DUP @ SWAP CELL+ COUNT xt EXECUTE
   LOOP
   a FREE THROW ;
 
+: DUMP-CONST-FILE=> ( a u <--> value name-a name-u ) R> DUMP-CONST-FILE-WITH-XT ;
+
+: DUMP-CONST-FILE ( a u -- ) LAMBDA{ ROT . ." CONSTANT " TYPE CR } DUMP-CONST-FILE-WITH-XT ;
+
 \ S" lib/win/winconst/windows.const" +ModuleDirName DUMP-CONST-FILE
-\ S" devel/~yz/cons/windows.const" +ModuleDirName DUMP-CONST-FILE
-\ S" devel/~ygrek/lib/data/curl.const" +ModuleDirName DUMP-CONST-FILE
+\ S" devel/~yz/cons/windows.const" +ModuleDirName :NONAME DUMP-CONST-FILE=> TYPE ."  = " . CR ; EXECUTE
+
+\ S" devel/~ygrek/lib/data/curl.const" +ModuleDirName 
+\ :NONAME " int {s} = {n};{EOLN}" STYPE ; DUMP-CONST-FILE-WITH-XT
