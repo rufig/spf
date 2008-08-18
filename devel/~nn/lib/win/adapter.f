ff15e1e94981cb75f669cf6e10a3f32e21ab2457
ilya73 <>
2008-08-18 23:11:26 +0000

    no message

--- devel/~nn/lib/win/adapter.f
+++
@@ -1,7 +0,0 @@
-REQUIRE (WINAPI:) ~nn/lib/winapi.f
-REQUIRE LH-INCLUDED  ~nn/lib/lh.f
-S" ~nn/lib/usedll.f" LH-INCLUDED
-S" ~nn/lib/wincon.f" INCLUDED
-UseDLL USER32.DLL
-UseDLL KERNEL32.DLL
-UseDLL GDI32.DLL
