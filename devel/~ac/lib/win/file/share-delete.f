9513523005b1916a35a2eb0b7e650e71594098d5
spf <>
2008-08-18 11:24:58 +0000

    Самоудаляемые файлы - CREATE-FILE-SHARED-DELETE-ON-CLOSE.

--- devel/~ac/lib/win/file/share-delete.f
+++ devel/~ac/lib/win/file/share-delete.f
@@ -25,3 +25,23 @@ REQUIRE WinNT? ~ac/lib/win/winver.f
   R> ( filename )
   CreateFileA DUP -1 = IF GetLastError ELSE 0 THEN
 ;
+
+0x04000000 CONSTANT FILE_FLAG_DELETE_ON_CLOSE
+
+: CREATE-FILE-SHARED-DELETE-ON-CLOSE ( c-addr u fam -- fileid ior )
+\ В отличие от предыдущей функции здесь не просто разрешается
+\ удаление открытого файла, но и указывается на необходимость его
+\ автоматического удаления при закрытии всех его хэндлов.
+\ Это похоже на CREATE-FILE-SHARED-DELETE+(сразу)DELETE-FILE при
+\ создании файла-флага, но позволяет работать с этим смертником (читать-писать).
+
+  SWAP DROP SWAP >R >R
+  0 FILE_ATTRIBUTE_ARCHIVE ( template attrs )
+    FILE_FLAG_DELETE_ON_CLOSE OR
+  CREATE_ALWAYS
+  SA ( secur )
+  WinNT? IF 7 ( share read/write/delete ) ELSE 3 THEN
+  R> ( access=fam )
+  R> ( filename )
+  CreateFileA DUP -1 = IF GetLastError ELSE 0 THEN
+;
