62176d456d53db90204eaadd2c1db064f9300c2e
ruv <>
2008-08-29 11:53:01 +0000

    backtrace to the point of "throw"

---
+++ devel/~pinka/spf/debug-throw.f
@@ -0,0 +1,16 @@
+\ 26.03.2007
+
+\ Подключать, когда непонятно, в каком месте исключение происходит:
+
+: THROW
+  DUP 0= IF THROW EXIT THEN
+
+  CR RP@
+  BEGIN DUP R0 @ U> 0= WHILE
+    STACK-ADDR.
+    CELL+
+  REPEAT
+  DROP
+
+  THROW
+;
