6864516b638345472e4be3cacc7d8f730b030d6d
ygreks <>
2008-08-30 12:27:17 +0000

    hack the hack -- correctly patch inlined VECT

--- lib/ext/patch.f
+++ lib/ext/patch.f
@@ -1,8 +1,23 @@
+\ $Id$
+
+: REPLACE-WORD ( by-xt what-xt ) 
+    \ http://n2.nabble.com/Прямая-компиляция-векторных-вызовов-td672884.html
+    DUP B@ 0xE8 = IF \ if we're replacing defer'red word 
+        DUP 1+ REL@ CELL+ ['] _VECT-CODE = IF 
+            >BODY ! EXIT 
+        THEN 
+    THEN 
+
+    0xE9 OVER B!  \ JMP ... 
+    1+ DUP >R 
+    CELL+ - 
+    R> ! 
+;
 
 \ from gforth
-: REPLACE-WORD ( by-xt what-xt )
-    [ HEX ] E9 [ DECIMAL ] OVER C!  \ JMP ...
-    1+ DUP >R
-    CELL+ -
-    R> !
-;
+\ : REPLACE-WORD ( by-xt what-xt )
+\     [ HEX ] E9 [ DECIMAL ] OVER C!  \ JMP ...
+\     1+ DUP >R
+\     CELL+ -
+\     R> !
+\ ;
