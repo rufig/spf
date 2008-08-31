e52521af17506e10100500ee8683aaa50835b1ed
ilya73 <>
2008-08-31 19:40:30 +0000

    *** empty log message ***

---
+++ devel/~nn/lib/sheader.f
@@ -0,0 +1,23 @@
+\ USER LAST-CFA
+\ CREATE SH-BUF 64 ALLOT
+REQUIRE [IF] lib/include/tools.f
+0 [IF]
+: SHEADER ( addr u -- )
+  HERE 0 , ( cfa )
+\  DUP LAST-CFA !
+  0 C,     ( flags )
+  ROT ROT WARNING @
+  IF 2DUP SFIND
+     IF DROP 2DUP TYPE ."  isn't unique" CR
+     ELSE 2DROP THEN
+  THEN
+  CURRENT @ +SWORD
+  ALIGN
+  HERE SWAP ! ( заполнили cfa )
+;
+[THEN]
+: SCREATE ( S" name" -- )
+  SHEADER
+  HERE DOES>A ! ( для DOES )
+  ['] _CREATE-CODE COMPILE,
+;
