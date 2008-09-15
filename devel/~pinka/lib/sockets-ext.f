d71451316488acc61142c8f612fdcea0b5966fd8
ruv <>
2008-09-15 18:11:52 +0000

    Слово ReadSocketExact теперь в базовом sockets.f Добавлено ReadoutSocket

--- devel/~pinka/lib/sockets-ext.f
+++ devel/~pinka/lib/sockets-ext.f
@@ -1,12 +1,10 @@
 REQUIRE ReadSocket ~ac/lib/win/winsock/SOCKETS.F
 
-: READ-SOCK-EXACT ( a u socket -- ior )
-  >R BEGIN DUP WHILE
-    2DUP R@ ReadSocket ?DUP IF NIP NIP NIP RDROP EXIT THEN ( a1 u1 u2 )
-    TUCK - -ROT + SWAP
-  REPEAT ( a1 0 )
-  NIP RDROP
+: READOUT-SOCK ( a u1 h -- a u2 ior ) \ on likeness READOUT-FILE
+  >R OVER SWAP R>   ( a a h1 h )
+  ReadSocket        ( a u2 ior )
+  DUP -1002 = IF 2DROP 0. THEN
 ;
-: ReadSocketExact ( a u socket -- ior )
-  READ-SOCK-EXACT
+: ReadoutSocket ( a u1 h -- a u2 ior ) \ on likeness ReadSocket
+  READOUT-SOCK
 ;
