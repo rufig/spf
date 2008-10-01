84ea0aee99e73e5a18e4160fde5c83d764ec98ba
spf <>
2008-10-01 12:42:20 +0000

    Эта библиотека незаметно отменяла работу таймаутов... Исправлено.

--- devel/~ac/lib/win/winsock/events.f
+++ devel/~ac/lib/win/winsock/events.f
@@ -6,11 +6,22 @@ WINAPI: WSAEventSelect            WS2_32.DLL
 WINAPI: MsgWaitForMultipleObjects USER32.DLL
 WINAPI: PostThreadMessageA        USER32.DLL
 WINAPI: PeekMessageA              USER32.DLL
+WINAPI: GetTickCount              KERNEL32.DLL
 
 1 10 LSHIFT 1- CONSTANT FD_ALL_EVENTS
         0x04FF CONSTANT QS_ALLINPUT
 
-USER uSocketEvent
+USER uSocketEvent \ event-сокет может быть только один per thread!
+                  \ vNoneventSocket? нужно переопредел€ть, если могут быть еще обычные сокеты
+USER uLastSocketRead \ когда последний раз был ReadSocket, а не сообщение
+\ ≈сли таймаут сокета больше частоты поступлени€ сообщений,
+\ например если поставить 500 вместо 5000 в примере ниже,
+\ то таймаут MsgWFMO не сработает, поэтому в этом случае стоит провер€ть
+\ сокет в обработчике сообщений тоже.
+\ ƒл€ упрощени€ (отмены :) этой заботы сделан трюк с uLastSocketRead.
+\   тому же теперь поток может в ответе на сообщение указать, как давно он
+\ висит на текущем ReadSocket.
+
 VARIABLE vSEvDebug
 
 VECT vProcessMessage
@@ -29,21 +40,31 @@ VECT vNoneventSocket?
 ; ' NoneventSocket?1 TO vNoneventSocket?
 
 : ReadSocket ( addr u s -- rlen ior )
-  DUP vNoneventSocket? IF ReadSocket EXIT THEN
+  DUP vNoneventSocket? IF ReadSocket GetTickCount uLastSocketRead ! EXIT THEN
   uSocketEvent @ 0= IF
     0 0 0 0 CreateEventA DUP 0= IF 2DROP 2DROP 0 GetLastError EXIT THEN
     uSocketEvent !
     DUP FD_ALL_EVENTS uSocketEvent @ ROT WSAEventSelect IF DROP 2DROP 0 GetLastError EXIT THEN
   THEN
   BEGIN
-    QS_ALLINPUT -1 0 uSocketEvent 1 MsgWaitForMultipleObjects
+    QS_ALLINPUT
+    TIMEOUT @ DUP 0= IF DROP -1 THEN
+    0 uSocketEvent 1 MsgWaitForMultipleObjects
+    DUP 258 = ( WAIT_TIMEOUT) IF 2DROP 2DROP 0 10060 EXIT THEN
     DUP 2 U< 0=
     IF vSEvDebug @ IF ." MsgWFMO error " THEN 2DROP 2DROP 0 -1003 TRUE
     ELSE 0=
       IF \ 0=сокеты
          DUP ToRead2 ?DUP IF 2>R DROP 2DROP 2R> EXIT THEN
-         IF ReadSocket TRUE ELSE FALSE THEN
+         IF ReadSocket GetTickCount uLastSocketRead ! TRUE ELSE FALSE THEN
       ELSE \ 1=сообщени€
+         \ DUP ToRead2 ?DUP IF 2>R DROP 2DROP 2R> EXIT THEN \ а жив ли сокет?
+         \ ^ здесь ToRead2 или ioctlsocket использовать нельз€, будет 10038
+         TIMEOUT @ 0<>
+         IF
+           GetTickCount uLastSocketRead @ - TIMEOUT @ >
+           IF DROP 2DROP 0 10060 EXIT THEN
+         THEN
          BEGIN
            1 0 0 0 PAD PeekMessageA
          WHILE
@@ -93,11 +114,13 @@ USER SOO USER PPP
   ( WAIT_TIMEOUT 258L)
   BEGIN
     ." w..."
-    PAD 10 SOO @ ReadSocket 2DUP . . THROW PAD SWAP TYPE
+    PAD 10 SOO @ ReadSocket 2DUP . . ." D3=" DEPTH .
+    THROW PAD SWAP TYPE
     ." ." CR
     1000 PAUSE
 ." depth2=" DEPTH . CR
   AGAIN
 ;
 : TTT ['] TEST CATCH . PPP @ STOP ;
+3000 TIMEOUT !
 TTT
