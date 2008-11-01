9fe778867bc80a9ed705a5b1427ec158e99b304a
ygreks <>
2008-11-01 09:07:18 +0000

    with history (only for the current session)

--- devel/~ygrek/lib/linux/readline.f
+++ devel/~ygrek/lib/linux/readline.f
@@ -4,9 +4,15 @@
 
 USE libreadline.so.5
 
+: start-readline-history (()) using_history DROP ;
+
+..: AT-PROCESS-STARTING start-readline-history ;..
+start-readline-history
+
 : ACCEPT-READLINE ( c-addr +n1 -- +n2 ) \ 94
   (( 0 )) readline >R
   R@ 0 = IF RDROP -1002 THROW THEN
+  (( R@ )) add_history DROP
   R@ ASCIIZ> 2SWAP ROT MIN >R R@ MOVE R>
   (( R> )) free DROP
 
