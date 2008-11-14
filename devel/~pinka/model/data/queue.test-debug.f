a69ce71c0ad1733738e1771cd3c60bac2939100d
ruv <>
2008-11-14 23:10:53 +0000

    The debug version for queue tracing

---
+++ devel/~pinka/model/data/queue.test-debug.f
@@ -0,0 +1,7 @@
+REQUIRE EMBODY    ~pinka/spf/forthml/index.f
+
+`http://forth.org.ru/~pinka/samples/2007/httpd/log.f.xml EMBODY
+
+`http://forth.org.ru/~pinka/model/data/queue.L1-debug-setup.f.xml EMBODY
+
+`queue.test.f INCLUDED
