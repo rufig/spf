ae035ca4f4da7f054b4f43e722b43ef8e93fc8f3
ygreks <>
2008-11-16 09:48:48 +0000

    sf.net changed shell access

--- tools/website/update.sh
+++ tools/website/update.sh
@@ -2,6 +2,10 @@
 #
 # Update SPF website manually (as cron is disabled at sf.net)
 # Usage:
-#   ./update.sh -l <sfusername> 
+#   ./update.sh <sfusername> 
 
-ssh $* shell.sf.net 'cd /home/groups/s/sp/spf/spf/tools/website && cvs up -d -P -C && make'
+host=$1,spf@shell.sf.net
+
+ssh $host create
+ssh $host 'cd /home/groups/s/sp/spf/spf/tools/website && cvs up -d -P -C && make'
+ssh $host shutdown
