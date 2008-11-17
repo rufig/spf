ecdb82083a0da0f577564f6e9f2d332afe2f17df
ruv <>
2008-11-17 19:41:16 +0000

    Testcase of the EXC-NO-FRAME chance

---
+++ devel/~pinka/spf/test/core.f
@@ -0,0 +1,23 @@
+\ 17.Nov.2008
+\ $Id$
+
+REQUIRE Wait ~pinka/lib/multi/Synchr.f
+REQUIRE TESTCASES ~ygrek/lib/testcase.f
+
+TESTCASES EXC-NO-FRAME
+
+: demark-frame ( -- flag )
+  0 FS@
+  DUP CELL- @ ['] DROP-EXC-HANDLER <> IF DROP FALSE EXIT THEN
+  CELL- 1+!
+  TRUE
+;
+
+: ttt CR demark-frame  0 / ; ' ttt TASK: tt
+
+: t 0 tt START DUP >R 1000 Wait R> CloseHandle DROP ;
+
+(( demark-frame -> TRUE ))
+(( t -> TRUE ))
+
+END-TESTCASES
