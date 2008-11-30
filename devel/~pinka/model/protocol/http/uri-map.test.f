0366bdcf446f576a6e907bc9de288cefe52472cc
ruv <>
2008-11-30 19:34:58 +0000

    dispatcher for http

---
+++ devel/~pinka/model/protocol/http/uri-map.test.f
@@ -0,0 +1,14 @@
+REQUIRE EMBODY    ~pinka/spf/forthml/index.f
+
+`http://forth.org.ru/~pinka/model/data/list-plain.f.xml EMBODY
+
+`uri-map.f.xml EMBODY
+
+: t1 ." t1: " pathname TYPE CR TRUE  ;  ' t1 add-handler
+: t2 ." t2  "               CR FALSE ;  ' t2 add-handler
+
+:NONAME ." key1: " pathname TYPE CR ; `key1 add-segment-handler
+
+ S" test/passed" dispatch-pathname . CR CR
+
+ S" key1/key2/key3" dispatch-pathname . CR CR
