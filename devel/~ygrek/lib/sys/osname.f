543a191ac1aaf3ef9cc7144aa6754c25adb99075
ygreks <>
2008-12-08 16:15:38 +0000

    OS name as one string

---
+++ devel/~ygrek/lib/sys/osname.f
@@ -0,0 +1,37 @@
+\ $Id$
+\ 
+\ OS_NAME ( -- s )
+\ OS name and version as a single string
+
+REQUIRE [DEFINED] lib/include/tools.f
+REQUIRE cat ~ygrek/lib/cat.f
+REQUIRE replace-str- ~pinka/samples/2005/lib/replace-str.f
+
+[DEFINED] WINAPI: [IF]
+
+REQUIRE /OSVERSIONINFO lib/win/osver.f
+
+: OSVER_INFO ( -- build minor major )
+   /OSVERSIONINFO ALLOCATE THROW DUP >R
+   /OSVERSIONINFO R@ !
+   GetVersionExA DROP
+   R@ dwBuildNumber @
+   R@ dwMinorVersion @
+   R@ dwMajorVersion @
+   R> FREE THROW
+;
+
+: OS_NAME ( -- s ) OSVER_INFO " Microsoft Windows {n}.{n}.{n}" ;
+
+[ELSE]
+
+: uname ( ? -- s )
+  S" /proc/sys/kernel/ostype" cat 
+  S" /proc/sys/kernel/osrelease" cat OVER S+
+  SWAP IF S" /proc/sys/kernel/version" cat OVER S+ THEN
+  DUP " {EOLN}" "  " replace-str- ;
+
+: OS_NAME FALSE uname ;
+
+[THEN]
+
