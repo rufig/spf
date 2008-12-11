adee718a6499d8928b128ec9326372e81adcecfc
ygreks <>
2008-12-11 10:26:34 +0000

    update

--- devel/~ygrek/prog/fsaver/filelist.f
+++ devel/~ygrek/prog/fsaver/filelist.f
@@ -1,33 +1,32 @@
 REQUIRE WildCMP-U ~pinka/lib/mask.f
 REQUIRE ITERATE-FILES ~profit/lib/iterate-files.f
-REQUIRE write-list ~ygrek/lib/list/all.f
+REQUIRE list-make ~ygrek/lib/list/make.f
 REQUIRE SGENRAND ~ygrek/lib/neilbawd/mersenne.f
+REQUIRE ms@ lib/include/facil.f
 
 MODULE: filelist
 
 0 VALUE list-size
-() VALUE list
+list::nil VALUE l
 
 : all-files% ( a u -- )
     100 ITERATE-FILES NIP ( a u flag )
       IF ( directory) 2DROP EXIT THEN
-      2DUP S" *.f" WildCMP-U 0= IF >STR %s ELSE 2DROP THEN ;
-
-WINAPI: GetTickCount kernel32.dll
+      2DUP S" *.f" WildCMP-U 0= IF >STR % ELSE 2DROP THEN ;
 
 EXPORT
 
-: randomName list-size GENRANDMAX list nth car STR@ ; 
+: randomName list-size GENRANDMAX l list::nth list::car STR@ ; 
 
 : names-init { a u -- }
-   GetTickCount SGENRAND
-   %[ a u all-files% ]% TO list
-   list empty? 
+   ms@ SGENRAND
+   %[ a u all-files% ]% TO l
+   list list::empty?
    IF
-    %[ a u " Tweak settings. Bad path : {s}" %s ]% TO list 
+    %[ a u " Tweak settings. Bad path : {s}" % ]% TO l
     1 TO list-size
    ELSE
-    list length TO list-size
+    l list::length TO list-size
    THEN ;
 
 ;MODULE
