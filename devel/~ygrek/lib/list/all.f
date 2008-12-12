fdfe836b8ca4d0ab106f874d09f1f79b3e0b3a80
ygreks <>
2008-12-12 14:47:09 +0000

    prevent multiple definitions of list-all

--- devel/~ygrek/lib/list/all.f
+++ devel/~ygrek/lib/list/all.f
@@ -1,8 +1,12 @@
 \ $Id$
 
+REQUIRE [UNDEFINED] lib/include/tools.f
+
 REQUIRE list ~ygrek/lib/list/core.f
 REQUIRE list-ext ~ygrek/lib/list/ext.f
 REQUIRE list-sort ~ygrek/lib/list/sort.f
 REQUIRE list-make ~ygrek/lib/list/make.f
 
+[UNDEFINED] list-all [IF]
 0 CONSTANT list-all
+[THEN]
