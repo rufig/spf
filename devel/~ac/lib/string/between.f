16107626b42ff280a0f30d9543f001db6726af75
spf <>
2009-02-21 18:37:37 +0000

    ruv: Третий момент: Лучше будут имена StringBefore, StringAfter и StringBetween (или Substring*)...

--- devel/~ac/lib/string/between.f
+++ devel/~ac/lib/string/between.f
@@ -4,24 +4,29 @@
 
 REQUIRE {             ~ac/lib/locals.f
 
-: StrBefore { a u sa su -- fa fu flag }
+: StringBefore { a u sa su -- fa fu flag }
   a u sa su SEARCH
   IF NIP u SWAP - a SWAP TRUE
   ELSE FALSE THEN
 ;
-: StrAfter { a u sa su -- fa fu flag }
+: StringAfter { a u sa su -- fa fu flag }
   a u sa su SEARCH
   IF su - SWAP su + SWAP TRUE
   ELSE FALSE THEN
 ;
-: StrBetween { a u aa au ba bu -- fa fu }
-  a u aa au StrAfter DROP ba bu StrBefore DROP
+: StringBetween { a u aa au ba bu -- fa fu flag }
+  a u aa au StringAfter
+  IF ba bu StringBefore
+  ELSE FALSE THEN
+;
+: SBetween { a u aa au ba bu -- fa fu }
+  a u aa au StringAfter DROP ba bu StringBefore DROP
 ;
 
 \EOF
 
-S" OpenSSL 0.9.8g 19 Oct 2007" S" SSL " StrAfter DROP S"  " StrBefore DROP TYPE CR
-S" libcurl/7.18.0 OpenSSL/0.9.8g zlib/1.2.3 libssh2/0.17" S" libcurl/" StrAfter DROP S"  " StrBefore DROP TYPE CR
+S" OpenSSL 0.9.8g 19 Oct 2007" S" SSL " StringAfter DROP S"  " StringBefore DROP TYPE CR
+S" libcurl/7.18.0 OpenSSL/0.9.8g zlib/1.2.3 libssh2/0.17" S" libcurl/" StringAfter DROP S"  " StringBefore DROP TYPE CR
 
-S" OpenSSL 0.9.8g 19 Oct 2007" S" SSL " S"  " StrBetween TYPE CR
-S" libcurl/7.18.0 OpenSSL/0.9.8g zlib/1.2.3 libssh2/0.17" S" libcurl/" S"  " StrBetween TYPE CR
+S" OpenSSL 0.9.8g 19 Oct 2007" S" SSL " S"  " StringBetween . TYPE CR
+S" libcurl/7.18.0 OpenSSL/0.9.8g zlib/1.2.3 libssh2/0.17" S" libcurl/" S"  " SBetween TYPE CR
