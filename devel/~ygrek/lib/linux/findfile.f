b31f4f81ff70712b547eb2d3b8d27662a4ef21f5
ygreks <>
2009-02-28 10:19:36 +0000

    implement FIND-FILES, fix stack bug in READDIR

--- devel/~ygrek/lib/linux/findfile.f
+++ devel/~ygrek/lib/linux/findfile.f
@@ -1,17 +1,16 @@
 \ $Id$
 \ 
-\ interface [will be] compatible with ~ac/lib/win/file/findfile-r.f
-\ READDIR ( a u xt -- )
-\ a u -- directory to enumerate files in
-\ xt ( a u data dir? -- )
-\ a u -- path
-\ data -- implementation defined information about entry
-\ dir? -- true if directory
+\ FIND-FILES and FIND-FILES-R words compatible with ~ac/lib/win/file/findfile-r.f
+
+\ REQUIRE MemReport ~day/lib/memreport.f
 
 REQUIRE ANSI-FILE lib/include/ansi-file.f
 REQUIRE CEQUAL ~pinka/spf/string-equal.f
 REQUIRE LINUX-CONSTANTS lib/posix/const.f
 REQUIRE /TEST ~profit/lib/testing.f
+REQUIRE KEEP! ~profit/lib/bac4th.f
+REQUIRE STR@ ~ac/lib/str5.f
+REQUIRE LAMBDA{ ~pinka/lib/lambda.f
 
 : IS..? ( a u -- ? )
   2DUP S" .." CEQUAL IF 2DROP TRUE EXIT THEN
@@ -19,10 +18,11 @@ REQUIRE /TEST ~profit/lib/testing.f
 
 : IS-DIR? ( dirent -- ? ) OFFSETOF_D_TYPE + B@ DT_DIR = ;
 
-: READDIR ( a u xt -- ) \ xt: ( a u data dir? -- )
+\ readdir(3) wrapper
+: READDIR ( a u xt -- ) \ xt: ( a u data dir? -- ) \ a u -- only filename
   >R
   ANSI-FILE::>ZFILENAME DROP 1 <( )) opendir
-  DUP 0= IF RDROP EXIT THEN
+  DUP 0= IF DROP RDROP EXIT THEN
   BEGIN
    DUP 1 <( )) readdir ?DUP 
   WHILE
@@ -33,9 +33,19 @@ REQUIRE /TEST ~profit/lib/testing.f
   RDROP
   1 <( )) closedir DROP ;
 
-: FIND-FILES ( a u xt -- )
-  TRUE ABORT" need full paths here" 
-  READDIR ;
+USER _xt
+USER _dir
+
+\ quick and dirty
+\ FIXME if xt throws exception -- _dir is not STRFREE'd
+: FIND-FILES ( a u xt -- )  
+  OVER 0 = IF 2DROP DROP EXIT THEN
+  _xt KEEP!
+  >STR _dir KEEP!
+  _dir @ STR@ 1- + C@ [CHAR] / <> IF S" /" _dir @ STR+ THEN
+  _dir @ STR@ LAMBDA{ 2SWAP _dir @ STR@ " {s}{s}" >R R@ STR@ 2SWAP _xt @ EXECUTE R> STRFREE } READDIR 
+  _dir @ STRFREE
+  ;
 
 : FIND-FILES-R ( a u xt -- )
   TRUE ABORT" not implemented"
@@ -43,5 +53,13 @@ REQUIRE /TEST ~profit/lib/testing.f
 
 /TEST
 
+\ print all files in current directory
 S" ." :NONAME ( a u data dir? -- ) . DROP TYPE CR ; READDIR
+
+\ test reenterability
+S" ~ygrek/spf" FIND-FULLNAME :NONAME NIP IF LAMBDA{ ." | " . DROP TYPE CR } FIND-FILES ELSE TYPE CR THEN ; FIND-FILES
+
+[DEFINED] MemReport [IF]
+MemReport
+[THEN]
 
