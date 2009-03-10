39b01ac7daf328ad4d2f192085dd0c568474256e
ruv <>
2009-03-10 06:22:42 +0000

    move READ-FILE-EXACT to another file

--- devel/~pinka/lib/files-ext.f
+++ devel/~pinka/lib/files-ext.f
@@ -5,4 +5,11 @@
   DUP 109 = IF 2DROP 0. THEN
 ;
 
-\ see also: ~pinka/lib/win/pipes.f -- READ-FILE-EXACT ( addr u h -- ior )
+: READ-FILE-EXACT ( addr u h -- ior )
+  >R BEGIN DUP WHILE
+    2DUP R@ READ-FILE ?DUP IF NIP NIP NIP RDROP EXIT THEN ( a1 u1 u2 )
+    DUP 0= IF DROP 2DROP RDROP -1002 EXIT THEN \ 109 is not throwable in spf4
+    TUCK - -ROT + SWAP
+  REPEAT ( a1 0 )
+  NIP RDROP
+;
--- devel/~pinka/lib/win/pipes.f
+++ devel/~pinka/lib/win/pipes.f
@@ -21,12 +21,3 @@ WINAPI: PeekNamedPipe kernel32.dll
   R> \ hNamedPipe
   PeekNamedPipe ERR THROW
 ;
-
-: READ-FILE-EXACT ( addr u h -- ior )
-  >R BEGIN DUP WHILE
-    2DUP R@ READ-FILE ?DUP IF NIP NIP NIP RDROP EXIT THEN ( a1 u1 u2 )
-    DUP 0= IF DROP 2DROP RDROP -1002 EXIT THEN \ 109 is not throwable in spf4
-    TUCK - -ROT + SWAP
-  REPEAT ( a1 0 )
-  NIP RDROP
-;
