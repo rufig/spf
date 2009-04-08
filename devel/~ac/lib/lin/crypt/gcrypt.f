a1368596f593268b5df6a3f296a0f5c1825a710d
spf <>
2009-04-08 16:53:30 +0000

    Обертки для бинарных вариантов функций MD5B и SHA1B, а также HMAC.

---
+++ devel/~ac/lib/lin/crypt/gcrypt.f
@@ -0,0 +1,68 @@
+( Обертки для бинарных вариантов функций MD5B и SHA1B, а также HMAC.
+  Традиционные строчные MD5 см. в ~clf/md5-ts.f
+  $Id$
+)
+\ Кроме libgcrypt.dll требуется libintl-2.dll
+\ Многопоточность см. http://www.gnupg.org/documentation/manuals/gcrypt/Multi_002dThreading.html
+
+REQUIRE SO            ~ac/lib/ns/so-xt.f
+REQUIRE {             lib/ext/locals.f
+
+37 CONSTANT GCRYCTL_DISABLE_SECMEM
+38 CONSTANT GCRYCTL_INITIALIZATION_FINISHED
+39 CONSTANT GCRYCTL_INITIALIZATION_FINISHED_P
+
+1 CONSTANT GCRY_MD_MD5
+2 CONSTANT GCRY_MD_SHA1
+
+2 CONSTANT GCRY_MD_FLAG_HMAC
+
+ALSO SO NEW: libgcrypt.dll
+ALSO SO NEW: libgcrypt.so.11
+
+: HMAC { addr u keya keyu algo \ hd mdlen p -- ha hu }
+  GCRY_MD_FLAG_HMAC algo ^ hd 3 gcry_md_open THROW
+  algo 1 gcry_md_get_algo_dlen -> mdlen
+  keyu keya hd 3 gcry_md_setkey THROW
+  u addr hd 3 gcry_md_write DROP
+  0 hd 2 gcry_md_read -> p
+  p mdlen HEAP-COPY mdlen
+  hd 1 gcry_md_close DROP
+;
+: MD5B ( addr u -- ha hu )
+  16 ALLOCATE THROW >R
+  SWAP R@ GCRY_MD_MD5 4 gcry_md_hash_buffer DROP
+  R> 16
+;
+: SHA1B ( addr u -- ha hu )
+  20 ALLOCATE THROW >R
+  SWAP R@ GCRY_MD_SHA1 4 gcry_md_hash_buffer DROP
+  R> 20
+;
+: GCryptInit ( -- flag )
+  S" 1.2.1" DROP 1 gcry_check_version ?DUP
+  IF ( ASCIIZ> TYPE) DROP ELSE ( ." unknown libgcrypt version") FALSE EXIT THEN
+  0 GCRYCTL_DISABLE_SECMEM 2 gcry_control THROW
+  0 GCRYCTL_INITIALIZATION_FINISHED 2 gcry_control THROW
+  0 GCRYCTL_INITIALIZATION_FINISHED_P 2 gcry_control
+;
+PREVIOUS
+PREVIOUS
+
+\EOF
+
+: TEST
+  GCryptInit
+  IF
+    S" test" MD5B DUMP \ возвращает 16 байт 09 8F 6B CD  46 21 D3 73  CA DE 4E 83  26 27 B4 F6
+                       \ в отличие от 32-байтной строки 098f6bcd4621d373cade4e832627b4f6, которую дает md5.f:MD5
+                       \ т.е. строка кодируется аналогично blob'ам в sql (см. xmldb2.f:DeBlob)
+    CR
+    S" test" SHA1B DUMP \ аналогично
+    CR
+    \ тесты из RFC2202:
+    S" what do ya want for nothing?" S" Jefe" GCRY_MD_MD5 HMAC DUMP CR \ 0x750c783e6ab0b503eaa86e310a5db738
+    S" what do ya want for nothing?" S" Jefe" GCRY_MD_SHA1 HMAC DUMP CR \ 0xeffcdf6ae5eb2fa2d27416d5f184df9c259a7c79
+  THEN
+;
+TEST
