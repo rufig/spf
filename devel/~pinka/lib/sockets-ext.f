ac598c069541a8c77f8fd058ed2b57b3204dd6f8
ruv <>
2009-04-20 14:14:22 +0000

    Упс, слово ReadSocketExact тут уже было как-то раньше )

--- devel/~pinka/lib/sockets-ext.f
+++ devel/~pinka/lib/sockets-ext.f
@@ -9,10 +9,5 @@ REQUIRE ReadSocket ~ac/lib/win/winsock/SOCKETS.F
   READOUT-SOCK
 ;
 
-: ReadSocketExact ( a u socket -- ior )
-  >R BEGIN DUP WHILE
-    2DUP R@ ReadSocket ?DUP IF NIP NIP NIP RDROP EXIT THEN ( a1 u1 u2 )
-    TUCK - -ROT + SWAP
-  REPEAT ( a1 0 )
-  NIP RDROP
-;
+\ ReadSocketExact ( a u socket -- ior )
+\ -- в базовом ~ac/lib/win/winsock/SOCKETS.F
