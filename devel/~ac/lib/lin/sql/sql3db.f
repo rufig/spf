edf1ffa35bd521b40630113125cf5e107ccd0d9d
spf <>
2009-04-20 02:14:38 +0000

    mquery - таблицы со стилями (еще один вынос из Eserv'а).

--- devel/~ac/lib/lin/sql/sql3db.f
+++ devel/~ac/lib/lin/sql/sql3db.f
@@ -54,3 +54,37 @@ USER SQS
   " </tbody></table>" SQS @ S+
   SQS @ STR@
 ;
+
+: (mquery) { i par ppStmt -- flag }
+  i 1 =
+  IF " <thead><tr class='sp_head'>" SQS @ S+
+    ppStmt db3_cols 0 ?DO
+      I ppStmt db3_colname
+      2DUP S" __" SEARCH IF NIP - ELSE 2DROP THEN
+      DUP IF 2DUP " <th class='{s}'>{s}</th>" SQS @ S+ ELSE 2DROP THEN
+    LOOP " </tr></thead>{CRLF}<tbody>" SQS @ S+
+  THEN
+  S" __tagsField" ppStmt db3_field
+  i 1 AND 0= IF S"  even" ELSE S" " THEN
+  i " <tr N='{n}' class='sp_data{s} sp_tag_{s}'>" SQS @ S+
+  ppStmt db3_cols 0 ?DO
+    I ppStmt db3_colu DUP 0= 
+             IF 2DROP S" &nbsp;"
+             ELSE
+                I ppStmt db3_colname S" __" SEARCH
+                IF SFIND IF ppStmt SWAP EXECUTE ELSE 2DROP THEN
+                ELSE 2DROP THEN
+             THEN
+    I ppStmt db3_coltype 3 < IF S"  numb" ELSE S" " THEN
+    I ppStmt db3_colname
+    2DUP S" __" SEARCH IF NIP - ELSE 2DROP THEN
+    DUP IF " <td class='{s}{s}'>{s}</td>" SQS @ S+ ELSE 2DROP 2DROP 2DROP THEN
+  LOOP  " </tr>{CRLF}" SQS @ S+
+  TRUE
+;
+: mquery ( addr u -- addr2 u2 ) \ вариант с модификаторами полей и тегами/стил€ми TR
+  " <table class='sortable' id='sp_table' cellpadding='0' cellspacing='0'>" SQS !
+  0 ['] (mquery) SQH @ db3_exec
+  " </tbody></table>" SQS @ S+
+  SQS @ STR@
+;
