d11b1ebfe8ccb3477dc6a6d2087ec0e96e764c71
spf <>
2009-04-24 09:08:05 +0000

    Запись имени последней вызванной API-функции для дампа внутри EXCEPTION'а.

---
+++ devel/~ac/lib/tools/api_trace.f
@@ -0,0 +1,17 @@
+\ все WINAPI-функции будут оставл€ть след в uLastApiFunc,
+\ чтобы в случае EXCEPTION'а вне тела форта иметь намЄк на сбойнувшую функцию
+
+USER uLastApiFunc
+
+: _WINAPI-TRACE
+  uLastApiFunc !
+;
+: WINAPI: ( "»м€ѕроцедуры" "»м€Ѕиблиотеки" -- )
+  NEW-WINAPI?
+  IF HEADER
+  ELSE -1 >IN @ HEADER >IN ! THEN
+  LATEST LIT,                      \  LATEST ] POSTPONE LITERAL POSTPONE [  :)
+  POSTPONE _WINAPI-TRACE
+  POSTPONE _WINAPI-CODE
+  __WIN:
+;
