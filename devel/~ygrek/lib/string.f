ba72b86a4961179ed601ecf849c8720a61f69c55
ygreks <>
2009-05-12 20:05:58 +0000

    Add comments header

--- devel/~ygrek/lib/string.f
+++ devel/~ygrek/lib/string.f
@@ -1,9 +1,11 @@
+\ $Id$
+\ Beware, no output buffer bounds checking
 
 \ Append a char to the end of the string
 : CHAR-APPEND ( a1 u1 c -- ) -ROT CHARS + C! ;
 
 \ append a string to the buffer
-: STR-APPEND ( a1 u1 a2 u2 -- ) 2SWAP CHARS + SWAP CMOVE> ;
+: STRING-APPEND ( a1 u1 a2 u2 -- ) 2SWAP CHARS + SWAP CMOVE> ;
 
 \ get string bounds
 : BOUNDS ( addr u -- addr-end addr ) CHARS OVER + SWAP ;
