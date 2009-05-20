b21f4846f816268fc70c66fa19b41a106ef3b752
spf <>
2009-05-20 03:25:59 +0000

    tquery - простейший вариант с plaintext результатом.

--- devel/~ac/lib/lin/sql/sql3db.f
+++ devel/~ac/lib/lin/sql/sql3db.f
@@ -36,7 +36,7 @@ USER SQS
   i 1 AND 0= IF S"  even" ELSE S" " THEN
   i " <tr N='{n}' class='sp_data{s}'>" SQS @ S+
   ppStmt db3_cols 0 ?DO
-    I ppStmt db3_colu DUP 0= ( >R 2DUP S" NULL" COMPARE 0= R> OR) IF 2DROP S" &nbsp;" THEN
+    I ppStmt db3_colu DUP 0= ( >R 2DUP S" NULL" COMPARE 0= R> OR) IF 2DROP S" &#160;" THEN
     I ppStmt db3_coltype 3 < IF S"  numb" ELSE S" " THEN
     I ppStmt db3_colname  " <td class='{s}{s}'>{s}</td>" SQS @ S+
   LOOP  " </tr>{CRLF}" SQS @ S+
@@ -69,7 +69,7 @@ USER SQS
   i " <tr N='{n}' class='sp_data{s} sp_tag_{s}'>" SQS @ S+
   ppStmt db3_cols 0 ?DO
     I ppStmt db3_colu DUP 0= 
-             IF 2DROP S" &nbsp;"
+             IF 2DROP S" &#160;"
              ELSE
                 I ppStmt db3_colname S" __" SEARCH
                 IF SFIND IF ppStmt SWAP EXECUTE ELSE 2DROP THEN
@@ -86,5 +86,18 @@ USER SQS
   " <table class='sortable' id='sp_table' cellpadding='0' cellspacing='0'>" SQS !
   0 ['] (mquery) SQH @ db3_exec
   " </tbody></table>" SQS @ S+
+  SQS @ STR@
+;
+: (tquery) { i par ppStmt \ n -- flag }
+  ppStmt db3_cols DUP -> n
+  0 ?DO
+    I ppStmt db3_colu SQS @ STR+
+    I n 1- = IF CRLF ELSE S"  " THEN SQS @ STR+
+  LOOP
+  TRUE
+;
+: tquery ( addr u -- addr2 u2 ) \ простейший вариант с plaintext результатом
+  "" SQS !
+  0 ['] (tquery) SQH @ db3_exec
   SQS @ STR@
 ;
