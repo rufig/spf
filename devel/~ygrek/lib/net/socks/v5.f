823efd9d56c6f5e1c0c8bd1e83b79726d334db24
ygreks <>
2009-05-31 09:47:46 +0000

    get rid of hardcoded exception numbers

--- devel/~ygrek/lib/net/socks/v5.f
+++ devel/~ygrek/lib/net/socks/v5.f
@@ -7,15 +7,28 @@
 
 REQUIRE ConnectHost ~ygrek/lib/net/sockets.f
 REQUIRE { lib/ext/locals.f
-REQUIRE CASE lib/ext/case.f
 REQUIRE /TEST ~profit/lib/testing.f
+REQUIRE EXCEPTION ~ygrek/lib/exception.f
+REQUIRE STHROW ~pinka/spf/sthrow.f
 
 MODULE: SOCKS5
 
-4624000 VALUE IOR_BASE
+: EXN EXCEPTION CONSTANT ;
+
+S" Connection to proxy server failed." EXN #connect
+S" Step1: send hello failed" EXN #hello
+S" Didn't get reply" EXN #noreply
+S" Bad reply (incorrect length)" EXN #badreply
+S" Step1: bad proto version in reply" EXN #badproto
+S" Step1: bad auth" EXN #badauth
+S" Step2: send CONNECT failed" EXN #send
+S" Step2: bad proto version in reply" EXN #badproto2
+S" Step2: CONNECT failed" EXN #connect2
+S" Step2: Domain format unsupported" EXN #domain
+S" Step2: No response" EXN #noresponse
 
 \ Выкинуть ior если условие f ложно
-: MUST ( ? ior -- ) SWAP IF DROP ELSE IOR_BASE + THROW THEN ;
+: MUST ( ? ior -- ) SWAP IF DROP ELSE THROW THEN ;
 
 \ Пакет для "знакомства" с прокси сервером
 CREATE step1 3 C, ( count) 5 C, ( version) 1 C, ( methods) 0 C, ( noauth)
@@ -24,7 +37,7 @@ CREATE step1 3 C, ( count) 5 C, ( version) 1 C, ( methods) 0 C, ( noauth)
 : 2nd-byte ( u -- c ) 8 RSHIFT 0xFF AND ;
 
 \ Собрать пакет для CONNECT в буфере addr
-: step2 ( a u port addr -- ) 
+: step2 ( a u port addr -- )
     HERE >R
     DP !
     OVER 7 + C, \ COUNT - not proto
@@ -33,7 +46,7 @@ CREATE step1 3 C, ( count) 5 C, ( version) 1 C, ( methods) 0 C, ( noauth)
      0 C, \ reserved
      3 C, \ domain
      ( a u port)
-     >R 
+     >R
      255 MIN \ prevent buffer overflow
      S", \ FQDN
      R> ( port) DUP 2nd-byte C, C, \ port (reversed bytes)
@@ -41,57 +54,37 @@ CREATE step1 3 C, ( count) 5 C, ( version) 1 C, ( methods) 0 C, ( noauth)
     ;
 
 : ProxyStep1 ( a u port -- sock )
-   ConnectHost 0= 1 MUST
+   ConnectHost 0= #connect MUST
    { sock | [ 2 ] reply }
-   step1 COUNT sock WriteSocket 0= 2 MUST
-   reply 2 sock ReadSocket 0= 3 MUST
-   2 = 4 MUST
-   reply C@ 5 = 5 MUST
-   reply 1+ C@ 0 = 6 MUST
+   step1 COUNT sock WriteSocket 0= #hello MUST
+   reply 2 sock ReadSocket 0= #noreply MUST
+   2 = #badreply MUST
+   reply C@ 5 = #badproto MUST
+   reply 1+ C@ 0 = #badauth MUST
    sock ;
 
 : ProxyStep2 ( a u port sock -- )
   500 ALLOCATE THROW { sock buf } \ хватит т.к. в пакете меньше 10 ячеек + FQDN который меньше 256
-  buf step2 
+  buf step2
   buf COUNT \ 2DUP DUMP
-  sock WriteSocket 0= 7 MUST
-  buf 4 sock ReadSocket 0= 11 MUST 4 = 4 MUST
-  buf C@ 5 = 8 MUST
-  buf 1 + C@ 0 = 9 MUST
+  sock WriteSocket 0= #send MUST
+  buf 4 sock ReadSocket 0= #noresponse MUST 4 = #badreply MUST
+  buf C@ 5 = #badproto2 MUST
+  buf 1 + C@ 0 = #connect2 MUST
   buf 2 + C@ 0 <> IF CR ." Warning: rsrv used" THEN
-  buf 3 + C@ 3 = IF 
-  buf 1 sock ReadSocket 0= 3 MUST 1 = 4 MUST
-  buf 1+ buf C@ 2+ sock ReadSocket 0= 3 MUST buf C@ = 4 MUST
+  buf 3 + C@ 3 = IF
+  buf 1 sock ReadSocket 0= #noreply MUST 1 = #badreply MUST
+  buf 1+ buf C@ 2+ sock ReadSocket 0= #noreply MUST buf C@ = #badreply MUST
   ELSE
-   buf 3 + C@ 1 = IF buf 4 2 + sock ReadSocket 0= 3 MUST 6 = 4 MUST
-  ELSE 
-   buf 3 + C@ 4 = IF buf 16 2 + sock ReadSocket 0= 3 MUST 18 = 4 MUST
+   buf 3 + C@ 1 = IF buf 4 2 + sock ReadSocket 0= #noreply MUST 6 = #badreply MUST
   ELSE
-   FALSE 10 MUST
+   buf 3 + C@ 4 = IF buf 16 2 + sock ReadSocket 0= #noreply MUST 18 = #badreply MUST
+  ELSE
+   #domain THROW
   THEN
   THEN
   THEN
   buf FREE THROW ;
-
-\ Обьяснить ior возвращаемый этим модулем
-: explain ( ior -- a u )
-   DUP 0= IF 0 EXIT THEN
-   IOR_BASE -
-   CASE
-     1 OF S" Connection to proxy server failed." ENDOF
-     2 OF S" Step1: send hello failed" ENDOF
-     3 OF S" Didn't get reply" ENDOF
-     4 OF S" Bad reply (incorrect length)" ENDOF
-     5 OF S" Step1: bad proto version in reply" ENDOF
-     6 OF S" Step1: bad auth" ENDOF
-     7 OF S" Step2: send CONNECT failed" ENDOF
-     8 OF S" Step2: bad proto version in reply" ENDOF
-     9 OF S" Step2: CONNECT failed" ENDOF
-    10 OF S" Step2: Domain format unsupported" ENDOF
-    11 OF S" Step2: No response" ENDOF
-   ENDCASE ;
-
-: ETHROW DUP IF explain CR TYPE CR -1 THROW ELSE DROP THEN ;
 
 EXPORT
 
@@ -99,7 +92,7 @@ EXPORT
 \ через SOCKS5 прокси "proxy-au:proxy-port"
 \ Если proxy-port = 0, то соединение идёт напрямую, минуя прокси
 \ Если соединение установлено - ior = 0
-: ConnectHostViaSocks5 ( host-a u port proxy-a u proxy-port -- sock ior ) 
+: ConnectHostViaSocks5 ( host-a u port proxy-a u proxy-port -- sock ior )
    DUP 0= IF DROP 2DROP ConnectHost EXIT THEN
    ['] ProxyStep1 CATCH DUP IF >R 2DROP 2DROP 2DROP 0 R> EXIT ELSE DROP THEN
    >R
@@ -110,6 +103,8 @@ EXPORT
 
 ;MODULE
 
+: ETHROW ( exn -- ) ?DUP IF EXCEPTION>TEXT STHROW THEN ;
+
 /TEST
 
 REQUIRE fgets ~ac/lib/win/winsock/PSOCKET.F
@@ -117,7 +112,7 @@ REQUIRE fgets ~ac/lib/win/winsock/PSOCKET.F
 \ Попробуем добыть страничку через Tor
 : test { | s }
   SocketsStartup THROW
-  S" forth.org.ru" 80 S" localhost" 9050 ConnectHostViaSocks5 SOCKS5::ETHROW SocketLine TO s
+  S" forth.org.ru" 80 S" localhost" 9050 ConnectHostViaSocks5 ETHROW SocketLine TO s
 " GET / HTTP/1.0
 Host: www.forth.org.ru
 Connection: close
