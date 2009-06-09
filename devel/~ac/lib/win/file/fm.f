fdd56453a8c49921356d5a16902c6fde7fccf79a
spf <>
2009-06-09 18:09:41 +0000

    В SPF уже есть подходящие реализации openfile/createfile.

--- devel/~ac/lib/win/file/fm.f
+++ devel/~ac/lib/win/file/fm.f
@@ -13,42 +13,6 @@ DECIMAL
 
 0 VALUE MAP-BASE
 
-( ниже новые варианты CREATE-FILE и OPEN-FILE с разделением доступа)
-
-: CREATE-FILE ( c-addr u fam -- fileid ior ) \ 94 FILE
-\ Создать файл с именем, заданным c-addr u, и открыть его в соответствии
-\ с методом доступа fam. Смысл значения fam определен реализацией.
-\ Если файл с таким именем уже существует, создать его заново как
-\ пустой файл.
-\ Если файл был успешно создан и открыт, ior нуль, fileid его идентификатор,
-\ и указатель чтения/записи установлен на начало файла.
-\ Иначе ior - определенный реализацией код результата ввода/вывода,
-\ и fileid неопределен.
-  SWAP DROP SWAP >R >R
-  0 FILE_ATTRIBUTE_ARCHIVE ( template attrs )
-  CREATE_ALWAYS
-  0 ( secur )
-  FILE_SHARE_READ FILE_SHARE_WRITE OR ( share )  
-  R> ( access=fam )
-  R> ( filename )
-  CreateFileA DUP -1 = IF GetLastError ELSE 0 THEN
-;
-: OPEN-FILE ( c-addr u fam -- fileid ior ) \ 94 FILE
-\ Открыть файл с именем, заданным строкой c-addr u, с методом доступа fam.
-\ Смысл значения fam определен реализацией.
-\ Если файл успешно открыт, ior ноль, fileid его идентификатор, и файл
-\ позиционирован на начало.
-\ Иначе ior - определенный реализацией код результата ввода/вывода,
-\ и fileid неопределен.
-  SWAP DROP SWAP >R >R
-  0 FILE_ATTRIBUTE_ARCHIVE ( template attrs )
-  OPEN_EXISTING
-  0 ( secur )
-  FILE_SHARE_READ FILE_SHARE_WRITE OR ( share )
-  R> ( access=fam )
-  R> ( filename )
-  CreateFileA DUP -1 = IF GetLastError ELSE 0 THEN
-;
 : MAP-FILE ( size c-addr u -- fileid objid mapbase ior )
 \ Отобразить файл с именем c-addr u в адресное пространство процесса
 \ начиная с выбранного системой адреса. Берется фрагмент файла
@@ -56,8 +20,8 @@ DECIMAL
 \ Возвращается код ошибки ior или ноль, адрес отображения mapbase,
 \ хэндлы объекта-отображения и объекта-отображаемого файла.
   OVER >R
-  2DUP R/W OPEN-FILE      ( size c-addr u fileid ior )
-  IF DROP R/W CREATE-FILE
+  2DUP R/W OPEN-FILE-SHARED      ( size c-addr u fileid ior )
+  IF DROP R/W CREATE-FILE-SHARED
   ELSE NIP NIP 0 THEN        ( size fileid ior )
   ?DUP IF NIP R> SWAP 0 SWAP EXIT THEN \ не удалось открыть/создать файл
                      ( size fileid )
