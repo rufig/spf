033d6bce6c44c361c4caafea3f5617a6d4637143
spf <>
2009-07-10 15:56:07 +0000

    Правила такие: - если на конце строки слэш, то передан каталог, и шаблон *.* - если строка указывает на реальный каталог, то передан каталог, и шаблон *.* - иначе это путь с шаблоном, и его надо резать В FIND-FILES-U помещается длина пути к каталогу (без слэша) Каталоги обходятся все, файлы выбираются сравнением с шаблоном через WildCMP-U (~pig)

--- devel/~ac/lib/win/file/findfile-r.f
+++ devel/~ac/lib/win/file/findfile-r.f
@@ -1,4 +1,5 @@
 ( 26.01.03 А.Ч. )
+( 08.07.09 ~pig )
 
 \ Рекурсивный обход каталогов и выполнение 
 \ групповых действий над файлами и каталогами.
@@ -18,6 +19,7 @@
 REQUIRE FIND-FILES       ~ac/lib/win/file/FINDFILE.F
 REQUIRE {                ~ac/lib/locals.f
 REQUIRE STR@             ~ac/lib/str5.f
+REQUIRE WildCMP-U        ~pinka/lib/mask.f
 
 USER FIND-FILES-RL    \ уровень вложенности 0-...
 USER FIND-FILES-DEPTH \ ограничение вложенности, 0 - без ограничения,
@@ -35,11 +37,29 @@ USER FIND-FILES-USE-RET \ true, если надо вызывать xt также при выходе
 \ addr u - имя каталога для обхода
 \ xt ( addr u data flag -- ) - процедура вызываемая для каждого файла
 
-  { addr u xt \ addr2 data id f dir }
+  { addr u xt \ addr2 up tmpla tmplu data id f dir }
 
-  FIND-FILES-RL @ 0= IF u FIND-FILES-U ! THEN
-  addr u S" *" SEARCH NIP NIP
-  addr u ROT IF " {s}" ELSE " {s}/*.*" THEN -> addr2
+  u				\ пустой путь обработать особым образом
+  IF
+    addr u 1- + C@ is_path_delimiter	\ если на конце слэш - запрошен путь к каталогу
+    IF
+      u 1- S" *.*"		\ слэш для корректности отрезать
+    ELSE
+      addr GetFileAttributesA DUP FILE_ATTRIBUTE_DIRECTORY AND
+      0<> SWAP -1 <> AND	\ IsDirectory - если это каталог, то задан путь без шаблона
+      IF
+        u S" *.*"
+      ELSE
+        addr u 2DUP CUT-PATH NIP DUP 1- 0 MAX SWAP
+        2SWAP -ROT OVER + -ROT - 0 MAX
+      THEN
+    THEN
+  ELSE
+    0 S" *.*"			\ обойти весь текущий диск (как в FIND-DIRS-R)
+  THEN
+  -> tmplu -> tmpla -> up
+  FIND-FILES-RL @ 0= IF up FIND-FILES-U ! THEN	\ фиксируется длина базового пути без шаблона
+  addr up " {s}/*.*" -> addr2	\ поиск всегда по всем файлам
   /WIN32_FIND_DATA ALLOCATE THROW -> data
   data /WIN32_FIND_DATA ERASE
   data addr2 STR@ DROP FindFirstFileA -> id
@@ -47,22 +67,29 @@ USER FIND-FILES-USE-RET \ true, если надо вызывать xt также при выходе
   BEGIN
     data cFileName ASCIIZ> 2DUP IsNot.. FIND-FILES.. @ OR
     IF
-      data dwFileAttributes @ FILE_ATTRIBUTE_DIRECTORY AND 0<> -> dir
-      addr u 2DUP + 1- C@ [CHAR] * =
-      IF 1-
-         BEGIN 2DUP + 1- C@ DUP [CHAR] \ <> SWAP [CHAR] / <> AND WHILE 1- REPEAT 1-
+      data dwFileAttributes @ FILE_ATTRIBUTE_DIRECTORY AND 0<> DUP -> dir	\ либо каталог
+      ?DUP 0=
+      IF
+        tmpla tmplu S" *.*" COMPARE	\ *.* - особый шаблон, с ним сравнивается всё
+        IF 2DUP tmpla tmplu WildCMP-U 0= ELSE TRUE THEN	\ либо файл проходит сравнение с шаблоном
       THEN
-      " {s}/{s}" DUP -> f STR@ data dir xt EXECUTE
-      dir
-      IF FIND-FILES-RL 1+!
-         FIND-FILES-RL @ FIND-FILES-DEPTH @ < FIND-FILES-DEPTH @ 0= OR
-         data cFileName ASCIIZ> IsNot.. AND
-         IF f STR@ xt RECURSE THEN
-         FIND-FILES-RL @ 1- FIND-FILES-RL !
-         FIND-FILES-USE-RET @
-         IF f STR@ 0 dir xt EXECUTE THEN
-      THEN
-      f STRFREE
+      IF
+        addr up " {s}/{s}" DUP -> f STR@ data dir xt EXECUTE
+        dir
+        IF FIND-FILES-RL 1+!
+           FIND-FILES-RL @ FIND-FILES-DEPTH @ < FIND-FILES-DEPTH @ 0= OR
+           data cFileName ASCIIZ> IsNot.. AND
+           IF
+             tmpla tmplu f STR@ " {s}/{s}" DUP >R
+             STR@ xt RECURSE
+             R> STRFREE
+           THEN
+           FIND-FILES-RL @ 1- FIND-FILES-RL !
+           FIND-FILES-USE-RET @
+           IF f STR@ 0 dir xt EXECUTE THEN
+        THEN
+        f STRFREE
+      ELSE 2DROP THEN
     ELSE 2DROP THEN
     data id FindNextFileA 0=
   UNTIL
