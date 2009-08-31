601026a4b757cfa408f66b8013d3eae5d0649bfd
ygreks <>
2009-08-31 07:55:22 +0000

    Do not depend on devel Thanks to Anton Ertl -- http://groups.google.com/group/comp.lang.forth/browse_thread/thread/f9a14ef8a6a618c9#msg_304a74267b187d6a Code from ~ac/lib/string/uppercase.f

--- lib/ext/caseins-tools.f
+++ lib/ext/caseins-tools.f
@@ -1,38 +1,14 @@
-REQUIRE CHAR-UPPERCASE ~ac/lib/string/uppercase.f
-\ REQUIRE /TEST ~profit/lib/testing.f
-
-\ TRUE - strings are equal ignoring case
-: CEQUAL-U ( a1 u1 a2 u2 -- flag )
-  ROT TUCK <> IF DROP 2DROP FALSE EXIT THEN
-  0 ?DO ( a1i a2i ) 2DUP
-  C@ CHAR-UPPERCASE SWAP C@ CHAR-UPPERCASE <> IF 2DROP UNLOOP FALSE EXIT THEN
-  SWAP CHAR+ SWAP CHAR+
-  LOOP 2DROP TRUE
-;
-
-\ /TEST
-\ 
-\ REQUIRE TESTCASES ~ygrek/lib/testcase.f
-\ 
-\ TESTCASES CEQUAL-U
-\ 
-\ (( S" 2DROP" S" RDROP" CEQUAL-U -> FALSE ))
-\ (( S" rDROP" S" RDROP" CEQUAL-U -> TRUE ))
-\ (( S" " S" " CEQUAL-U -> TRUE ))
-\ (( S" 2DROP" S" 2DRO" CEQUAL-U -> FALSE ))
-\ (( S" SeArCh-woRDLiSt" S" SEARCH-WORDLIST" CEQUAL-U -> TRUE ))
-\ 
-\ END-TESTCASES
+REQUIRE CEQUAL-U lib/ext/uppercase.f
 
 : [else]   \ 94 TOOLS EXT
     1
     BEGIN
       PARSE-NAME DUP
-      IF  
+      IF
          2DUP S" [if]"   CEQUAL-U  IF 2DROP 1+                 ELSE 
          2DUP S" [else]" CEQUAL-U  IF 2DROP 1- DUP  IF 1+ THEN ELSE 
               S" [then]" CEQUAL-U  IF       1-                 THEN
-                                    THEN  THEN   
+                                    THEN  THEN
       ELSE 2DROP REFILL  AND \   SOURCE TYPE
       THEN DUP 0=
     UNTIL  DROP 
---
+++ lib/ext/uppercase.f
@@ -0,0 +1,41 @@
+
+\ Convert given char to uppercase. 
+\ Behaviour for values above 128 is implementation-defined.
+: CHAR-UPPERCASE ( c -- c1 )
+  DUP [CHAR] a [CHAR] z 1+ WITHIN IF 32 - EXIT THEN
+  DUP [CHAR] а [CHAR] я 1+ WITHIN IF 32 - THEN
+;
+
+\ Convert (addr u) to uppercase
+: UPPERCASE ( addr u -- )
+  OVER + SWAP ?DO
+    I C@ CHAR-UPPERCASE I C!
+  LOOP ;
+
+\ TRUE - strings are equal ignoring case
+: CEQUAL-U ( a1 u1 a2 u2 -- flag )
+  ROT TUCK <> IF DROP 2DROP FALSE EXIT THEN
+  0 ?DO ( a1i a2i ) 2DUP
+  C@ CHAR-UPPERCASE SWAP C@ CHAR-UPPERCASE <> IF 2DROP UNLOOP FALSE EXIT THEN
+  SWAP CHAR+ SWAP CHAR+
+  LOOP 2DROP TRUE
+;
+
+\EOF
+
+REQUIRE /TEST ~profit/lib/testing.f
+
+/TEST
+
+REQUIRE TESTCASES ~ygrek/lib/testcase.f
+
+TESTCASES CEQUAL-U
+
+(( S" 2DROP" S" RDROP" CEQUAL-U -> FALSE ))
+(( S" rDROP" S" RDROP" CEQUAL-U -> TRUE ))
+(( S" " S" " CEQUAL-U -> TRUE ))
+(( S" 2DROP" S" 2DRO" CEQUAL-U -> FALSE ))
+(( S" SeArCh-woRDLiSt" S" SEARCH-WORDLIST" CEQUAL-U -> TRUE ))
+
+END-TESTCASES
+
