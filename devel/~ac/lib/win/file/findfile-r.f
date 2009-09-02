6bb3b72f625036b44aeba1d6d54f8d2f2c27777a
spf <>
2009-09-02 20:34:59 +0000

    Последняя версия FIND-FILES-R не работает для сложных масок (просто отрезает их по CUT-PATH), несовместимо, поэтому вернулся к старому варианту, а ~pig-версию переименовал в FIND-FILES-RP.

--- devel/~ac/lib/win/file/findfile-r.f
+++ devel/~ac/lib/win/file/findfile-r.f
@@ -37,6 +37,45 @@ USER FIND-FILES-USE-RET \ true, если надо вызывать xt также при выходе
 \ addr u - имя каталога для обхода
 \ xt ( addr u data flag -- ) - процедура вызываемая для каждого файла
 
+  { addr u xt \ addr2 data id f dir }
+
+  FIND-FILES-RL @ 0= IF u FIND-FILES-U ! THEN
+  addr u S" *" SEARCH NIP NIP
+  addr u ROT IF " {s}" ELSE " {s}/*.*" THEN -> addr2
+  /WIN32_FIND_DATA ALLOCATE THROW -> data
+  data /WIN32_FIND_DATA ERASE
+  data addr2 STR@ DROP FindFirstFileA -> id
+  id -1 = IF data FREE DROP addr2 STRFREE EXIT THEN
+  BEGIN
+    data cFileName ASCIIZ> 2DUP IsNot.. FIND-FILES.. @ OR
+    IF
+      data dwFileAttributes @ FILE_ATTRIBUTE_DIRECTORY AND 0<> -> dir
+      addr u 2DUP + 1- C@ [CHAR] * =
+      IF 1-
+         BEGIN 2DUP + 1- C@ DUP [CHAR] \ <> SWAP [CHAR] / <> AND WHILE 1- REPEAT 1-
+      THEN
+      " {s}/{s}" DUP -> f STR@ data dir xt EXECUTE
+      dir
+      IF FIND-FILES-RL 1+!
+         FIND-FILES-RL @ FIND-FILES-DEPTH @ < FIND-FILES-DEPTH @ 0= OR
+         data cFileName ASCIIZ> IsNot.. AND
+         IF f STR@ xt RECURSE THEN
+         FIND-FILES-RL @ 1- FIND-FILES-RL !
+         FIND-FILES-USE-RET @
+         IF f STR@ 0 dir xt EXECUTE THEN
+      THEN
+      f STRFREE
+    ELSE 2DROP THEN
+    data id FindNextFileA 0=
+  UNTIL
+  addr2 STRFREE
+  id FindClose DROP
+  data FREE DROP
+;
+: FIND-FILES-RP ( addr u xt -- )
+\ addr u - имя каталога для обхода
+\ xt ( addr u data flag -- ) - процедура вызываемая для каждого файла
+
   { addr u xt \ addr2 up tmpla tmplu data id f dir }
 
   u				\ пустой путь обработать особым образом
