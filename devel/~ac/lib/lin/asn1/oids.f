80b65429217185baf0a32b329f09bfc9c7d58508
spf <>
2010-04-21 10:46:38 +0000

    Сокращения.

---
+++ devel/~ac/lib/lin/asn1/oids.f
@@ -0,0 +1,20 @@
+\ OID'ы, используемые в сертификатах
+
+: OID_commonName                   S" 2.5.4.3"  ;
+: OID_countryName                  S" 2.5.4.6"  ;
+: OID_localityName                 S" 2.5.4.7"  ;
+: OID_organizationName             S" 2.5.4.10" ;
+: OID_organizationalUnitName       S" 2.5.4.11" ;
+
+: OID_Certificate-policies         S" 2.5.29.32" ;
+: OID_Extended-key-usage           S" 2.5.29.37" ;
+
+: OID_emailAddress                 S" 1.2.840.113549.1.9.1" ;
+: OID_rsaEncryption                S" 1.2.840.113549.1.1.1" ; \ Public Key Algorithm
+: OID_md5WithRSAEncryption         S" 1.2.840.113549.1.1.4" ; \ Signature Algorithm
+: OID_sha1WithRSAEncryption        S" 1.2.840.113549.1.1.5" ;
+
+: OID_Revocation-URL               S" 2.16.840.1.113730.1.3"  ;
+: OID_Netscape-certificate-comment S" 2.16.840.1.113730.1.13" ;
+
+: OID_Etype                        S" 1.3.6.1.4.1.18474" ; \ iso.org.dod.internet.private.enterprises.etype
