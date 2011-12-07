cf70ec233df7b5e2acbc914669a1f2358012445c
ruv <>
2011-12-07 00:47:39 +0000

    make-forthml -- more verbal

--- devel/~pinka/spf/forthml/make-forthml.cmd
+++ devel/~pinka/spf/forthml/make-forthml.cmd
@@ -1,5 +1,7 @@
 @echo off
+set target=forthml.exe
 FOR %%I IN ( %0 ) DO pushd %%~dI%%~pI..\..\..\..\
 rem -- pushd to the sp-forth root directory
-spf4.exe ~pinka/spf/forthml/index.f  ~pinka/lib/win/directory.f S" forthml.exe" SAVE BYE
+echo creating: %CD%\%target%
+spf4.exe ~pinka/spf/forthml/index.f  ~pinka/lib/win/directory.f S" %target%" SAVE BYE
 popd
