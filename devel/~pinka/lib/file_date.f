a2a6ba96b0ba5c604f352a464ccfde8b78c7ea8f
ruv <>
2011-12-16 19:48:49 +0000

    fix bugs in file_date.f

--- devel/~pinka/lib/file_date.f
+++ devel/~pinka/lib/file_date.f
@@ -1,54 +1,16 @@
 \ 20.Jul.2002 Sat 14:02
+\ $Id$
 
-REQUIRE @FTime ~pinka\lib\ftime.f
-
-WINAPI: GetFileInformationByHandle KERNEL32.DLL (
-    LPBY_HANDLE_FILE_INFORMATION lpFileInformation  // address of structure 
-    HANDLE hFile,   // handle of file 
-  -- BOOL )   
-
-2 CELLS CONSTANT /FILETIME
-
-MODULE: BY_HANDLE_FILE_INFORMATION
-
-0 \ typedef struct _BY_HANDLE_FILE_INFORMATION // bhfi  
- 1 CELLS --   dwFileAttributes 
- /FILETIME -- ftCreationTime
- /FILETIME -- ftLastAccessTime
- /FILETIME -- ftLastWriteTime
- 1 CELLS --   dwVolumeSerialNumber
- 1 CELLS --   nFileSizeHigh
- 1 CELLS --   nFileSizeLow
- 1 CELLS --   nNumberOfLinks
- 1 CELLS --   nFileIndexHigh
- 1 CELLS --   nFileIndexLow
-
-CONSTANT /BY_HANDLE_FILE_INFORMATION
-
-;MODULE
-
-ALSO BY_HANDLE_FILE_INFORMATION
+REQUIRE FILE-FILETIME-A ~pinka/lib/win/file-info-time.f
 
 : FILE-CTIME ( h -- ftime-lo ftime-hi ior )
-  /BY_HANDLE_FILE_INFORMATION RALLOT DUP ROT
-  GetFileInformationByHandle IF ( a )
-  ftCreationTime @FTime 0    ELSE
-  DROP 0 0 GetLastError      THEN
-  /BY_HANDLE_FILE_INFORMATION RFREE    
+  FILE-FILETIME-C
 ;
 : FILE-WTIME ( h -- ftime-lo ftime-hi ior )
-  /BY_HANDLE_FILE_INFORMATION RALLOT DUP ROT
-  GetFileInformationByHandle IF ( a )
-  ftLastWriteTime @FTime 0   ELSE
-  DROP 0 0 GetLastError      THEN
-  /BY_HANDLE_FILE_INFORMATION RFREE    
+  FILE-FILETIME-W
 ;
 : FILE-ATIME ( h -- ftime-lo ftime-hi ior )
-  /BY_HANDLE_FILE_INFORMATION RALLOT DUP ROT
-  GetFileInformationByHandle IF ( a )
-  ftLastAccessTime @FTime 0  ELSE
-  DROP 0 0 GetLastError      THEN
-  /BY_HANDLE_FILE_INFORMATION RFREE    
+  FILE-FILETIME-A
 ;
 
-PREVIOUS
+\ -- for the backward compatibility
