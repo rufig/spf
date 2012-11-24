e65babd5b91cb023a9032d1fd2227685f02b179e
ruv <>
2012-11-24 09:56:57 +0000

    model xml xmlsoft -- 'writer' basics

---
+++ devel/~pinka/model/xml/xmlsoft/writer.L1.test.f
@@ -0,0 +1,10 @@
+REQUIRE EMBODY ~pinka/spf/forthml/index.f
+REQUIRE createWriterFilename ~pinka/lib/lin/xml/libxml2-writer.f
+
+`http://forth.org.ru/~pinka/model/trans/rules-slot.f.xml EMBODY
+
+
+`xmlwriter WORDLIST-NAMED PUSH-DEVELOP
+
+`writer.L1.f.xml EMBODY
+
