efa510b7647e0f9e9e01321407b6a3fb29c173cf
ruv <>
2012-12-20 23:41:31 +0000

    xmlsoft -- eliminate dependence on cURL in the some example

--- devel/~pinka/lib/lin/xml/xml-doc.f
+++ devel/~pinka/lib/lin/xml/xml-doc.f
@@ -1,5 +1,7 @@
 REQUIRE libxml2.dll  ~ac/lib/lin/xml/xml.f
 REQUIRE libxslt.dll  ~ac/lib/lin/xml/xslt.f
+\ It brings dependence on cURL
+\ Use xml-doc-lite.f to avoid this dependence.
 
 ALSO libxml2.dll
 ALSO libxslt.dll
--- devel/~pinka/model/xml/xmlsoft/xslt.test.f
+++ devel/~pinka/model/xml/xmlsoft/xslt.test.f
@@ -1,7 +1,8 @@
 REQUIRE EMBODY ~pinka/spf/forthml/index.f
 REQUIRE STHROW ~pinka/spf/sthrow.f
 
-REQUIRE  XslResultToString ~pinka/lib/lin/xml/xml-doc.f
+REQUIRE  XslResultToString ~pinka/lib/lin/xml/xml-doc-lite.f
+REQUIRE FILENAME-CONTENT   ~pinka/lib/files-ext.f
 
 \ see also: http://xmlsoft.org/
 
@@ -9,6 +10,7 @@ REQUIRE  XslResultToString ~pinka/lib/lin/xml/xml-doc.f
 `xslt.L2.f.xml EMBODY
 
   
-  `../../../proposal/io.ru.xml FILE load-xml-chunk  0. load-xml-chunk
+  `../../../proposal/io.ru.xml FILENAME-CONTENT load-xml-chunk  0. load-xml-chunk
+  \ buffer has been leaked ;)
 
   `../../../samples/2007/notion/xhtml.xsl  apply-xslt-result TYPE CR
