47b8cb71a782e6b88b0b42d26c4ddb337250961b
ruv <>
2012-12-20 03:11:03 +0000

    spf rbuf -- add RCARBON and ENSURE-ASCIIZ-R

--- devel/~pinka/spf/rbuf.f
+++ devel/~pinka/spf/rbuf.f
@@ -19,6 +19,25 @@
   SWAP
 ;
 
+: RCARBON ( addr u -- addr2 u )
+  R>
+  OVER CHAR+ CELL+ 1- >CELLS DUP RALLOT SWAP >R  ( u r a )
+  ['] (FREE-RBUF) >R
+  SWAP >R ( addr u a )
+  SWAP ( addr a u )
+  2DUP 2>R MOVE 2R> 2DUP + 0 SWAP C!
+;
+
+: ENSURE-ASCIIZ-R ( addr u -- addr2 u ) ( R: addr -- i*x addr )
+\ если поданая строка не имеет 0 за последним символом,
+\ то создается копия строки в формате ASCIIZ на стеке возвратов
+\ которая автоматически освобождается при выходе из слова,
+\ в котором вызвано ENSURE-ASCIIZ-R
+  OVER 0= IF EXIT THEN
+  2DUP + C@ 0= IF EXIT THEN
+  R> -ROT RCARBON ROT >R
+;
+
 \EOF
 
 : test
