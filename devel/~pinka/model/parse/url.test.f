cbbe14c71548a6d74d67f2eb7deb2cc05848bfe9
ruv <>
2013-01-12 18:04:30 +0000

    model parse url -- add 'authority', 'local-ref'; minor refactoring

--- devel/~pinka/model/parse/url.test.f
+++ devel/~pinka/model/parse/url.test.f
@@ -10,3 +10,5 @@ REQUIRE EMBODY    ~pinka/spf/forthml/index.f
 
   `http://A:B@forth.org.ru:81/path/file?query-part#fragment-identifier HERE OVER 2SWAP S,
   assume-url dump-location
+
+  .( local-ref -- ) local-ref TYPE CR
