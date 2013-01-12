6eb5e327b3bea64431599f8d78272cbc1f614813
ruv <>
2013-01-12 10:02:32 +0000

    add WRITE-SOCKET-FILENAME

--- devel/~pinka/lib/sockets-ext.f
+++ devel/~pinka/lib/sockets-ext.f
@@ -9,5 +9,29 @@ REQUIRE ReadSocket ~ac/lib/win/winsock/SOCKETS.F
   READOUT-SOCK
 ;
 
+\ READ-SOCK-EXACT ( a u socket -- ior )
 \ ReadSocketExact ( a u socket -- ior )
 \ -- в базовом ~ac/lib/win/winsock/SOCKETS.F
+
+
+REQUIRE PutFileTr   ~ac/lib/win/winsock/transmit.f
+
+: WRITE-SOCK-FILE ( h sock -- ior ) PutFileTr ;
+
+: WRITE-SOCK-FILENAME ( d-filename sock -- ior )
+  >R
+  R/O OPEN-FILE-SHARED DUP IF NIP RDROP EXIT THEN ( 0 ) DROP
+  R> OVER >R
+  WRITE-SOCK-FILE
+  R> CLOSE-FILE OVER IF DROP EXIT THEN NIP
+;
+
+\ just aliases:
+: WRITE-SOCKET-FILE     (          h sock -- ior ) WRITE-SOCK-FILE ;
+: WRITE-SOCKET-FILENAME ( d-filename sock -- ior ) WRITE-SOCK-FILENAME ;
+
+( -- discussion
+
+  Может быть, в именах вместо '-SOCK' использовать '-SOCKET'?
+  см. ~pinka/model/protocol/http/write-basic.f.xml
+)
