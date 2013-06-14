11218482f9f545db28d08dda608fce0545aad38b
ruv <>
2013-06-14 15:40:07 +0000

    lib win enum-hardlink -- minor bugfix, comments

--- devel/~pinka/lib/win/enum-hardlinks.f
+++ devel/~pinka/lib/win/enum-hardlinks.f
@@ -11,13 +11,16 @@ WINAPI: FindNextFileNameW       kernel32.dll
 WINAPI: FindClose               kernel32.dll
 [THEN]
 
-0x26 CONSTANT ERROR_HANDLE_EOF
+0x26 CONSTANT ERROR_HANDLE_EOF  \ 38
+0xEA CONSTANT ERROR_MORE_DATA   \ 234
 
 [UNDEFINED] MAX_PATH  [IF]
 260 CONSTANT MAX_PATH [THEN]
 
-: (TREAT-HARDLINK) ( xt a-buf -- )
-  DUP CELL- @ ( count-wchars ) DUP 2* SWAP RBUF UTF16>ANSI DUP IF 1- THEN ROT EXECUTE
+: (TREAT-HARDLINK) ( h xt a-buf -- h )
+  DUP CELL- @ ( count-wchars ) DUP 2* SWAP RBUF UTF16>ANSI
+    DUP IF 1- THEN \ remove null character
+  2SWAP SWAP >R EXECUTE R>
 ;
 : (BUF-PAIR) ( a-buf -- a-buf a-n-buf ) DUP CELL- MAX_PATH OVER !  ;
 
@@ -28,14 +31,14 @@ WINAPI: FindClose               kernel32.dll
   MAX_PATH 2* CELL+ RBUF DROP CELL+ (BUF-PAIR) OVER >R ( xt a-txt  a-buf a-n-buf )
   2SWAP SWAP R> 2>R 0 SWAP ( a-buf a-n-buf flags a-filename ) ( R: xt a-buf )
   FindFirstFileNameW
-  DUP -1 = IF GetLastError THROW THEN
+  DUP -1 = IF GetLastError THROW THEN ( h )
   2R@ (TREAT-HARDLINK)
   BEGIN ( h )
-    DUP R@ ( h a-buf ) 
+    DUP R@ ( h h a-buf ) 
     (BUF-PAIR) ROT ( h a-buf a-n-buf h )
     FindNextFileNameW
   WHILE ( h )
-    2R@ ROT >R (TREAT-HARDLINK) R>
+    2R@ (TREAT-HARDLINK)
   REPEAT ( h )
   GetLastError DUP ERROR_HANDLE_EOF = IF DROP 0 THEN SWAP
   FindClose ERR SWAP THROW THROW
