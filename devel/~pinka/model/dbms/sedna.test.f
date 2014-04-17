b5e2a80b7ba6872658d48dd6751c4deb4939792d
ruv <>
2014-04-17 18:12:56 +0000

    ADD model dbms sedna -- protection against indirect recursion while querying

--- devel/~pinka/model/dbms/sedna.test.f
+++ devel/~pinka/model/dbms/sedna.test.f
@@ -1,4 +1,5 @@
 REQUIRE EMBODY          ~pinka/spf/forthml/index.f
+REQUIRE TRHOW-DUMP      ~pinka/spf/throw-dump.f
 REQUIRE STHROW          ~pinka/spf/sthrow.f
 REQUIRE ReadSocketExact ~ac/lib/win/winsock/SOCKETS.F
 
