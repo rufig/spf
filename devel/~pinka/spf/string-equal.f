1227a103550f814b696ce13a57b29f4da049546e
ruv <>
2015-06-26 21:08:48 +0000

    FAC model forthproc -- renaming, 'EQUALS' as verb should be in the same form as 'CONTAINS'

--- devel/~pinka/spf/string-equal.f
+++ devel/~pinka/spf/string-equal.f
@@ -1,12 +1,22 @@
 \ 2006, 2007
 
-: CEQUAL ( c-addr1 u1 c-addr2 u2 -- flag ) \ counts in chars
+REQUIRE NAMING ~pinka/spf/compiler/native-wordlist.f
+
+: CEQUALS ( c-addr1 u1 c-addr2 u2 -- flag ) \ counts in chars
   DUP 3 PICK <> IF 2DROP 2DROP FALSE EXIT THEN
   COMPARE 0=
 ;
 
-: EQUAL ( c-addr1 u1 c-addr2 u2 -- flag ) \ counts in addres units (bytes)
+: EQUALS ( c-addr1 u1 c-addr2 u2 -- flag ) \ counts in addres units (bytes)
   DUP 3 PICK <> IF 2DROP 2DROP FALSE EXIT THEN
   \ 2>R >CHARS 2R> >CHARS
   COMPARE 0=
 ;
+
+( The idea:
+  'EQUALS' as verb should be in the same form as 'STARTS-WITH', 'ENDS-WITH', 'CONTAINS'
+)
+
+\ aliases for back compatibility
+S" EQUAL"   ' EQUALS    NAMING
+S" CEQUAL"  ' CEQUALS   NAMING
