783473e16b9714f8ee0e7e95f0db3f630c85cf5b
ruv <>
2015-10-01 11:46:30 +0000

    ADD lib win utf16 -- converting for UTF8

--- devel/~pinka/lib/win/utf16.f
+++ devel/~pinka/lib/win/utf16.f
@@ -28,3 +28,23 @@ WINAPI: WideCharToMultiByte     kernel32.dll
 \ m.b.: ANSI>W  and  W>ANSI ?
 
 \ see also: ~ac/lib/win/utf8.f
+
+
+
+: UTF8>UTF16 ( d-txt1 d-buf1 -- d-buf2 ) \ AKA Wide char
+  OVER >R
+  SWAP 2SWAP SWAP
+  OVER 0= IF 2DROP 2DROP R> 0 EXIT THEN
+  0 ( flags ) 65001 ( CP_UTF8 )  MultiByteToWideChar
+  DUP ERR THROW
+  2* R> SWAP
+;
+
+: UTF16>UTF8 ( d-txt1 d-buf1 -- d-buf2 )
+  2>R 0 0 2SWAP
+  2/ SWAP R> R@ 2SWAP
+  OVER 0= IF 2DROP 2DROP R> 0 EXIT THEN
+  0 ( flags ) 65001 ( CP_UTF8 )  WideCharToMultiByte
+  DUP ERR THROW
+  R> SWAP
+;
