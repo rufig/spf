2ece01f4f6e9ae07623bbd26d36bbf9fe2161cc8
Ruvim <rvm@marisdev.com>
2017-11-07 17:59:36 +0300

    FIX trans nf-ext -- bug when NOUTFOUND is absent
    The bug: enqueueNOTFOUND works incorrectly when the previous
    NOTFOUND does not exist. NOOP does not return any flag but
    it should return a flag.
    
    (missed commit from 2017-11-07 in git repo)

--- devel/~pinka/samples/2006/core/trans/nf-ext.f
+++ devel/~pinka/samples/2006/core/trans/nf-ext.f
@@ -6,14 +6,14 @@
 \ транслятор, заданный xt
 \ xt ( a u -- a u false | n*x true )
 
-  S" NOTFOUND" SFIND 0= IF 2DROP ['] NOOP THEN
+  S" NOTFOUND" SFIND 0= IF 2DROP 0 THEN
     WARNING @ >R WARNING 0!
   S" NOTFOUND" CREATED , ,
     R> WARNING !
   DOES> ( a u  a1 )
     DUP 2OVER 2>R >R
-    @ CATCH DUP 0= IF DROP RDROP RDROP RDROP EXIT THEN
-    DUP -2003 <>  OVER -321 <> AND  OVER -2011 <> AND  IF THROW THEN DROP
+    @ DUP IF CATCH DUP 0= IF DROP RDROP RDROP RDROP EXIT THEN ( x x ior )
+    DUP -2003 <>  OVER -321 <> AND  OVER -2011 <> AND  IF THROW THEN THEN ( x x ior|0 ) DROP
     \ see also: src/compiler/spf_translate.f # NOTFOUND
     ( x x ) 2DROP
     R> 2R> ROT CELL+ @ EXECUTE IF EXIT THEN
