0725a31cd2c39a2287dc6438ed944de7e8e5d0ad
Ruvim <rvm@marisdev.com>
2019-11-24 01:12:48 +0300

    FIX ~ lib win utf16 -- possible buffer overflow in UTF8>UTF16
    MultiByteToWideChar accepts output buffer size as number of wide chars
    (not bytes). Passing its size in bytes can cause buffer overrun.
    
    Testcase:
        S" xx" S" xxy" UTF8>UTF16
      - should fail (throw an exception)
    
    Also, use unsigned RSHIFT in place of signed 2/

--- devel/~pinka/lib/win/utf16.f
+++ devel/~pinka/lib/win/utf16.f
@@ -4,11 +4,13 @@ REQUIRE [UNDEFINED]             lib/include/tools.f
 WINAPI: MultiByteToWideChar     kernel32.dll
 WINAPI: WideCharToMultiByte     kernel32.dll
 [THEN]
+\ https://docs.microsoft.com/en-us/windows/win32/api/stringapiset/nf-stringapiset-multibytetowidechar
+\ https://docs.microsoft.com/en-us/windows/win32/api/stringapiset/nf-stringapiset-widechartomultibyte
 
 
 : ANSI>UTF16 ( d-txt1 d-buf1 -- d-buf2 ) \ AKA Wide char
   OVER >R
-  SWAP 2SWAP SWAP
+  1 RSHIFT SWAP 2SWAP SWAP
   OVER 0= IF 2DROP 2DROP R> 0 EXIT THEN
   0 ( flags ) 0 ( CP_ACP )  MultiByteToWideChar
   DUP ERR THROW
@@ -17,7 +19,7 @@ WINAPI: WideCharToMultiByte     kernel32.dll
 
 : UTF16>ANSI ( d-txt1 d-buf1 -- d-buf2 )
   2>R 0 0 2SWAP
-  2/ SWAP R> R@ 2SWAP
+  1 RSHIFT SWAP R> R@ 2SWAP
   OVER 0= IF 2DROP 2DROP R> 0 EXIT THEN
   0 ( flags ) 0 ( CP_ACP )  WideCharToMultiByte
   DUP ERR THROW
@@ -33,7 +35,7 @@ WINAPI: WideCharToMultiByte     kernel32.dll
 
 : UTF8>UTF16 ( d-txt1 d-buf1 -- d-buf2 ) \ AKA Wide char
   OVER >R
-  SWAP 2SWAP SWAP
+  1 RSHIFT SWAP 2SWAP SWAP
   OVER 0= IF 2DROP 2DROP R> 0 EXIT THEN
   0 ( flags ) 65001 ( CP_UTF8 )  MultiByteToWideChar
   DUP ERR THROW
@@ -42,7 +44,7 @@ WINAPI: WideCharToMultiByte     kernel32.dll
 
 : UTF16>UTF8 ( d-txt1 d-buf1 -- d-buf2 )
   2>R 0 0 2SWAP
-  2/ SWAP R> R@ 2SWAP
+  1 RSHIFT SWAP R> R@ 2SWAP
   OVER 0= IF 2DROP 2DROP R> 0 EXIT THEN
   0 ( flags ) 65001 ( CP_UTF8 )  WideCharToMultiByte
   DUP ERR THROW
