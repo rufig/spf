5ecfa63bcd51d02143b4d6bd98b1936203bc07c5
Ruvim <rvm@marisdev.com>
2019-11-24 01:22:53 +0300

    FFA ~ lib win file-link.utf8 -- non asciiz input string
    - Now input strings may be non ASCIIz
        (i.e. without NUL after the last char).
    
    - Factor out R:UTF8>UTF16 helper word (actually, a macro).

--- devel/~pinka/lib/win/file-link.utf8.f
+++ devel/~pinka/lib/win/file-link.utf8.f
@@ -22,9 +22,13 @@ WINAPI: CreateSymbolicLinkW  kernel32.dll
 )
 
 
+: R:UTF8>UTF16 ( d-txt -- d-txt2-r )
+  POSTPONE ENSURE-ASCIIZ-R POSTPONE 1+ POSTPONE DUP POSTPONE 2* POSTPONE RBUF POSTPONE UTF8>UTF16
+; IMMEDIATE
+
 : COPY-FILE-HARDLINK ( d-src d-trg -- ior )
   \ works only for regular files
-  1+ DUP 2* RBUF UTF8>UTF16 2SWAP 1+ DUP 2* RBUF UTF8>UTF16 2SWAP
+  R:UTF8>UTF16 2SWAP R:UTF8>UTF16 2SWAP
   DROP NIP \ ASCIIZ
   0 -ROT
   CreateHardLinkW ERR
@@ -32,7 +36,7 @@ WINAPI: CreateSymbolicLinkW  kernel32.dll
 
 : COPY-FILE-SYMLINK ( d-src d-trg -- ior )
   \ works only for regular files
-  1+ DUP 2* RBUF UTF8>UTF16 2SWAP 1+ DUP 2* RBUF UTF8>UTF16 2SWAP
+  R:UTF8>UTF16 2SWAP R:UTF8>UTF16 2SWAP
   DROP NIP \ ASCIIZ
   0 -ROT
   CreateSymbolicLinkW ERR
