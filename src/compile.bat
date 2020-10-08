98aa88f895f2936453e379a570980441d9b90cab
Ruvim <rvm@marisdev.com>
2020-10-08 13:32:53 +0300

    UPD src makefile -- Cygwin and more options
    - Support building Win32-based version in Cygwin.
    - Provide variable to use another host Forth system binary.
    - Ensure that the host Forth system is terminated on error.
    - Fix a bug that +x was missed for downloaded binary.
    - In Linux, spf4 binary is created in .. directory at once.

--- src/compile.bat
+++ src/compile.bat
@@ -1,3 +1,7 @@
+@echo off
+setlocal
 cd /d %~dp0 && cd ..
-@echo Wait a bit while compiling...
-@jpf375c.exe src\tc-configure-lines.f src\spf.f
+echo Current directory is %CD%
+echo Wait a bit while compiling...
+echo HALT 1  | jpf375c.exe src/tc-configure-lines.f src/spf.f
+endlocal
