e63a2ac4911594a8dcfbb4a16b1cc469239e3a47
Ruvim <ruv@users.noreply.github.com>
2021-10-05 07:59:46 +0300

    FIX lib win file-info-size -- a symlink case
    'FOR-FILE1-PROPS' passes filename without any path to the callback.
    So this filename cannot be uses as is to open the file, if it isn't
    located in the current directory.
    
    When the file is a symlink, use the input filename to open the file.

--- devel/~pinka/lib/win/file-info-size.f
+++ devel/~pinka/lib/win/file-info-size.f
@@ -5,11 +5,12 @@
 
 REQUIRE FILENAME-SYMLINK ~pinka/lib/win/file-info-symlink.f
 
-: FILENAME-SIZE ( d-txt-filename -- d-size | 0 0 )
+: FILENAME-SIZE ( sd-filename-full -- d-size | 0 0 )
   \ it may throw ERROR_ACCESS_DENIED at the least
-  0. 2SWAP \ size is zero if the file is not exists
-  LAMBDA{ ( 0 0 addr u data )
-    -ROT >R >R >R 2DROP
+  0. 2OVER \ size is zero if the file is not exists
+  LAMBDA{ ( sd-filename-full 0 0 sd-filename-no-path data -- sd-filename x x )
+    \ NB: sd-filename-no-path doesn't contain a path and so cannot be used here as is.
+    >R 2DROP 2DROP
     R@ nFileSizeLow   T@
     R@ nFileSizeHigh  T@
     [ /CELL 8 < 0= ] [IF] 32 LSHIFT OR 0 [THEN] \ recombination in case of 64bits system
@@ -17,11 +18,10 @@ REQUIRE FILENAME-SYMLINK ~pinka/lib/win/file-info-symlink.f
     R@ dwFileAttributes T@ FILE_ATTRIBUTE_REPARSE_POINT AND IF
     R@ dwReserved0      T@ IO_REPARSE_TAG_SYMLINK = IF \ file is symlink
       2DROP
-      RDROP R> R> R/O OPEN-FILE-SHARED THROW DUP >R FILE-SIZE THROW R> CLOSE-FILE THROW
-      EXIT
+      2DUP R/O OPEN-FILE-SHARED THROW DUP >R FILE-SIZE THROW R> CLOSE-FILE THROW
     THEN THEN THEN
-    RDROP RDROP RDROP
-  } FOR-FILE1-PROPS
+    RDROP
+  } FOR-FILE1-PROPS  2NIP
 ;
 \ TODO: throw error in case of the file is not found #maybe
 
