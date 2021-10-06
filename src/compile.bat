8b6edfb456552a73f32bfa4fa9c57b31ed2f0fea
Ruvim <ruv@users.noreply.github.com>
2021-10-06 14:35:21 +0300

    FIX src makefile -- wrong exit status #minor
    Due to a typo, an undefined exit status was used on an error.

--- src/compile.bat
+++ src/compile.bat
@@ -3,5 +3,5 @@ setlocal
 cd /d %~dp0 && cd ..
 echo Current directory is %CD%
 echo Wait a bit while compiling...
-echo HALT 1  | jpf375c.exe src/tc-configure-lines.f src/spf.f
+echo 1 HALT  | jpf375c.exe src/tc-configure-lines.f src/spf.f
 endlocal
