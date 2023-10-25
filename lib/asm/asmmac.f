df5155ec1fc3eaa186c7580de4487af9dbac1973
ruv <ruv@users.noreply.github.com>
2023-10-25 04:04:48 +0400

    FIX lib asm -- incorrect use of "IMMEDIATE"
    The definition for ";M" (via "MACRO: ;M") was made immediate
    when the compilation word list was already changed. It's incorrect.
    This bug is fixed via introducing "ENDM-IMM", which ends the macro
    definition and also makes it immediate.
    
    Along with this bug fix, the synonym "FORTH.IMMEDIATE" for "IMMEDIATE"
    was introduced in the vocabulary ASM-HIDDEN to make some code shorter.

--- lib/asm/asmmac.f
+++ lib/asm/asmmac.f
@@ -16,7 +16,7 @@
 (    along with this program; if not, write to the Free Software          )
 (    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.            )
 
-MACRO: ;M POSTPONE ;MACRO ENDM IMMEDIATE
+MACRO: ;M POSTPONE ;MACRO ENDM-IMM
 MACRO: AL, AL , ;M
 MACRO: CL, CL , ;M
 MACRO: DL, DL , ;M
