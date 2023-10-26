30389514875b6e6f25c74aae7de37465ed2fac4f
ruv <ruv@users.noreply.github.com>
2023-10-26 19:30:31 +0400

    FEA src compile.bat -- autoload jpf binary if missing
    If the file "jpf375c.exe" is missing, download it from
    a release in "spf4-cvs-archive" repo using powershell.

--- src/compile.bat
+++ src/compile.bat
@@ -2,6 +2,11 @@
 setlocal
 cd /d %~dp0 && cd ..
 echo Current directory is %CD%
+if not exist "jpf375c.exe" (
+  echo "jpf375c.exe" is not exist, downloading...
+  set url=https://github.com/rufig/spf4-cvs-archive/releases/download/v1.0/jpf375c.exe
+  powershell "(new-object System.Net.WebClient).DownloadFile('%url%','./jpf375c.exe')"
+)
 echo Wait a bit while compiling...
 echo 1 HALT  | jpf375c.exe src/tc-configure-lines.f src/spf.f
 endlocal
