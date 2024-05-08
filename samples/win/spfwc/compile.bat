e037fe3961d741f65e03667a94316defce202ec2
ruv <ruv@users.noreply.github.com>
2024-05-09 01:54:38 +0400

    FIX samples win spfwc -- support lf lines in compile.bat
    1. If a Windows batch file (bat or cmd) is saved with LF line endings,
    some labels may not be found sometimes for an unknown reason.
    Adding a dummy comment after the label solves this problem.
    
    2. Fix a minor bug: "&" was incorrectly used instead of "&&" in the
    batch file.

--- samples/win/spfwc/compile.bat
+++ samples/win/spfwc/compile.bat
@@ -9,9 +9,9 @@ cd ..\..\..\devel\~yz\prog\fres
 ..\..\..\..\spf4.exe fres.f
 cd ..\..\..\..\samples\win\spfwc
 :fres_exe_done
-if exist spf4wc.fres goto fres_done
+if exist spf4wc.fres goto fres_done rem this comment is to avoid the error "The system cannot find the batch label specified"
 ..\..\..\devel\~yz\prog\fres\fres.exe spf4wc.res
 :fres_done
 ..\..\..\spf4.exe spf4wc.f
 
-@if exist spf4wc.exe move /Y spf4wc.exe ..\..\..\spf4wc.exe & echo (!) The spf4wc.exe is located near the spf4.exe & pause
+@if exist spf4wc.exe move /Y spf4wc.exe ..\..\..\spf4wc.exe && echo (!) NB: spf4wc.exe is placed near spf4.exe & pause
