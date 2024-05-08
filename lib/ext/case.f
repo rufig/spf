d1e7cb11be6d879d3d4e3d73cf8a6de8e67f06c3
ruv <ruv@users.noreply.github.com>
2024-05-08 17:57:58 +0400

    ADD lib include control-case -- thread-safe CASE ENDCASE
    "lib/include/control-case.f" - a simpler and thread-safe
    implementation for the CASE ... ENDCASE control structure
    (unlike the old one in "lib/ext/case.f").
    
    This new implementation is now used in "lib/" and "samples/".
    The old one is kept for backward compatibility.

--- lib/ext/case.f
+++ lib/ext/case.f
@@ -1,8 +1,18 @@
-\ Конструкция выбора CASE
-\ с учетом возможной вложенности операторов CASE
+\ Конструкция выбора CASE ENDCASE
+
+\ Данная реализация полагает, что "orig" располагается на стеке данных.
+\ Из-за глобального определения стека "S-CSP" эта реализация
+\ не является потокобезопасной во время компиляции.
+
+\ Эта реализация используется в "./spf-asm.f" и "../lib/asm/486asm.f"
+\ И там используются слова из семейства CSP, определенные здесь.
+
+\ Более простая, портабельная и потокобезопасная реализация: "lib/include/control-case.f"
 
 
-DECIMAL
+REQUIRE [IF] lib/include/tools.f
+
+
 VARIABLE   CSP    \ Указатель стека контроля
 6 CONSTANT L-CAS# \ Допустимый уровень вложенности
 CREATE     S-CSP   L-CAS# CELLS ALLOT \ Стек контроля
@@ -24,6 +34,11 @@ S-CSP CSP !
   SP@ CSP@ <> 37 ?ERROR ( ABORT" Сбой стека по CSP !")
   -CSP
 ;
+
+
+S" ENDCASE" GET-CURRENT SEARCH-WORDLIST 0= ?DUP NIP [IF]
+\ avoid redefinition if these words are already defined in the compilation word list
+
 : CASE ( -> )
   !CSP
 ; IMMEDIATE
@@ -38,3 +53,5 @@ S-CSP CSP !
   POSTPONE DROP BEGIN SP@ CSP@ =
   0=  WHILE  [COMPILE] THEN  REPEAT -CSP
 ; IMMEDIATE
+
+[THEN]
