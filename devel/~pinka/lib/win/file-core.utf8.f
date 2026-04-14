648961435be799a2c4d0b547298c2b2619c916d5
ruv <ruv@users.noreply.github.com>
2026-04-14 16:03:50 +0000

    ADD devel ~ lib win file-core.utf8 -- missing words
    Add UTF-8 versions of the words
      - `CREATE-FILE` `FILE-STATUS`

--- devel/~pinka/lib/win/file-core.utf8.f
+++ devel/~pinka/lib/win/file-core.utf8.f
@@ -7,6 +7,8 @@ REQUIRE R:UTF8>UTF16        ~pinka/lib/win/utf16-rbuf.f
 [UNDEFINED] DeleteFileW             [IF]  WINAPI: DeleteFileW           kernel32.dll    [THEN]
 [UNDEFINED] GetFileAttributesW      [IF]  WINAPI: GetFileAttributesW    kernel32.dll    [THEN]
 
+[UNDEFINED] FILE_ATTRIBUTE_DIRECTORY    [IF] 16 CONSTANT FILE_ATTRIBUTE_DIRECTORY   [THEN]
+[UNDEFINED] INVALID_FILE_ATTRIBUTES     [IF] -1 CONSTANT INVALID_FILE_ATTRIBUTES    [THEN]
 
 
 \ The original definitions are taken from: spf4/src/win/spf_win_io.f
@@ -36,6 +38,17 @@ REQUIRE R:UTF8>UTF16        ~pinka/lib/win/utf16-rbuf.f
 : DELETE-FILE ( c-addr u -- ior ) \ 94 FILE
   R:UTF8>UTF16 DROP DeleteFileW ERR
 ;
+: CREATE-FILE ( c-addr u fam -- fileid ior ) \ Forth-94 FILE
+  UNROT R:UTF8>UTF16 ROT
+  NIP SWAP >R >R
+  0 FILE_ATTRIBUTE_ARCHIVE ( template attrs )
+  CREATE_ALWAYS
+  0 ( secur )
+  0 ( share )
+  R> ( access=fam )
+  R> ( filename )
+  CreateFileW DUP -1 = IF GetLastError ELSE 0 THEN
+;
 : OPEN-FILE ( c-addr u fam -- fileid ior ) \ 94 FILE
   -ROT R:UTF8>UTF16 ROT
   NIP SWAP >R >R
@@ -47,6 +60,12 @@ REQUIRE R:UTF8>UTF16        ~pinka/lib/win/utf16-rbuf.f
   R> ( filename )
   CreateFileW DUP -1 = IF GetLastError ELSE 0 THEN
 ;
+: FILE-STATUS ( sd.filename -- x.attributes 0 | 0 ior\0 ) \ Forth-94 FILE-EXT
+  R:UTF8>UTF16 DROP
+  GetFileAttributesW
+  DUP INVALID_FILE_ATTRIBUTES <> IF 0 EXIT THEN
+  DROP 0 GetLastError
+;
 : FILE-EXIST ( addr u -- f )
   R:UTF8>UTF16
   DROP GetFileAttributesW -1 <>
@@ -55,3 +74,6 @@ REQUIRE R:UTF8>UTF16        ~pinka/lib/win/utf16-rbuf.f
   R:UTF8>UTF16
   DROP GetFileAttributesW INVERT 16 ( FILE_ATTRIBUTE_DIRECTORY) AND 0<>
 ;
+
+
+\ #eof
